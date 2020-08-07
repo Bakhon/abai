0439af36fa67279abc227e0b02700a4cddc1ae1a
Almukhan Omarov <almukhan.flt@gmail.com>
2020-08-07 14:59:33 +0600

    scor fix

--- resources/views/production/gtmscor.blade.php
+++ resources/views/production/gtmscor.blade.php
@@ -5,7 +5,7 @@
         <h2 class="subtitle">ГТМ скорпион</h2>
         <div class="level1-content row">
             <div class="main col-md-12 col-lg-12 row">
-            <iframe src="{{ url('http://localhost:8088/superset/dashboard/23') }}" width="100%" height="725px" frameborder="0" allowfullscreen></iframe>
+            <iframe src="{{ url('http://172.20.103.32:8080/superset/dashboard/2') }}" width="100%" height="725px" frameborder="0" allowfullscreen></iframe>
             </div>
         </div>
     </div>
