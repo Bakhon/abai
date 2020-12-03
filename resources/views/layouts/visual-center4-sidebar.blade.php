f18a3cbcca8d50c25b51968bbf03b8161a9e982d
Константин <aynkz@mail.ru>
2020-12-03 10:18:04 +0600

    vc update app.scss

--- resources/views/layouts/visual-center4-sidebar.blade.php
+++ resources/views/layouts/visual-center4-sidebar.blade.php
@@ -2,7 +2,7 @@
 <div id="sidebar-container2" class="col-0">
     <div>
         <!--<ul class="menu-collapsed d-none " aria-expanded="false" data-toggle="collapse show">-->
-        <ul class="menu-collapsed" aria-expanded="false" data-toggle="collapse show">
+        <ul class="menu-collapsed padding" aria-expanded="false" data-toggle="collapse show">
             <a href="#" class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
                 <div class="d-flex w-100 justify-content-start align-items-center"><img src="/img/gno/home.png" width="25" height="25" class="companyLogo"> <span class="menu-collapsed companyName d-none"></span></div>
             </a>
