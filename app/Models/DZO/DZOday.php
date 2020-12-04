84a09ff780a9ce16878279773dd756308bd5666a
o.smagul <o.smagul@niikmg.kz>
2020-12-04 09:38:55 +0600

    commit2

--- app/Models/DZO/DZOday.php
+++ app/Models/DZO/DZOday.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class DZOday extends Model
 {
     protected $table = 'd_z_odays';
+
+    protected $fillable = [
+        'date'
+    ];
 }
--- app/Models/DZO/DZOyear.php
+++ app/Models/DZO/DZOyear.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class DZOyear extends Model
 {
     protected $table = 'd_z_oyears';
+
+    protected $fillable = [
+        'date'
+    ];
 }
