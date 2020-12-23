782cbfc8ba5736eccae46b15aa4ecdfb8248e29c
Ruslan <russel1987@mail.ru>
2020-12-23 16:10:12 +0300

    temporarily commented module block in sidebar

--- resources/views/layouts/head-sidebar.blade.php
+++ resources/views/layouts/head-sidebar.blade.php
@@ -1,13 +1,13 @@
 <div id="sidebar-container2" class="col-0">
     <div class="left-panel h-100">
         @yield('sidebar_menu')
-        <div class="module-block">
+<!--        <div class="module-block">
             <div class="module-block__icon">
                 @yield('module_icon')
             </div>
             <div class="module-block__title">
                 @yield('module_title')
             </div>
-        </div>
+        </div>-->
     </div>
 </div>
