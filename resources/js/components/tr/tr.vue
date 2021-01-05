4fe5900c0b4e02da7a3d7272b9429d4ad49deafb
A.Kirikbayev <A.Kirikbayev@1-3484-P>
2021-01-05 16:40:24 +0600

    error_correction_ front _and_css

--- resources/js/components/tr/tr.vue
+++ resources/js/components/tr/tr.vue
@@ -119,13 +119,14 @@
                 id="companySelect"
                 @change="onChangeYear($event)"
               >
+                <option value="2021">2021</option>
                 <option value="2020">2020</option>
                 <option value="2019">2019</option>
-                <option value="2018">2018</option>
+                <!-- <option value="2018">2018</option>
                 <option value="2017">2017</option>
                 <option value="2016">2016</option>
                 <option value="2015">2015</option>
-                <option value="2014">2014</option>
+                <option value="2014">2014</option> -->
               </select>
             </div>
             <a href="#" @click.prevent="chooseDt" class="btn btn-sm button_form"
