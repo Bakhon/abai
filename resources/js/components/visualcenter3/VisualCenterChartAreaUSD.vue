3f83d2bf9dc0076c833568ab5899f174254c3f79
Serhii Suhal <vio3273@gmail.com>
2021-02-12 16:25:32 +0200

    oil chart tooltip fixes

--- resources/js/components/visualcenter3/VisualCenterChartAreaUSD.vue
+++ resources/js/components/visualcenter3/VisualCenterChartAreaUSD.vue
@@ -147,13 +147,8 @@ export default {
     };
   },
   methods: {
-    getDate(timestamp) {
-      return this.$moment(timestamp).format('dddd, DD MMMM YYYY');
-    },
     updateChartOptions(value) {
       let self = this;
-
-      // console.log(value);
 
       this.chartOptions = {
         ...this.chartOptions, ...{
@@ -162,17 +157,17 @@ export default {
           },
           tooltip: {
             x: {
-              format: 'dd MMM yyyy'
+              formatter: function (value) {
+                return self.$moment(value).format('DD MMM YYYY');
+              }
             },
             style: {
               fontSize: '14px',
             },
             custom: function({series, seriesIndex, dataPointIndex, w}) {
-
               return '<div class="vc-chart-tooltip">' +
                   '<div>Покупка</div>' +
                   '<div class="vc-chart-tooltip-value">' + series[seriesIndex][dataPointIndex] + '</div>' +
-                  // '<div>' + self.getDate(w.globals.labels[dataPointIndex]) + '</div>' +
                   '</div>'
             }
           },
