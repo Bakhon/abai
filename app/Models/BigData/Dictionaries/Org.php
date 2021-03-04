3c75159258917c61daab7fe4b516fe82821cc2eb
Ruslan <russel1987@mail.ru>
2021-03-04 14:01:20 +0300

    updated links to database tables

--- app/Models/BigData/Dictionaries/Org.php
+++ app/Models/BigData/Dictionaries/Org.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\DB;
 
 class Org extends TBDModel
 {
-    protected $table = 'dict.org';
+    protected $table = 'tbdi.org';
 
     //relations
 
@@ -20,9 +20,9 @@ class Org extends TBDModel
     public function fieldIds()
     {
         $result = DB::connection('tbd')
-            ->table('dict.org as org')
-            ->select('org.id', 'df.field as field')
-            ->leftJoin('dict.dzo_field as df', 'org.id', '=', 'df.dzo')
+            ->table('tbdi.org as org')
+            ->select('tbdi.id', 'df.field as field')
+            ->leftJoin('tbdi.dzo_field as df', 'org.id', '=', 'df.dzo')
             ->where('org.id', '=', $this->id)
             ->whereNotNull('field')
             ->get()
