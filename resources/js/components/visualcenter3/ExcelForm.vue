7742360be51e9a128167c98b1d8fb7c9f15fa1af
Serhii Suhal <vio3273@gmail.com>
2021-03-05 11:16:54 +0200

    temporary

--- resources/js/components/visualcenter3/ExcelForm.vue
+++ resources/js/components/visualcenter3/ExcelForm.vue
@@ -1,115 +1,155 @@
 <template>
-  <div>
-    <hot-table
-      :data="data"
-      :colHeaders="true"
-      :settings="hotSettings"
-      :rowHeaders="true"
-      licenseKey="non-commercial-and-evaluation"
-      ref="myTable"
-    >
-      <input type="hidden" name="oil_plan" v-model="oil_plan" />
-      <input type="hidden" name="oil_fact" v-model="oil_fact" />
-      <button @click="changeData()" class="btn btn-primary" id="submit_btn">
-        Сохранить
-      </button>
-      <div @click="changeData()" class="btn btn-primary" id="submit_btn">
-        Проверить ошибки
-      </div>
-    </hot-table>
-  </div>
+    <div class="table-form">
+        <v-grid
+                theme="material"
+                :source="rows"
+                :columns="columns"
+                @afterEdit="afterEdit"
+                :rowSize="30"
+        ></v-grid>
+    </div>
 </template>
- 
+
 <script>
-import { HotTable } from "@handsontable/vue";
-export default {
-  data: function () {
-    return {
-      temp: [1],
-      oil_plan: "",
-      oil_fact: "",
-      data: [
-        [
-          '                           СУТОЧНЫЙ РАПОРТ  АО"КАРАЖАНБАСМУНАЙ"  ЗА 			',
-          " ",
-          " ",
-          " ",
-          "13.02.2021",
-          " ",
-          " ",
-          " ",
-        ],
-        ["НЕФТЬ /OIL", " ДОБЫЧА, тонн 						"],
-        [, "ГРАФИК на месяц ", "СУТОЧНАЯ 		", " ", " ", "С НАЧАЛА МЕСЯЦА		"],
-        [, , "План ", "Факт", "(+,-)", "План", "Факт ", "(+,-)"],
-        ['АО "КАРАЖАНБАСМУНАЙ" '],
-      ],
-      hotSettings: {
-        colHeaders: true,
-        rowHeaders: true,
-        contextMenu: true,
-        readOnly: true,
-        className: "first-class",
-        stretchH: "all",
-        mergeCells: [
-          { row: 0, col: 0, rowspan: 1, colspan: 4 },
-          { row: 1, col: 0, rowspan: 3, colspan: 1 },
-          { row: 1, col: 1, rowspan: 1, colspan: 7 },
-          { row: 2, col: 1, rowspan: 2, colspan: 1 },
-          { row: 2, col: 2, rowspan: 1, colspan: 3 },
-          { row: 2, col: 5, rowspan: 1, colspan: 3 },
-        ],
-        startCols: 30,
-        startRows: 50,
-        formulas: true,
-        licenseKey: "non-commercial-and-evaluation",
-        cells: function (row, col, prop) {
-          var cellProperties = {};
+    import VGrid from "@revolist/vue-datagrid";
 
-          if (row === 4) {
-            cellProperties.readOnly = false;
-          }
-
-          if (col > 0) {
-            if (row === 4) {
-              cellProperties.type = "numeric";
-              cellProperties.allowEmpty = false;
-            }
-          }
-
-          return cellProperties;
+    export default {
+        data: function () {
+            return {
+                columns: [
+                    {
+                        prop: "name",
+                        size: 350,
+                        cellProperties: ({prop, model, data, column}) => {
+                            return {
+                                style: {
+                                    border: '1px solid #F4F4F6',
+                                    fontWeight: 'bold'
+                                },
+                            };
+                        },
+                    },
+                    {
+                        prop: "details",
+                        size: 280,
+                        cellProperties: ({prop, model, data, column}) => {
+                            return {
+                                style: {
+                                    border: '1px solid #F4F4F6'
+                                },
+                            };
+                        },
+                    },
+                    {
+                        prop: "name2",
+                        size: 250,
+                        cellProperties: ({prop, model, data, column}) => {
+                            return {
+                                style: {
+                                    border: '1px solid #F4F4F6'
+                                },
+                            };
+                        },
+                    },
+                    {
+                        prop: "name3",
+                        size: 260,
+                        cellProperties: ({prop, model, data, column}) => {
+                            return {
+                                style: {
+                                    border: '1px solid #F4F4F6'
+                                },
+                            };
+                        },
+                    },
+                    {
+                        prop: "name4",
+                        size: 150,
+                        cellProperties: ({prop, model, data, column}) => {
+                            return {
+                                style: {
+                                    border: '1px solid #F4F4F6'
+                                },
+                            };
+                        },
+                    },
+                    {
+                        prop: "name5",
+                        size: 150,
+                        cellProperties: ({prop, model, data, column}) => {
+                            return {
+                                style: {
+                                    border: '1px solid #F4F4F6'
+                                },
+                            };
+                        },
+                    },
+                    {
+                        prop: "name6",
+                        size: 150,
+                        cellProperties: ({prop, model, data, column}) => {
+                            return {
+                                style: {
+                                    border: '1px solid #F4F4F6'
+                                },
+                            };
+                        },
+                    },
+                    {
+                        prop: "name7",
+                        size: 150,
+                        cellProperties: ({prop, model, data, column}) => {
+                            return {
+                                style: {
+                                    border: '1px solid #F4F4F6'
+                                },
+                            };
+                        },
+                    },
+                ],
+                rows: [],
+                rowsCount: 72,
+            };
         },
-      },
+        async mounted() {
+            this.createInitialRows();
+        },
+        methods: {
+            createInitialRows() {
+                for (let i = 1; i <= this.rowsCount; i++) {
+                    this.rows.push(this.getRowData());
+                }
+            },
+            getRowData() {
+                let row = [];
+                _.forEach(this.columns, function (column) {
+                    let initialObject = {};
+                    initialObject[column.prop] = "";
+                    row.push(initialObject);
+                })
+                return row;
+            },
+            afterEdit(e) {
+                console.log(e);
+                //console.log(e.detail.data[0]['column_1']);
+                //e.detail.data[0]['column_1'] = "55";
+                // e.detail.data.val = "123";
+            },
+        },
+        components: {
+            VGrid,
+        },
     };
-  },
-  methods: {
-    changeData: function () {
-      this.oil_plan = this.data[4][2];
-      this.oil_fact = this.data[4][3];
-      if (this.oil_plan * 2 < this.oil_fact) {
-        this.$refs.myTable.hotInstance.setCellMeta(
-          4,
-          3,
-          "className",
-          "bg-danger text-white"
-        ); 
-        this.$refs.myTable.hotInstance.render(); 
-      } else {
-        this.$refs.myTable.hotInstance.setCellMeta(
-          4,
-          3,
-          "className",
-          "first-class"
-        );
-        this.$refs.myTable.hotInstance.render();
-        alert("Ошибок нет")
-
-      }
-    },
-  },
-  components: {
-    HotTable,
-  },
-};
 </script>
+
+<style>
+    revo-grid {
+        height: 900px;
+    }
+
+    .table-form {
+        max-width: 1700px;
+        background-color: white;
+    }
+</style>
 
