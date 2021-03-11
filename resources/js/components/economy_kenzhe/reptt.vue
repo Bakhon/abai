1da384b15e3882e3c82215eb5f2286a4e51a0b01
akhmedinov <azat_ahmedinov@mail.ru>
2021-03-11 18:14:07 +0600

    code formatting fixed in vue file

--- resources/js/components/economy_kenzhe/reptt.vue
+++ resources/js/components/economy_kenzhe/reptt.vue
@@ -5,54 +5,59 @@
 </template>
 
 <script>
-  export default {
-      props:[ 
-          'reptt'
-      ],
-    data() {
-      return {
-        data: [],
-        defaultProps: {
-          id: 'id',
-          children: 'repts',
-          label: 'title', 
-        }
-      };
-    },
-    methods: {
-      handleNodeClick(data) {
-        console.log(data);
-      }
-    },
-    computed:{
-      propsChildren() {
-        return this.props.children || 'repts' || 'childRepts';
-      },
-    }
-  };
-</script>
- <style>
-            .el-tree-node__label{
-                color: white!important;
+    export default {
+        props: [
+            'reptt'
+        ],
+        data() {
+            return {
+                data: [],
+                defaultProps: {
+                    id: 'id',
+                    children: 'repts',
+                    label: 'title',
+                }
+            };
+        },
+        methods: {
+            handleNodeClick(data) {
+                console.log(data);
             }
-            .el-tree{
-  padding: 16px;
-  background-color: #20274f!important;
-}
-.el-tree-node:focus>.el-tree-node__content>.el-tree-node__label{
- color: #20274f!important;
-}
+        },
+        computed: {
+            propsChildren() {
+                return this.props.children || 'repts' || 'childRepts';
+            },
+        }
+    };
+</script>
+<style>
+    .el-tree-node__label {
+        color: white !important;
+    }
 
-.el-tree-node__expand-icon{
-  color: white;
-}
-.el-tree-node__content:hover .el-tree-node__label, .el-tree-node__label:hover{
-  color: #20274f!important;
-}
-.el-tree-node__content:hover .el-tree-node__expand-icon.is-leaf{
-color: none!important;
-}
-.el-tree-node__content:hover .el-tree-node__expand-icon{
-color: #20274f!important;
-}
-        </style>
+    .el-tree {
+        padding: 16px;
+        background-color: #20274f !important;
+    }
+
+    .el-tree-node:focus > .el-tree-node__content > .el-tree-node__label {
+        color: #20274f !important;
+    }
+
+    .el-tree-node__expand-icon {
+        color: white;
+    }
+
+    .el-tree-node__content:hover .el-tree-node__label, .el-tree-node__label:hover {
+        color: #20274f !important;
+    }
+
+    .el-tree-node__content:hover .el-tree-node__expand-icon.is-leaf {
+        color: none !important;
+    }
+
+    .el-tree-node__content:hover .el-tree-node__expand-icon {
+        color: #20274f !important;
+    }
+</style>
