a6db6046a19b5b54977bb07a15c556567ed2345a
Serhii Suhal <vio3273@gmail.com>
2021-03-30 07:07:46 +0300

    get wrapper outside

--- tests/Unit/Visualcenter3ExcelForm/Chemistry_block_is_visible.spec.js
+++ tests/Unit/Visualcenter3ExcelForm/Chemistry_block_is_visible.spec.js
@@ -2,13 +2,14 @@ import { mount } from "@vue/test-utils";
 import Setup from '../../Config/setup.js';
 import ExcelForm from '@/visualcenter3/importForm/ExcelForm.vue';
 
+const wrapper = mount(ExcelForm, {
+    mocks: {
+        localeUrl: () => {'/dzo_chemistry_excel_form'},
+    }
+});
+
 describe('ExcelForm component', () => {
     it('chemistry button is visible', () => {
-        const wrapper = mount(ExcelForm, {
-            mocks: {
-                localeUrl: () => {'/dzo_chemistry_excel_form'},
-            }
-        });
         expect(wrapper
             .find('#chemistryButton')
             .isVisible())
