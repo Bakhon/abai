fc6779ab88ded6ef8f673edc419f940cee6cb99c
Serhii Suhal <vio3273@gmail.com>
2021-03-30 07:22:46 +0300

    change arch

--- tests/Unit/Visualcenter3ExcelForm/Chemistry_block_is_visible.spec.js
+++ tests/Unit/Visualcenter3ExcelForm/Chemistry_block_is_visible.spec.js
@@ -2,11 +2,19 @@ import { mount } from "@vue/test-utils";
 import Setup from '../../Config/setup.js';
 import ExcelForm from '@/visualcenter3/importForm/ExcelForm.vue';
 
-const wrapper = mount(ExcelForm, {
-    mocks: {
-        localeUrl: () => {'/dzo_chemistry_excel_form'},
-    }
+let wrapper = null;
+
+beforeEach( () => {
+    wrapper = mount(ExcelForm, {
+        mocks: {
+            localeUrl: () => {'/dzo_chemistry_excel_form'},
+        }
+    });
 });
+
+afterEach( () => {
+    wrapper.destroy();
+})
 
 describe('ExcelForm component', () => {
     it('chemistry button is visible', () => {
