01427866a5961476dd130ef40abd720a9cff83de
Nikolai Demin <demin.nikolai@Inbox.ru>
2021-04-01 14:51:41 +0300

    add migration

--- database/migrations/2021_03_25_194245_update_eco_refs_first_costs_table.php
+++ database/migrations/2021_03_25_194245_update_eco_refs_first_costs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateEcoRefsCostsTable extends Migration
+class UpdateEcoRefsFirstCostsTable extends Migration
 {
     /**
      * Run the migrations.
