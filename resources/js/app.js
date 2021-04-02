81dd3a0cb61930e59dc00bdd6713e160b0475515
Кирикбаев Абат Саматович <A.Kirikbayev@niikmg.kz>
2021-04-02 11:59:56 +0600

    add column in table

--- resources/js/app.js
+++ resources/js/app.js
@@ -145,7 +145,7 @@ Vue.component('cat-loader', require('./components/ui-kit/CatLoader.vue').default
 Vue.component('pagination', require('laravel-vue-pagination'));
 Vue.component('report-export', require('./components/reports/export.vue').default);
 Vue.component('tr_mode-table', require('./components/tr/TechMode.vue').default);
-Vue.component('tr_weekly_chart', require('./components/tr/FaWeeklyChart.vue').default);
+Vue.component('fa_weekly_chart', require('./components/tr/FaWeeklyChart.vue').default);
 
 
 
