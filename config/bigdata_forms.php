6c224aab4bba4e37d80e02146751dd56e1d3decb
Konstantin Borodkin <k.borodkin@niikmg.kz>
2021-04-05 21:18:14 +0600

    added input form for injection wells, branch in work

--- config/bigdata_forms.php
+++ config/bigdata_forms.php
@@ -2,4 +2,5 @@
   'well_register' => 'App\\Services\\BigData\\Forms\\WellRegister',
   'fluid_production' => 'App\\Services\\BigData\\Forms\\FluidProduction',
   'production_well' => '\\App\\Services\\BigData\\Forms\\ProductionWell',
+  'injection_wells' => '\\App\\Services\\BigData\\Forms\\InjectionWells',
 );
