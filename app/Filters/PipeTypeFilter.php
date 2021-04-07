0b57c9a49b95bd1ebbdcffabc6b6c239d96b2fd6
Nikolai Demin <demin.nikolai@Inbox.ru>
2021-04-07 13:40:53 +0300

    clear code

--- app/Filters/PipeTypeFilter.php
+++ app/Filters/PipeTypeFilter.php
@@ -10,7 +10,7 @@ class PipeTypeFilter extends BaseFilter
             case 'material':
                 $this->query
                     ->select('pipe_types.*')
-                    ->leftJoin('materials', 'materials.id', '=', 'pipe_types.material_id')
+                    ->leftJoin('materials', 'materials.id', 'pipe_types.material_id')
                     ->orderBy('materials.name', $desc === true ? 'desc' : 'asc');
                 break;
             default:
