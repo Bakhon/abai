10ee0f38baeb20904e0ea2e20b8bd3768bb64024
Workstation Ryzen <fannyfan414@gmail.com>
2021-04-07 10:26:17 +0200

    offset design fix

--- app/Models/EcoRefsCost.php
+++ app/Models/EcoRefsCost.php
@@ -7,31 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class EcoRefsCost extends Model
 {
     protected $fillable = [
-        'sc_fa',
-        'company_id',
-        'date',
-        'variable',
-        'fix_noWRpayroll',
-        'fix_payroll',
-        'fix_nopayroll',
-        'fix',
-        'gaoverheads',
-        'wr_nopayroll',
-        'wr_payroll',
-        'wo',
-        'comment',
-        'author_id',
-        'log_id'
+        'sc_fa', 'company_id', 'date', 'variable', 'fix_noWRpayroll', 'fix_payroll',
+        'fix_nopayroll', 'fix', 'gaoverheads', 'wr_nopayroll', 'wr_payroll', 'wo',
+        'comment', 'author_id', 'log_id'
     ];
 
     public function scfa()
     {
-        return $this->hasOne('App\Models\Refs\EcoRefsScFa','id','sc_fa')->withDefault();
+        return $this->hasOne('App\Models\Refs\EcoRefsScFa', 'id', 'sc_fa')->withDefault();
     }
 
     public function company()
     {
-        return $this->hasOne('App\Models\EcoRefsCompaniesId','id','company_id')->withDefault();
+        return $this->hasOne('App\Models\EcoRefsCompaniesId', 'id', 'company_id')->withDefault();
     }
 
     public function author()
