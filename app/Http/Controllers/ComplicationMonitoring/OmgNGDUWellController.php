4eefd333497be62fbd9a3be8b139a1129683a2a2
Nikolai Demin <demin.nikolai@Inbox.ru>
2021-04-12 12:15:22 +0300

    clean code

--- app/Http/Controllers/ComplicationMonitoring/OmgNGDUWellController.php
+++ app/Http/Controllers/ComplicationMonitoring/OmgNGDUWellController.php
@@ -14,7 +14,6 @@ use App\Models\Refs\Zu;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 
 class OmgNGDUWellController extends CrudController
