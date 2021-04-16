92fccb58ff83f23f37aeca75d8f635a6be295077
RakhatAubakirov <rakha.aubakirov@gmail.com>
2021-04-16 13:46:36 +0600

    fixed translations in history omguhe

--- resources/lang/kz/monitoring.php
+++ resources/lang/kz/monitoring.php
@@ -143,8 +143,6 @@ return array (
       'bsw' => 'Сулану, %',
       'surge_tank_pressure' => 'Буферлік сыйымдылық қысымы, кгс\\см2',
       'pump_discharge_pressure' => 'Сораптың шығыс қысымы, кгс\\см2',
-      'heater_inlet_pressure' => 'Пештің кіріс температурасы, С',
-      'heater_output_pressure' => 'Пештің шығыс температурасы, С',
       'temperature' => 'Пештің кіріс температурасы, С',
       'heater_inlet_temperature' => 'Пештің кіріс температурасы, С',
       'heater_output_temperature' => 'Пештің шығыс температурасы, С',
@@ -211,23 +209,6 @@ return array (
     ),
   ),
   'corrosion_name' => 'Коррозия',
-  'pipe' => 
-  array (
-    'menu' => 'Құбырлар',
-    'title' => 'Құбырлар дерекқоры',
-    'create_title' => 'Құбырлар бойынша деректерді енгізу',
-    'edit_title' => 'Құбырлар бойынша түзетулерді енгізу',
-    'fields' => 
-    array (
-      'length' => 'Ұзындық',
-      'outside_diameter' => 'Сыртқы диаметр',
-      'inner_diameter' => 'Ішкі диаметр',
-      'thickness' => 'Қабырға қалыңдығы',
-      'roughness' => 'Кедір-бұдырлық',
-      'material' => 'Материал',
-      'plot' => 'Бөлшегі',
-    ),
-  ),
   'inhibitor' => 
   array (
     'title' => 'Ингибитор анықтамалығы',
@@ -262,7 +243,27 @@ return array (
       'daily_gas_production_in_sib' => 'БӨС газ мөлшері',
     ),
   ),
-  'well' => 'Ұңғы',
+  'well' => 
+  array (
+    'well' => 'Ұңғы',
+  ),
+  'pipe' => 
+  array (
+    'fields' => 
+    array (
+      'inner_diameter' => 'Ішкі диаметр',
+      'length' => 'Ұзындық',
+      'material' => 'Материал',
+      'outside_diameter' => 'Сыртқы диаметр',
+      'plot' => 'Бөлшегі',
+      'roughness' => 'Кедір-бұдырлық',
+      'thickness' => 'Қабырға қалыңдығы',
+    ),
+    'edit_title' => 'Құбырлар бойынша түзетулерді енгізу',
+    'title' => 'Құбырлар дерекқоры',
+    'menu' => 'Құбырлар',
+    'create_title' => 'Құбырлар бойынша деректерді енгізу',
+  ),
   'pipe_types' => 
   array (
     'fields' => 
