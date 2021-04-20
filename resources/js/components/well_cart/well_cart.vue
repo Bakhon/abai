5aa95d539bdf11491c91a2225f36dd44356440af
IlyasDossanov <i.dossanov@kmg.kz>
2021-04-21 01:50:49 +0600

    add some html+css

--- resources/js/components/well_cart/well_cart.vue
+++ resources/js/components/well_cart/well_cart.vue
@@ -1,3 +1,52 @@
 <template>
-  <h1>О, получилось!</h1>
+  <div class="row">
+    <div class="col-md-6 bg-dark">
+        <div class="row">
+            <div class="col">
+            <select class ="transparent-select" id="">
+                <option>Дело скважины</option>
+                <option>Option 2</option>
+            </select>
+            </div>
+        </div>
+            <div class="row">
+            <div class="col">
+            <select id ="" class="form-control small-select">
+                <option>Фонд</option>
+                <option>Option 2</option>
+            </select >
+            <select id ="" class="form-control small-select">
+                <option>График замеров</option>
+                <option>Option 2</option>
+            </select>
+            <select id ="" class="form-control small-select">
+                <option>Эксплуатационные показатели</option>
+                <option>Option 2</option>
+            </select>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-2">
+        <div class="bg-dark">
+         <h1>О, получилось!</h1>
+        </div>
+    </div>
+        <div class="col-md-4">
+        <div class="bg-dark">
+         <h1>О, получилось!</h1>
+        </div>
+    </div>
+  </div>
 </template>
+<style>
+    .small-select{
+        width:30%;
+        margin: 10px;
+        padding: 10px;
+    }
+    .transparent-select{
+        color: white;
+        background: none;
+
+    }
+</style>
