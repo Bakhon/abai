147556ce4ee14d5f3966d520760236cf385e77e1
IlyasDossanov <i.dossanov@kmg.kz>
2021-04-21 18:05:01 +0600

    in-progress-add-file-structure

--- resources/js/components/well_cart/well_cart.vue
+++ resources/js/components/well_cart/well_cart.vue
@@ -3,23 +3,38 @@
 
         <div class="col-md-3">
                 <div class="bg-dark">
-                        <div class="row">
-                                <div class="well-deal">
-                                    <div class="icon-ierarchy"></div>
-                                    <h2>Дело скважины</h2>
-                                </div>
-                        </div>
-                        <div class="row">
-                            <div class="directory">
-                                <img src="/img/bd/pointer-solid.svg">
-                                <div class="icon-pointer-solid">
+                    <div class="row">
+                        <div class="col">
+                        <div class="well-deal">
+                            <div class="icon-ierarchy"></div>
+                            <h2>Дело скважины</h2>
+                         </div>
+                     </div>
 
+                        <div class="directory">
+                            <div class="row">
+                                <div class="col">
+                                    <img src="/img/bd/pointer-solid.svg" class="pointer">
+                                    <div class="border-pointer-solid"></div>
+                                    <img src="/img/bd/folder.svg" class="full-size-icon">
+                                        <h3>КПД Заместителя председателя Правления по розведке и добыче
+                                        <span class="names">(Марабаев Ж. Н.)</span>
+                                    </h3>
                                 </div>
-                                <h3>КПД Заместителя председателя Правления по розведке и добыче</h3>
+                            </div>
+
+                            <div class="row">
+                                <div class="col">
+                                    <div class="file">
+                                        <img src="/img/bd/file.svg" class="full-size-icon">
+                                        <h3>Прирост извлекаемых запасов нефти и конденсата</h3>
+                                    </div>
+                                </div>
                             </div>
                         </div>
+                    </div>
                 </div>
-         </div>
+        </div>
 
     <div class="col-md-4 bg-dark">
         <div class="row">
@@ -109,16 +124,22 @@
         }
     }
     .directory{
-        display:flex;
+        display:block;
         border-top: 1px solid #555BA6;
         width: 100%;
         padding: 10px;
-        height: 100px;
+        //height: 100px;
         margin: 10px 40px 10px 25px;
+        col{
+            display:flex;
+        }
         h3{
             font-size: 14px;
             font-weight: 700;
             line-height: 17px;
+        }
+        .names{
+                color: #999DC0;
         }
         .icon-directory{
             width: 10px;
@@ -126,20 +147,20 @@
             background: url(/img/bd/folder.svg) no-repeat;
             padding: 0 15px 0 10px;
         }
-        .icon-pointer-solid{
+        .border-pointer-solid{
             width: 10px;
             height: 100%;
-            //background: url(/img/bd/pointer-solid.svg) no-repeat;
             padding: 0 15px 0 10px;
-            margin-top: 5px;
+            margin-top: 2px;
             border-top: 1px dashed #555BA6;
             border-left: 1px dashed #555BA6;
             border-bottom: 1px dashed #555BA6;
-            
         }
-        img{
-            width: 5px;
-        }
+        .pointer{
+            position: absolute;
+            width: 10px;
+            margin-bottom: auto;
+            }
         /*
         .directory-border{
             border-top: 1px dashed #555BA6;
@@ -147,6 +168,10 @@
             border-bottom: 1px dashed #555BA6;
         }
         */
+        .file{
+            padding-left: 35px;
+            display: flex;
+        }
     }
     .txt5{
         display: flex;
@@ -178,6 +203,14 @@
     .dropdown-icon-3{
         background: white url(/img/bd/chart.svg) 50% 50% no-repeat;
     }
+    .file-icon{
+        background: url(/img/bd/file.svg) 50% 50% no-repeat;
+    }
+    .full-size-icon{
+        width: 20px;
+        margin-bottom: auto;
+        padding-right: 5px;
+    }
     .icon-all{
         float: left;
         height: 20px;
@@ -205,13 +238,7 @@
         white-space: nowrap;
         background:url(/img/bd/pointer.svg) 50% 100% no-repeat;
     }
-    .dropdown-toggle{
-        white-space: initial;
-    }
-    .dropdown-toggle::after{
-        float: right;
-        content: none url(/img/bd/pointer.svg) 50% 50% no-repeat;
-    }
+
     .col{
         display: flex;
     }
