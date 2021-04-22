31c6115c97f9bce82e6a88281bbd238039657436
Nikolai Demin <demin.nikolai@Inbox.ru>
2021-04-22 15:30:31 +0300

    added calculation hydro dinamics of pipes

--- app/Jobs/ExportHydroCalcTableToExcel.php
+++ app/Jobs/ExportHydroCalcTableToExcel.php
@@ -3,6 +3,8 @@
 namespace App\Jobs;
 
 use App\Exports\PipeLineCalcExport;
+use App\Exports\PipeLineResultExport;
+use App\Imports\HydroCalcResultImport;
 use App\Models\ComplicationMonitoring\OmgNGDU;
 use App\Models\ComplicationMonitoring\TrunklinePoint;
 use Illuminate\Bus\Queueable;
@@ -10,6 +12,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
 use Imtigger\LaravelJobStatus\Trackable;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -45,6 +48,7 @@ class ExportHydroCalcTableToExcel implements ShouldQueue
     public function handle()
     {
         $points = TrunklinePoint::with('map_pipe.pipeType', 'map_pipe.firstCoords', 'map_pipe.lastCoords', 'gu', 'trunkline_end_point')->get();
+        $isErrors = false;
 
         foreach($points as $key => $point) {
             if (!$points[$key]->gu) {
@@ -61,12 +65,19 @@ class ExportHydroCalcTableToExcel implements ShouldQueue
             $points[$key]->omgngdu = $query->orderBy('date', 'desc')->first();
 
             if (!$points[$key]->omgngdu) {
+                $isErrors = true;
                 continue;
             }
 
             $temperature = $points[$key]->omgngdu->heater_output_temperature;
             $temperature = $temperature ? ($temperature < 40 ? 50 : $temperature) : 50;
             $points[$key]->omgngdu->heater_output_temperature = $temperature;
+
+            if (!$points[$key]->omgngdu->pump_discharge_pressure ||
+                !$points[$key]->omgngdu->daily_fluid_production ||
+                !$points[$key]->omgngdu->bsw) {
+                $isErrors = true;
+            }
         }
 
         $columnNames = [
@@ -98,13 +109,38 @@ class ExportHydroCalcTableToExcel implements ShouldQueue
             'columnNames' => $columnNames
         ];
 
-        $fileName = '/export/pipeline_calc_input.xlsx';
-        Excel::store(new PipeLineCalcExport($data), 'public'.$fileName);
+        $fileName = 'pipeline_calc_input.xlsx';
+        $filePath = 'public/export/'.$fileName;
+//        Excel::store(new PipeLineCalcExport($data), $filePath);
+//        copy(Storage::path($filePath), env('HYDRO_CALC_PATH').$fileName);
+
+        if ($isErrors AND isset($this->input['date'])) {
+            $command = escapeshellcmd(env('HYDRO_CALC_PATH').env('HYDRO_CALC_PY_FILENAME'));
+            $output = shell_exec($command);
+
+            if ($output) {
+                $this->setOutput(
+                    [
+                        'error' => $output
+                    ]
+                );
+            }
+
+            if (file_exists(env('HYDRO_CALC_PATH').'calc_results.csv')) {
+                $this->importResult();
+                Excel::store(new PipeLineResultExport($this->input['date']), $filePath);
+            }
+        }
 
         $this->setOutput(
             [
-                'filename' => '/storage'.$fileName
+                'filename' => Storage::url($filePath)
             ]
         );
+    }
+
+    protected function importResult ()
+    {
+        Excel::import(new HydroCalcResultImport($this->input['date']), env('HYDRO_CALC_PATH').'calc_results.csv');
     }
 }
