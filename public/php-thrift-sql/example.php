917ffd51df259a33ffbf99a2466f08e624c19c96
Константин <aynkz@mail.ru>
2021-04-22 10:45:31 +0600

    work connection

--- public/php-thrift-sql/example.php
+++ public/php-thrift-sql/example.php
@@ -1,32 +1,23 @@
 <?php
 
-  // Load this lib
-  require_once __DIR__ . '/ThriftSQL.phar';
 
-  // Try out a Hive query via iterator object
-  $hive = new \ThriftSQL\Hive( '172.20.103.38', 10000, 'hive', 'hive' );
-  $hiveTables = $hive
-    ->connect()
-    ->getIterator( 'SHOW TABLES' );
+require_once __DIR__ . '/ThriftSQL.phar';
 
-  // Try out an Impala query via iterator object
-  /*$impala = new \ThriftSQL\Impala( '172.20.103.38' );//( 'impala.host.local' );
-  $impalaTables = $impala
-    ->connect()
-    ->setOption( 'REQUEST_POOL', 'php' )
-    ->setOption( 'MEM_LIMIT', '256mb' )
-    ->getIterator( 'SHOW TABLES' );*/
 
-  // Execute the Hive query and iterate over the result set
-  foreach( $hiveTables as $rowNum => $row ) {
-    print_r( $row );
-  }
+$hive = new \ThriftSQL\Hive('172.20.103.38', 10000, 'hive', 'hive');
+$hiveTables = $hive
+  ->connect()->getIterator('select * from tbd.agent_type');
 
-  // Execute the Impala query and iterate over the result set
-  foreach( $impalaTables as $rowNum => $row ) {
-    print_r( $row );
-  }
 
-  // Don't forget to close socket connection once you're done with it
-  $hive->disconnect();
-  $impala->disconnect();
+echo "<pre>";
+print_r($hiveTables);
+echo "<pre>";
+
+foreach ($hiveTables as $rowNum => $row) {
+  echo "<pre>";
+  print_r($row);
+  echo "</pre>";
+}
+
+
+$hive->disconnect();
