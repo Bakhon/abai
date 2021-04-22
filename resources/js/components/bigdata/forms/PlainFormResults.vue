3edba531f4cc95e1194f8ed7f8b0242789a682b5
Ruslan <russel1987@mail.ru>
2021-04-22 14:47:40 +0300

    added table with form results, refactored a bit

---
+++ resources/js/components/bigdata/forms/PlainFormResults.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="bd-main-block__table">
+    <table v-if="rows" class="table">
+      <thead>
+      <tr>
+        <th>ID</th>
+        <th v-for="column in columns">{{ column.title }}</th>
+        <th></th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(row, index) in rows">
+        <td>{{ row.id }}</td>
+        <td v-for="column in columns">
+          {{ row[column.code] }}
+        </td>
+        <td>
+          <a href="#" @click.prevent="editRow(row)">Редактировать</a>
+          <a href="#" @click.prevent="deleteRow(row, index)">Удалить</a>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BigdataPlainFormResults",
+  props: {
+    params: {
+      type: Object,
+      required: true
+    },
+    wellId: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      rows: null,
+      columns: null
+    }
+  },
+  watch: {
+    params() {
+      this.init()
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      this.axios.get(
+          this.localeUrl(`/api/bigdata/forms/${this.params.code}/results`),
+          {params: {well_id: this.wellId}}
+      ).then(({data}) => {
+        this.rows = data.rows
+        this.columns = data.columns
+      })
+    },
+    editRow(row) {
+      this.$emit('edit', row)
+    },
+    deleteRow(row, rowIndex) {
+      this.axios.delete(this.localeUrl(`/api/bigdata/forms/${this.params.code}/${row.id}`)).then(({data}) => {
+        this.rows.splice(rowIndex, 1)
+      })
+    }
+  }
+}
+</script>
+<style lang="scss">
+.bd-main-block {
+  &__table {
+    .table {
+      color: #fff;
+    }
+  }
+}
+</style>
---
+++ resources/js/components/bigdata/forms/PlainFormWrapper.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="bd-main-block">
+    <notifications position="top"></notifications>
+    <div class="bd-main-block__header">
+      <div>
+        <p v-if="well" class="bd-main-block__header-title">Скважина {{ well.uwi }}</p>
+        <p class="bd-main-block__header-title">{{ params.title }}</p>
+      </div>
+      <div>
+        <a v-if="isFormShowed" href="#" @click="isFormShowed = false">Закрыть</a>
+        <a v-else href="#" @click="openCreateForm">Добавить запись</a>
+      </div>
+    </div>
+    <BigdataPlainForm v-if="isFormShowed" :params="params" :values="formValues" :well-id="wellId"></BigdataPlainForm>
+    <BigdataPlainFormResults
+        v-else
+        :params="params"
+        :well-id="wellId"
+        @edit="openEditForm"
+    >
+    </BigdataPlainFormResults>
+  </div>
+</template>
+
+<script>
+import BigdataPlainForm from './PlainForm'
+import BigdataPlainFormResults from './PlainFormResults'
+
+export default {
+  name: "BigDataPlainFormWrapper",
+  components: {
+    BigdataPlainForm,
+    BigdataPlainFormResults
+  },
+  props: {
+    params: {
+      type: Object,
+      required: true
+    },
+    wellId: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      well: null,
+      isFormShowed: false,
+      formValues: null
+    }
+  },
+  watch: {
+    params() {
+      this.isFormShowed = false
+    }
+  },
+  mounted() {
+    this.axios.get(this.localeUrl(`/api/bigdata/wells/${this.wellId}`)).then(({data}) => {
+      this.well = data.well
+    })
+  },
+  methods: {
+    openCreateForm(values) {
+      this.isFormShowed = true
+      this.formValues = null
+    },
+    openEditForm(values) {
+      this.isFormShowed = true
+      this.formValues = values
+    }
+  }
+};
+</script>
+<style lang="scss">
+.bd-main-block {
+  max-width: 1340px;
+  margin: 0 auto;
+
+  &__header {
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    margin: 16px 0 20px;
+
+    &-title {
+      color: #fff;
+      font-weight: bold;
+      font-size: 20px;
+      line-height: 24px;
+      margin: 0;
+    }
+  }
+}
+</style>
