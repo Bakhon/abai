1bc3716fefadba56b102c38b71031448aeae4edc
Nikolai Demin <demin.nikolai@Inbox.ru>
2021-04-22 10:19:13 +0300

    fix v-for warning

--- resources/js/components/omgca/table.vue
+++ resources/js/components/omgca/table.vue
@@ -54,7 +54,7 @@
       <button type="button" class="close" data-dismiss="alert">&times;</button>
       <p>{{ params.success }}</p>
     </div>
-    <b-alert v-for="alert in alerts"  :variant="alert.variant" show dismissible>
+    <b-alert v-for="(alert, index) in alerts" :key="index" :variant="alert.variant" show dismissible>
       {{ alert.message }}
     </b-alert>
     <div class="table-page__wrapper">
