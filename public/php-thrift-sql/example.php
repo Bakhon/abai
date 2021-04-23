0afccd79811db76c271ca6dcde5366a636954e0f
Константин <aynkz@mail.ru>
2021-04-23 09:49:51 +0600

    update bd and table

--- public/php-thrift-sql/example.php
+++ public/php-thrift-sql/example.php
@@ -6,11 +6,11 @@ require_once __DIR__ . '/ThriftSQL.phar';
 
 $hive = new \ThriftSQL\Hive('172.20.103.38', 10000, 'hive', 'hive');
 $hiveTables = $hive
-  ->connect()->getIterator('select * from tbd.agent_type');
+  ->connect()->getIterator('select * from kazger.KMG_I_PRD_AREA_VIEW');
 
 
 echo "<pre>";
-print_r($hiveTables);
+//print_r($hiveTables);
 echo "<pre>";
 
 foreach ($hiveTables as $rowNum => $row) {
