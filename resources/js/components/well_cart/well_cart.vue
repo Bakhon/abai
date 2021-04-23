183c144f2162ec0563183561061dcd1902eef378
IlyasDossanov <i.dossanov@kmg.kz>
2021-04-24 00:00:53 +0600

    relese: well-passport block

--- resources/js/components/well_cart/well_cart.vue
+++ resources/js/components/well_cart/well_cart.vue
@@ -52,7 +52,12 @@
                             <div class="dropdown small-select">
                                     <button class="btn btn-secondary select-button" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         <div class="button1-vc-inner">
-                                                <div class="icon-all dropdown-icon-1"> </div>
+                                                <div class="icon-all "> 
+                                                    <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M15.5708 0H10.4567C4.80802 0 0.228516 4.56843 0.228516 10.2035V15.3052C0.228516 20.9403 4.80802 25.5087 10.4567 25.5087H15.5708C21.2196 25.5087 25.7991 20.9403 25.7991 15.3052V10.2035C25.7991 4.56843 21.2196 0 15.5708 0Z" fill="#FEFEFE"/>
+                                                    <path d="M12.6738 19.9728C9.63312 19.9728 7.34045 18.6828 7.34045 16.9728V9.30609H7.37179C7.60493 8.43959 8.23036 7.73225 9.06179 7.39476C10.1926 6.87128 11.428 6.61294 12.6738 6.63942C15.7145 6.63942 18.0071 7.92942 18.0071 9.63942C18.0065 9.75126 17.9957 9.86282 17.9751 9.97276H18.0071V16.9728C18.0071 18.6828 15.7145 19.9728 12.6738 19.9728ZM8.52512 11.5688V16.9728C8.52512 17.6521 10.1405 18.6394 12.6738 18.6394C15.2071 18.6394 16.8218 17.6521 16.8218 16.9728V11.5688C15.5719 12.3196 14.1309 12.6914 12.6738 12.6394C11.2164 12.6914 9.77532 12.3194 8.52512 11.5688ZM12.6738 7.97276C10.1405 7.97276 8.52512 8.96009 8.52512 9.63942C8.52512 10.3188 10.1405 11.3061 12.6738 11.3061C15.2071 11.3061 16.8218 10.3188 16.8218 9.63942C16.8218 8.96009 15.2071 7.97276 12.6738 7.97276Z" fill="#237DEB"/>
+                                                    </svg>
+                                                </div>
                                                 <div class="txt5">Фонд <div class="icon-pointer"> </div>
                                                 </div>
                                                 
@@ -67,7 +72,12 @@
                             <div class="dropdown small-select">
                                     <button class="btn btn-secondary select-button" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         <div class="button1-vc-inner">
-                                                <div class="icon-all dropdown-icon-2"> </div>
+                                                <div class="icon-all">
+                                                    <svg width="27" height="26" viewBox="0 0 27 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M16.2447 0H11.1306C5.48184 0 0.902344 4.56843 0.902344 10.2035V15.3052C0.902344 20.9403 5.48184 25.5087 11.1306 25.5087H16.2447C21.8934 25.5087 26.473 20.9403 26.473 15.3052V10.2035C26.473 4.56843 21.8934 0 16.2447 0Z" fill="#FEFEFE"/>
+                                                    <path d="M13.3164 18.9729C9.63623 18.9688 6.65382 15.9864 6.64978 12.3062V12.1729C6.72307 8.50923 9.73951 5.59151 13.4036 5.64013C17.0676 5.68875 20.0056 8.68547 19.9816 12.3497C19.9577 16.0141 16.9808 18.9721 13.3164 18.9729ZM13.3164 6.97287C10.3721 6.97581 7.98605 9.3619 7.98311 12.3062H13.3164V6.97287Z" fill="#237DEB"/>
+                                                    </svg>
+                                                </div>
                                                 <div class="txt5">График замеров <div class="icon-pointer"></div></div>
                                         </div>
                                     </button>
@@ -83,7 +93,12 @@
                             <div class="dropdown small-select">
                                     <button class="btn btn-secondary select-button" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         <div class="button1-vc-inner">
-                                                <div class="icon-all dropdown-icon-3"> </div>
+                                                <div class="icon-all">
+                                                    <svg width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M16.2451 0.590454H11.1306C5.48188 0.590454 0.902344 5.15888 0.902344 10.7939V15.8957C0.902344 21.5308 5.48188 26.0992 11.1306 26.0992H16.2451C21.8939 26.0992 26.4734 21.5308 26.4734 15.8957V10.7939C26.4734 5.15888 21.8939 0.590454 16.2451 0.590454Z" fill="#FEFEFE"/>
+                                                    <path d="M14.3874 14.8393C14.5124 14.9644 14.6819 15.0346 14.8587 15.0346H15.2494C15.4262 15.0346 15.5957 14.9644 15.7207 14.8393L18.8587 11.7013L20.3874 13.23V9.22998H16.3874L17.9161 10.7586L15.0541 13.6206L13.0541 11.6206C12.9291 11.4956 12.7595 11.4254 12.5827 11.4253H12.1921C12.0153 11.4254 11.8457 11.4956 11.7207 11.6206L7.24939 16.092L8.19206 17.0346L12.3874 12.8393L14.3874 14.8393Z" fill="#237DEB"/>
+                                                    </svg>
+                                                </div>
                                                 <div class="txt5">Эксплуатационные показатели <div class="icon-pointer"></div></div>
                                         </div>
                                     </button>
@@ -103,7 +118,11 @@
                                     <div class="heading">
                                         <p>Поиск скважины</p>                                
                                     </div>           
-                                    <div class="sheare-icon"></div>     
+                                    <div class="sheare-icon">
+                                        <svg width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M2.15108 1.67037V10.0779H10.5791V7.57345C10.5791 6.4918 12.155 6.48024 12.155 7.57345V10.83C12.155 11.3117 11.8157 11.6502 11.3328 11.6502H1.39717C0.914278 11.6502 0.574951 11.3117 0.574951 10.83V0.91834C0.574951 0.436621 0.914278 0.0981445 1.39717 0.0981445H4.78901C5.87368 0.0981445 5.88526 1.67037 4.78901 1.67037H2.15108ZM10.5794 2.80054L9.02811 4.32156C8.59501 4.75437 7.2806 6.28694 6.70546 6.28694C6.10677 6.28694 5.95124 5.91689 5.95124 5.39898C5.95124 4.99466 7.5975 3.54026 7.95378 3.18523L9.44653 1.66999C9.0999 1.66961 8.75397 1.6696 8.40734 1.67037C7.77236 1.67191 6.97219 1.73738 6.97219 0.850182C6.97219 0.41082 7.37128 0.0981445 7.79395 0.0981445H11.3328C11.8157 0.0981445 12.155 0.436621 12.155 0.91834V4.44863C12.155 4.92997 11.8157 5.26844 11.3328 5.26844C10.494 5.26844 10.5783 4.34697 10.5791 3.76937C10.5794 3.4463 10.5794 3.12323 10.5794 2.80054Z" fill="#82BAFF"/>
+                                        </svg>
+                                    </div>  
                                 </div>
                             </div>
                         <div class="row">
@@ -599,7 +618,11 @@
                             <div class="heading">
                                 <p>Паспорт скважины</p>                                
                             </div>           
-                            <div class="sheare-icon"></div>   
+                        <div class="sheare-icon">
+                            <svg width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M2.15108 1.67037V10.0779H10.5791V7.57345C10.5791 6.4918 12.155 6.48024 12.155 7.57345V10.83C12.155 11.3117 11.8157 11.6502 11.3328 11.6502H1.39717C0.914278 11.6502 0.574951 11.3117 0.574951 10.83V0.91834C0.574951 0.436621 0.914278 0.0981445 1.39717 0.0981445H4.78901C5.87368 0.0981445 5.88526 1.67037 4.78901 1.67037H2.15108ZM10.5794 2.80054L9.02811 4.32156C8.59501 4.75437 7.2806 6.28694 6.70546 6.28694C6.10677 6.28694 5.95124 5.91689 5.95124 5.39898C5.95124 4.99466 7.5975 3.54026 7.95378 3.18523L9.44653 1.66999C9.0999 1.66961 8.75397 1.6696 8.40734 1.67037C7.77236 1.67191 6.97219 1.73738 6.97219 0.850182C6.97219 0.41082 7.37128 0.0981445 7.79395 0.0981445H11.3328C11.8157 0.0981445 12.155 0.436621 12.155 0.91834V4.44863C12.155 4.92997 11.8157 5.26844 11.3328 5.26844C10.494 5.26844 10.5783 4.34697 10.5791 3.76937C10.5794 3.4463 10.5794 3.12323 10.5794 2.80054Z" fill="#82BAFF"/>
+                            </svg>
+                        </div>   
                         </div>
                     </div>
                     <div class="row">
@@ -621,11 +644,11 @@
                                 <div class="row">
                                     <div class="col">
                                         <div class="icon-container">
-                                            <svg width="25" height="23" viewBox="0 0 25 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <svg style="float:left" width="25" height="23" viewBox="0 0 25 23" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" clip-rule="evenodd" d="M3.46183 0.00268555C2.00545 0.00268555 0.824219 1.18058 0.824219 2.63344V19.7331C0.824219 21.1863 2.00545 22.3639 3.46183 22.3639H21.921C23.3774 22.3639 24.5585 21.1863 24.5585 19.7331V5.26422C24.5585 3.81098 23.3774 2.63344 21.921 2.63344H10.0543C10.0543 1.18058 8.8734 0.00268555 7.41741 0.00268555H3.46183Z" fill="white"/>
                                             </svg>
 
-                                            <svg width="24" height="23" viewBox="0 0 24 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <svg style="margin-left:20px" width="24" height="23" viewBox="0 0 24 23" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" clip-rule="evenodd" d="M3.51517 9.72488C3.26582 9.72488 3.01655 9.62976 2.82626 9.43992C2.44528 9.05986 2.44528 8.44533 2.82626 8.06527L9.55673 1.35125C9.93771 0.970807 10.5539 0.970807 10.9348 1.35125C11.3158 1.73131 11.3158 2.34588 10.9348 2.72594L4.20423 9.43992C4.01432 9.62976 3.76491 9.72488 3.51517 9.72488Z" fill="white"/>
                                             <path fill-rule="evenodd" clip-rule="evenodd" d="M11.5551 4.86371C11.3652 4.86371 11.178 4.78979 11.0387 4.64963L7.62743 1.24681C7.41783 1.03888 7.35571 0.725045 7.46881 0.452805C7.58191 0.180564 7.84858 0.00268555 8.14388 0.00268555H11.5551C11.9589 0.00268555 12.2863 0.329208 12.2863 0.7316V4.13442C12.2863 4.42899 12.1079 4.69546 11.835 4.80828C11.7442 4.84525 11.6497 4.86371 11.5551 4.86371Z" fill="white"/>
                                             <path fill-rule="evenodd" clip-rule="evenodd" d="M7.41382 13.8571V19.4475H1.56641V13.8571C1.56641 13.1859 2.11177 12.6418 2.78456 12.6418H6.19566C6.86807 12.6418 7.41382 13.1863 7.41382 13.8571Z" fill="white"/>
@@ -634,7 +657,7 @@
                                             <path fill-rule="evenodd" clip-rule="evenodd" d="M23.2504 22.3638H1.32199C0.918623 22.3638 0.591309 22.0372 0.591309 21.6345C0.591309 21.232 0.918623 20.9055 1.32199 20.9055H23.2504C23.6538 20.9055 23.9816 21.232 23.9816 21.6345C23.9816 22.0372 23.6538 22.3638 23.2504 22.3638Z" fill="white"/>
                                             </svg>
 
-                                            <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <svg style="float:right" width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" clip-rule="evenodd" d="M12.5546 0V12.4246H17.5359L11.3684 18.6364L5.08203 12.4246H10.0634V0H12.5546Z" fill="#A18F47"/>
                                             <path fill-rule="evenodd" clip-rule="evenodd" d="M22.5176 19.8789H0.0996094V22.3638H22.5176V19.8789Z" fill="#A18F47"/>
                                             </svg>
@@ -785,6 +808,7 @@ h4{
           background-color: #A18F47;
       }
       .icon-container{
+          width: 100%;
           margin:10px;
       }
     }
@@ -792,6 +816,8 @@ h4{
         display: flex;
         padding-left: 5px;
         font-size: 12px;
+        text-align: left;
+        padding-left: 10px;
     }
     .dropdown{
         position:relative;
@@ -799,6 +825,7 @@ h4{
     .small-select{
         min-width: 244px;
         margin-right: 0.25em;
+        margin-bottom: 0.25em;
     }
     .transparent-select{
         padding: 10px 0;
@@ -845,22 +872,6 @@ h4{
             border-left: 1px solid #464D7A;
         }
     }
-    .dropdown-icon-1{
-        background: white url(/img/bd/fond.svg) 50% 50% no-repeat;
-    }
-    .dropdown-icon-2{
-        background: white url(/img/bd/donat-chart.svg) 50% 50% no-repeat;
-    }
-    .dropdown-icon-3{
-        background: white url(/img/bd/chart.svg) 50% 50% no-repeat;
-    }
-    .file-icon{
-        background: url(/img/bd/file.svg) 50% 50% no-repeat;
-    }
-    .pointer-large-icon{
-        margin-top: auto;
-        margin-bottom: auto;
-    }
     .full-size-icon{
         width: 20px;
         margin-bottom: auto;
@@ -871,9 +882,12 @@ h4{
         height: 20px;
         width: 20px;
         border-radius: 15px;
+        margin-top: auto;
+        margin-bottom: auto;
     }
     .button1-vc-inner{
         display: inline-block;
+        float:left;
     }
     .btn:not(:disabled):not(.disabled){
         width: 100%;
@@ -897,12 +911,9 @@ h4{
         display: flex;
         margin-right: auto;
         margin-left: auto;
-        padding: 10px 30px 0px 20px;
-    }center
+      //  padding: 10px 30px 0px 20px;
+    }
     .sheare-icon{
-        background: url("/img/bd/share.svg") no-repeat;
-        width: 10px;
-        padding: 10px;
         margin-left: auto;
         margin-top: auto;
         margin-bottom: auto;
@@ -918,6 +929,9 @@ h4{
         margin: none;
         padding:20px;
         }
+    }
+    .pointer-large-icon{
+        padding: 20px;
     }
     .bg-dark-transparent{
         background-color: rgba(39, 41, 83, 0.85);
