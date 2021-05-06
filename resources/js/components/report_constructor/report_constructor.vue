455c638c2fb3a6c90cae457eeaf7c4bb5c5b3d15
IlyasDossanov <i.dossanov@kmg.kz>
2021-05-06 16:52:02 +0600

    add structure

--- resources/js/components/report_constructor/report_constructor.vue
+++ resources/js/components/report_constructor/report_constructor.vue
@@ -1,20 +1,55 @@
 <template>
   <div class="row">
-    <div class="col-md-3">
-
-    </div>
-    <div class="col-md-9">
-      <div class="row">
-
+    <div class="col">
+      <div class="left-section bg-dark">
+        <section class="">
+          LEFT MENU
+        </section>
       </div>
-      <div class="row">
-
+    </div>
+    <div class="section-main">
+      <div class="col">
+        <div class="row">
+          <section class="section-top  bg-dark">Right TOP</section>
+        </div>
+      </div>
+      <div class="col">
+        <div class="row">
+          <section class="section-content bg-dark">
+            RIGHT MAIN
+          </section>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
+body {
+  color: white;
+}
+
+.left-section {
+  width: 441px;
+}
+
+.section-main {
+  width: -webkit-fill-available;
+  max-width: calc(100% - 500px);
+
+  .section-top {
+    display: table-cell;
+  }
+
+  .section-content {
+    display: table-cell;
+  }
+}
+
+.row {
+  display: flex;
+  color: white;
+}
 
 </style>
 
