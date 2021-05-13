906423cfe730fdc90cc9e90d92c63a0ade131e43
Константин <aynkz@mail.ru>
2021-05-13 18:13:49 +0600

    fix change symbol 2

--- app/Console/Commands/HiveDataFromAvoset.php
+++ app/Console/Commands/HiveDataFromAvoset.php
@@ -27,8 +27,7 @@ class HiveDataFromAvoset extends Command
      * Create a new command instance.
      *
      * @return void
-     */
-    
+     */    
     public function __construct()
     {
         parent::__construct();
