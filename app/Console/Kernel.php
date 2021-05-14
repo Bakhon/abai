f6067a9dd570980a9f6f148e5a1844096b55bb8c
Константин <aynkz@mail.ru>
2021-05-14 19:18:15 +0600

    update fields and schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,8 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('hive-data-from-avocet:cron')->dailyAt('08:10')->timezone('Asia/Almaty');
-        $schedule->command('hive-data-from-avocet:cron')->dailyAt('19:00')->timezone('Asia/Almaty');
+        $schedule->command('hive-data-from-avocet:cron')->dailyAt('08:10')->timezone('Asia/Almaty');       
         $schedule->command('parse-usd:cron')->dailyAt('16:20')->timezone('Asia/Almaty');
         $schedule->command('parse-usd:cron')->dailyAt('18:30')->timezone('Asia/Almaty');
         $schedule->command('parse-oil:cron')->dailyAt('08:10')->timezone('Asia/Almaty');
