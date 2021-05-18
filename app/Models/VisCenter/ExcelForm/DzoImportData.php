f05c006a86c1d6664d80644e1a626f6ec4217a07
Serhii Suhal <vio3273@gmail.com>
2021-05-18 08:59:23 +0200

    rebuild after merge conflict

--- app/Models/VisCenter/ExcelForm/DzoImportData.php
+++ app/Models/VisCenter/ExcelForm/DzoImportData.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class DzoImportData extends Model
 {
     public $timestamps = false;
+    protected $fillable = ['oil_production_fact','oil_delivery_fact','dzo_name','date','condensate_production_fact','condensate_delivery_fact'];
 
     public function importField()
     {
