2be6b140124dfd3101381605420065f715b34a6e
akhmedinov <azat_ahmedinov@mail.ru>
2021-05-19 23:14:33 +0600

    create raspmestor table, eco_refs_companies_ids table edited

---
+++ app/Models/EconomyKenzhe/RaspMestor.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\EconomyKenzhe;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RaspMestor extends Model
+{
+    //
+}
---
+++ database/migrations/2021_05_19_230439_update_eco_refs_companoes_ids_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateEcoRefsCompanoesIdsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('eco_refs_companies_ids', function (Blueprint $table) {
+            $table->string('num')->after('id');
+            $table->integer('level')->after('name');
+            $table->integer('parent_id')->after('level');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('eco_refs_companies_ids', function (Blueprint $table) {
+            $table->dropColumn(['num']);
+            $table->dropColumn(['level']);
+            $table->dropColumn(['parent_id']);
+        });
+    }
+}
