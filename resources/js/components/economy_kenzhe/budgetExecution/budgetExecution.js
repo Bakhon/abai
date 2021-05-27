d231fcb285cebf2def8f27b90b9dfd91d47f6c6c
Константин <aynkz@mail.ru>
2021-05-28 00:58:00 +0600

    fixing problems

--- resources/js/components/economy_kenzhe/budgetExecution/budgetExecution.js
+++ resources/js/components/economy_kenzhe/budgetExecution/budgetExecution.js
@@ -67,82 +67,119 @@ export default {
         .then(response => {
         if (response.data) {
           let
-            dataPlan = 0.00, dataFact = 0.00, dataFactPrevYear = 0.00, plan2020 = 0.00,
-            spendingPlan = 0.00, spendingFact = 0.00, spendingFactPrevYear = 0.00, spendingPlan2020 = 0.00,
-            costPlan = 0.00, costFact = 0.00, costFactPrevYear = 0.00, costPlan2020 = 0.00,
-            rlzSpendingPlan = 0.00, rlzSpendingFact = 0.00, rlzSpendingPrevYear = 0.00, rlzSpendingPlan2020 = 0.00,
-            admSpendingPlan = 0.00, admSpendingFact = 0.00, admSpendingFactPrevYear = 0.00, admSpendingPlan2020 = 0.00,
-            ebitdaMarginPlan = 0.00, ebitdaMarginFact = 0.00, ebitdaMarginFactPrevYear = 0.00, ebitdaMarginPlan2020 = 0.00,
-            ebitdaPlan = 0.00, ebitdaFact = 0.00, ebitdaFactPrevYear = 0.00, ebitdaPlan2020 = 0.00,
-            netProfitPlan = 0.00, netProfitFact = 0.00, netProfitFactPrevYear = 0.00, netProfitPlan2020 = 0.00,
-            capitalInvPlan = 0.00, capitalInvFact = 0.00, capitalInvFactPrevYear = 0.00, capitalInvPlan2020 = 0.00,
-            cashFlowPlan = 0.00, cashFlowFact = 0.00, cashFlowFactPrevYear = 0.00, cashFlowPlan2020 = 0.00,
-            kursPlan = 0.00, kursFact = 0.00, kursPrevYear = 0.00, kursPlan2020 = 0.00,
-            oilPricePlan = 0.00, oilPriceFact = 0.00, oilPricePrevYear = 0.00, oilPricePlan2020 = 0.00;
+            dataPlan = 0.00, 
+            dataFact = 0.00, 
+            dataFactPrevYear = 0.00, 
+            plan2020 = 0.00,
+            spendingPlan = 0.00, 
+            spendingFact = 0.00, 
+            spendingFactPrevYear = 0.00, 
+            spendingPlan2020 = 0.00,
+            costPlan = 0.00, 
+            costFact = 0.00, 
+            costFactPrevYear = 0.00, 
+            costPlan2020 = 0.00,
+            rlzSpendingPlan = 0.00, 
+            rlzSpendingFact = 0.00, 
+            rlzSpendingPrevYear = 0.00, 
+            rlzSpendingPlan2020 = 0.00,
+            admSpendingPlan = 0.00, 
+            admSpendingFact = 0.00, 
+            admSpendingFactPrevYear = 0.00, 
+            admSpendingPlan2020 = 0.00,
+            ebitdaMarginPlan = 0.00, 
+            ebitdaMarginFact = 0.00, 
+            ebitdaMarginFactPrevYear = 0.00, 
+            ebitdaMarginPlan2020 = 0.00,
+            ebitdaPlan = 0.00, ebitdaFact = 0.00, 
+            ebitdaFactPrevYear = 0.00, 
+            ebitdaPlan2020 = 0.00,
+            netProfitPlan = 0.00, 
+            netProfitFact = 0.00, 
+            netProfitFactPrevYear = 0.00, 
+            netProfitPlan2020 = 0.00,
+            capitalInvPlan = 0.00, 
+            capitalInvFact = 0.00, 
+            capitalInvFactPrevYear = 0.00, 
+            capitalInvPlan2020 = 0.00,
+            cashFlowPlan = 0.00, 
+            cashFlowFact = 0.00, 
+            cashFlowFactPrevYear = 0.00, 
+            cashFlowPlan2020 = 0.00,
+            kursPlan = 0.00, 
+            kursFact = 0.00, 
+            kursPrevYear = 0.00, 
+            kursPlan2020 = 0.00,
+            oilPricePlan = 0.00, 
+            oilPriceFact = 0.00, 
+            oilPricePrevYear = 0.00, 
+            oilPricePlan2020 = 0.00;
 
           _.forEach(response.data['dzoDataActual'], (item) => {
-            dataPlan += item.main_prc_val_plan;
-            dataFact += item.main_prc_val_fact;
+            
+              dataPlan += item.main_prc_val_plan;
+              dataFact += item.main_prc_val_fact;
 
-            spendingPlan += item.spending_val_plan;
-            spendingFact += item.spending_val_fact;
+              spendingPlan += item.spending_val_plan;
+              spendingFact += item.spending_val_fact;
 
-            costPlan += item.cost_val_plan;
-            costFact += item.cost_val_fact;
+              costPlan += item.cost_val_plan;
+              costFact += item.cost_val_fact;
 
-            rlzSpendingPlan += item.rlz_spending_val_plan;
-            rlzSpendingFact += item.rlz_spending_val_fact;
+              rlzSpendingPlan += item.rlz_spending_val_plan;
+              rlzSpendingFact += item.rlz_spending_val_fact;
 
-            admSpendingPlan += item.adm_spending_val_plan;
-            admSpendingFact += item.adm_spending_val_fact;
+              admSpendingPlan += item.adm_spending_val_plan;
+              admSpendingFact += item.adm_spending_val_fact;
 
-            ebitdaMarginPlan += item.editba_margin_val_plan;
-            ebitdaMarginFact += item.editba_margin_val_fact;
+              ebitdaMarginPlan += item.editba_margin_val_plan;
+              ebitdaMarginFact += item.editba_margin_val_fact;
 
-            ebitdaPlan += item.editba_val_plan;
-            ebitdaFact += item.editba_val_fact;
+              ebitdaPlan += item.editba_val_plan;
+              ebitdaFact += item.editba_val_fact;
 
-            netProfitPlan += item.net_profit_val_plan;
-            netProfitFact += item.net_profit_val_fact;
+              netProfitPlan += item.net_profit_val_plan;
+              netProfitFact += item.net_profit_val_fact;
 
-            capitalInvPlan += item.capital_inv_val_plan;
-            capitalInvFact += item.capital_inv_val_fact;
+              capitalInvPlan += item.capital_inv_val_plan;
+              capitalInvFact += item.capital_inv_val_fact;
 
-            cashFlowPlan += item.cash_flow_val_plan;
-            cashFlowFact += item.cash_flow_val_fact;
+              cashFlowPlan += item.cash_flow_val_plan;
+              cashFlowFact += item.cash_flow_val_fact;
 
-            kursPlan = item.kurs_plan;
-            kursFact = item.kurs_fact;
+              kursPlan = item.kurs_plan;
+              kursFact = item.kurs_fact;
 
-            oilPricePlan = item.oil_price_plan;
-            oilPriceFact = item.oil_price_fact;
+              oilPricePlan = item.oil_price_plan;
+              oilPriceFact = item.oil_price_fact;
 
-            plan2020 = item.main_prc_plan_2020;
-            spendingPlan2020 = item.spending_plan_2020;
-            costPlan2020 = item.cost_plan_2020;
-            admSpendingPlan2020 = item.adm_spending_plan_2020;
-            rlzSpendingPlan2020 = item.rlz_spending_plan_2020;
-            ebitdaMarginPlan2020 = item.editba_margin_plan_2020;
-            ebitdaPlan2020 = item.editba_plan_2020;
-            netProfitPlan2020 = item.net_profit_plan_2020;
-            capitalInvPlan2020 = item.capital_inv_plan_2020;
-            cashFlowPlan2020 = item.cash_flow_plan_2020;
-            kursPlan2020 = item.kurs_plan_2020;
-            oilPricePlan2020 = item.oil_price_plan_2020;
+              plan2020 = item.main_prc_plan_2020;
+              spendingPlan2020 = item.spending_plan_2020;
+              costPlan2020 = item.cost_plan_2020;
+              admSpendingPlan2020 = item.adm_spending_plan_2020;
+              rlzSpendingPlan2020 = item.rlz_spending_plan_2020;
+              ebitdaMarginPlan2020 = item.editba_margin_plan_2020;
+              ebitdaPlan2020 = item.editba_plan_2020;
+              netProfitPlan2020 = item.net_profit_plan_2020;
+              capitalInvPlan2020 = item.capital_inv_plan_2020;
+              cashFlowPlan2020 = item.cash_flow_plan_2020;
+              kursPlan2020 = item.kurs_plan_2020;
+              oilPricePlan2020 = item.oil_price_plan_2020;
           });
           _.forEach(response.data['dzoDataPrevYear'], (item) => {
-            dataFactPrevYear += item.main_prc_val_fact;
-            spendingFactPrevYear += item.spending_val_fact;
-            costFactPrevYear += item.cost_val_fact;
-            rlzSpendingPrevYear += item.rlz_spending_val_fact;
-            admSpendingFactPrevYear += item.adm_spending_val_fact;
-            ebitdaFactPrevYear += item.editba_val_fact;
-            ebitdaMarginFactPrevYear += item.editba_margin_val_fact;
-            netProfitFactPrevYear += item.net_profit_val_fact;
-            capitalInvFactPrevYear += item.capital_inv_val_fact;
-            cashFlowFactPrevYear += item.cash_flow_val_fact;
-            kursPrevYear = item.kurs_fact;
-            oilPricePrevYear = item.oil_price_fact;
+
+              dataFactPrevYear += item.main_prc_val_fact;
+              spendingFactPrevYear += item.spending_val_fact;
+              costFactPrevYear += item.cost_val_fact;
+              rlzSpendingPrevYear += item.rlz_spending_val_fact;
+              admSpendingFactPrevYear += item.adm_spending_val_fact;
+              ebitdaFactPrevYear += item.editba_val_fact;
+              ebitdaMarginFactPrevYear += item.editba_margin_val_fact;
+              netProfitFactPrevYear += item.net_profit_val_fact;
+              capitalInvFactPrevYear += item.capital_inv_val_fact;
+              cashFlowFactPrevYear += item.cash_flow_val_fact;
+              kursPrevYear = item.kurs_fact;
+              oilPricePrevYear = item.oil_price_fact;
           });
           this.dzoData.push({
             title: this.trans('economy_be.secondTable.operatingIncome'),
@@ -260,7 +297,7 @@ export default {
             dataFact: oilPriceFact,
             dataFactPrevYear: oilPricePrevYear,
             plan2020: oilPricePlan2020,
-          });
+          }); console.log(this.macroData);
           this.dzoData = this.dzoData.map((item) => {
             return {
               title: item.title,
@@ -271,10 +308,7 @@ export default {
               plan2020: item.plan2020 / item.divider,
             }
           });
-        } else {
-          console.log("No data");
         }
-
       })
     },
   },
