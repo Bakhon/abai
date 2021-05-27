e716101709cae957b6a4c75adfc012c9b025b0d5
Константин <aynkz@mail.ru>
2021-05-27 13:05:38 +0600

    update reptt

--- resources/js/components/economy_kenzhe/proactiveFactors/repttCompany/reptt_company2.vue
+++ resources/js/components/economy_kenzhe/proactiveFactors/repttCompany/reptt_company2.vue
@@ -4,9 +4,9 @@
       header-row-class-name="reptt-header"
       :data="dataReptt.reptt"
       :tree-props="defaultProps"
-      style="width: 100%"
+      style="width: 100%;"
       row-key="id"
-      border
+      border    
       :cell-class-name="getChangedColumnClass"
       :header-cell-class-name="getChangedColumnClass"
       :row-class-name="hideEmptyValues"
@@ -14,34 +14,22 @@
     >
       <el-table-column
         prop="name"
-        label="Наименование"
-        :min-width="12"
+        :label="this.trans('economy_pf.repttTable.name')"
+        :min-width="15"
+        :key="Math.random()" 
+      >
+      </el-table-column>
+      <el-table-column
+        prop="unit"
+        :label="this.trans('economy_pf.repttTable.unit')"
+        :min-width="5"
         :key="Math.random()"
       >
       </el-table-column>
-
-      <el-table-column
-        prop="plan_value"
-        label="План на январь - март"
-        :min-width="8"
-        :key="Math.random()"
-      >        <template slot-scope="scope" v-if="scope.row">
-          {{ formatter(scope.row.intermediate_plan_value[previousYear]) }}
-        </template>
-      </el-table-column>
       <el-table-column
         prop="fact_value"
-        label="Факт за январь - март"
-        :min-width="8"
-        :key="Math.random()"
-      >        <template slot-scope="scope" v-if="scope.row">
-          {{ formatter(scope.row.intermediate_fact_value[previousYear]) }}
-        </template>
-      </el-table-column>
-      <el-table-column
-        prop="fact_value"
-        label="Откл. абс"
-        :min-width="8"
+        :label="col3Reptt"
+        :min-width="10"
         :key="Math.random()"
       >
         <template slot-scope="scope" v-if="scope.row">
@@ -50,8 +38,8 @@
       </el-table-column>
       <el-table-column
         prop="plan_value"
-        label="Откл. %"
-        :min-width="8"
+        :label="col4Reptt"
+        :min-width="10"
         :key="Math.random()"
       >
         <template slot-scope="scope" v-if="scope.row">
@@ -60,8 +48,8 @@
       </el-table-column>
       <el-table-column
         prop="fact_value"
-        label="Прогноз"
-        :min-width="8"
+        :label="col5Reptt"
+        :min-width="10"
         :key="Math.random()"
       >
         <template slot-scope="scope" v-if="scope.row">
@@ -70,55 +58,41 @@
       </el-table-column>
       <el-table-column
         prop="plan_value"
-        label="Откл. абс. от утв. плана"
-        :min-width="8"
+        :label="col6Reptt"
+        :min-width="10"
         :key="Math.random()"
       >
         <template slot-scope="scope" v-if="scope.row">
-          {{ formatter(scope.row.intermediate_plan_value[currentYear]) }}
+          {{formatter( scope.row.intermediate_plan_value[currentYear] )}}
         </template>
       </el-table-column>
       <el-table-column
         prop="fact_value"
-        label="Откл. % от утв. плана"
-        :min-width="8"
+        :label="col7Reptt"
+        :min-width="10"
         :key="Math.random()"
       >
         <template slot-scope="scope" v-if="scope.row">
-          {{ formatter(scope.row.intermediate_fact_value[currentYear]) }}
+          {{formatter( scope.row.intermediate_fact_value[currentYear]) }}
         </template>
       </el-table-column>
-      <el-table-column
-        prop="fact_value"
-        label="Прогноз"
-        :min-width="8"
-        :key="Math.random()"
-      >
-        <template slot-scope="scope" v-if="scope.row">
-          {{ formatter(scope.row.plan_value[currentYear]) }}
-        </template>
-      </el-table-column>
-      <el-table-column :label="col8Reptt" :min-width="8">
+      <el-table-column :label="col8Reptt" :min-width="10">
         <template slot-scope="scope" v-if="scope.row">
           {{
-            formatter(
-              getAbsoluteDeviation(
-                scope.row.plan_value[currentYear],
-                scope.row.fact_value[currentYear]
-              )
-            )
+           formatter( getAbsoluteDeviation(
+              scope.row.plan_value[currentYear],
+              scope.row.fact_value[currentYear]
+            ))
           }}
         </template>
       </el-table-column>
-      <el-table-column :label="col9Reptt" :min-width="8">
+      <el-table-column :label="col9Reptt" :min-width="10">
         <template slot-scope="scope" v-if="scope.row">
           {{
-            formatter(
-              getRelativeDeviation(
-                scope.row.plan_value[currentYear],
-                scope.row.fact_value[currentYear]
-              )
-            )
+           formatter( getRelativeDeviation(
+              scope.row.plan_value[currentYear],
+              scope.row.fact_value[currentYear]
+            ))
           }}
         </template>
       </el-table-column>
