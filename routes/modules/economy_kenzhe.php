9c8320c0e21c53f20935cd88ca09a31e0e441f24
Константин <aynkz@mail.ru>
2021-05-28 10:06:37 +0600

    update  _ на -

--- routes/modules/economy_kenzhe.php
+++ routes/modules/economy_kenzhe.php
@@ -8,7 +8,7 @@ Route::group(
                 Route::get('/', 'EconomyKenzhe\MainController@index');
                 Route::get('/company/', 'EconomyKenzhe\MainController@company')->name('company');
                 Route::get('/companies', 'EconomyKenzhe\MainController@companies');
-                Route::get('/field_calculation', 'EconomyKenzhe\FieldCalcController@index');
+                Route::get('/field-calculation', 'EconomyKenzhe\FieldCalcController@index');
                 Route::match(['GET', 'POST'], '/import_rep', 'EconomyKenzhe\ImportController@importRepTtValues')->name('import_rep');
                 Route::match(['GET', 'POST'], '/import_reptt_titles', 'EconomyKenzhe\ImportController@importRepTtTitlesTree')->name('import_reptt_titles');
                 Route::get('/proactive-factors', 'EconomyKenzhe\proactiveFactorsController@proactiveFactors')->name('proactiveFactors');    
