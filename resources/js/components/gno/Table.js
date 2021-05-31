99ad5956cb02f020139300c6c5521b3d7e252a01
ZS <zh.sundetov@kazdream.kz>
2021-05-31 21:01:09 +0000

    fixing bugs

--- resources/js/components/gno/Table.js
+++ resources/js/components/gno/Table.js
@@ -345,7 +345,7 @@ export default {
       inflowCurveTitle: this.trans('pgno.krivaya_pritoka'),
       podborGnoTitle: this.trans('pgno.podbor_gno'),
       serviceOffline: false,
-      noneCentrators: true,
+      isCentrators: false,
     };
 
   },
@@ -393,16 +393,6 @@ export default {
     if (this.windowWidth <= 1300 && this.windowWidth > 991) {
       this.activeRightTabName = 'devices';
     }
-  },
-  computed: {
-
-    wellNum() {
-      return this.$store.state.wellNum
-    },
-    wellType() {
-      return this.$store.state.wellType
-    },
-    ...mapState(['wells'])
   },
   methods: {
     setHpumpValueFromIncl() {
@@ -1514,7 +1504,7 @@ export default {
     },
 
     onPgnoClick() {
-      this.noneCentrators = false;
+      this.isCentrators = true;
       if(this.qlPot * 1 < this.qlCelValue.split(' ')[0] * 1 && this.CelButton == 'ql'){
         this.$notify({
           message: this.trans('pgno.notify_cel_rezhim_more_perf'),
--- resources/js/components/gno/Table.vue
+++ resources/js/components/gno/Table.vue
@@ -123,7 +123,7 @@
               <div class="construction-data hperf table-border-gno-top no-gutter col-7">
                 {{trans('pgno.glubina_perf')}}
                 <template>
-                  <div v-if="noneCentrators"></div>
+                  <div v-if="!isCentrators"></div>
                   <div class="descr" v-else>
                     Центраторы: <b v-for="item in centratorsRequiredValue">{{ item }}</b>
                   </div>
@@ -2027,6 +2027,21 @@
 <script src="./Table.js"></script>
 
 <style scoped>
+.descr{
+    font-size: 12px;
+    display: none;
+    position:absolute;
+    top: -85px;
+    left: 140px;
+    width: 190px;
+    padding-left: 3px;
+    background:#494aa5;
+    border-radius: 4px;
+    height: auto;
+    top: -85px;
+    z-index:9999;
+    box-shadow: 0 0 5px black ;
+}
 
 .hperf:hover .descr{
     font-size: 12px;
