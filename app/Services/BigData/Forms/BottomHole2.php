457802646f95282ca063347b08f7321cb3d5d6cf
Ayana Adilova <aadilova19@gmail.com>
2021-06-03 15:56:29 +0600

    new form bottom hole type 2

---
+++ app/Services/BigData/Forms/BottomHole2.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\BigData\Forms;
+
+class BottomHole2 extends PlainForm
+{
+    protected $configurationFileName = 'bottom_hole2';
+}
--- config/bigdata_forms.php
+++ config/bigdata_forms.php
@@ -13,4 +13,5 @@
   'well_constr' => '\\App\\Services\\BigData\\Forms\\WellConstr',
   'tech_state' => '\\App\\Services\\BigData\\Forms\\TechState',
   'well_block' => '\\App\\Services\\BigData\\Forms\\WellBlock',
+  'bottom_hole2' => '\\App\\Services\\BigData\\Forms\\BottomHole2',
 );
