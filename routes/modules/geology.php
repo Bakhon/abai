f57cf28ab41bb718e9fac5afba8a3ee9066485d4
akbayk <akuderbekov@nu.edu.kz>
2021-06-07 16:40:57 +0600

    middlware add

--- routes/modules/geology.php
+++ routes/modules/geology.php
@@ -2,7 +2,7 @@
 Route::group(
     ['prefix' => App\Http\Middleware\LocaleMiddleware::getLocale()],
     function () {
-        Route::group(['prefix' => 'geology', 'namespace'=>'Geology'], function () {
+        Route::group(['middleware' => 'auth', 'prefix' => 'geology', 'namespace'=>'Geology'], function () {
             Route::resource('gis', 'GisController');
 ;                   Route::resource('core', 'CoreController');
         });
