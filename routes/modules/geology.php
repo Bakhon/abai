301ea31d7a6addc4728d06406931c4ec93d5f18e
akbayk <akuderbekov@nu.edu.kz>
2021-06-07 16:39:14 +0600

    transfer route to modules folder

---
+++ routes/modules/geology.php
@@ -0,0 +1,10 @@
+<?php
+Route::group(
+    ['prefix' => App\Http\Middleware\LocaleMiddleware::getLocale()],
+    function () {
+        Route::group(['prefix' => 'geology', 'namespace'=>'Geology'], function () {
+            Route::resource('gis', 'GisController');
+;                   Route::resource('core', 'CoreController');
+        });
+    }
+);
