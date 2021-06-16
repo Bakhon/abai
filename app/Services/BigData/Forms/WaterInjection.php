ed8eb33d2afd7f25763c56aaebb4981eee582412
Ayana Adilova <aadilova19@gmail.com>
2021-06-16 20:15:45 +0600

    new form

---
+++ app/Services/BigData/Forms/WaterInjection.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\BigData\Forms;
+
+class WaterInjection extends TableForm
+{
+    protected $configurationFileName = 'water_injection';
+}
