f3ddd13a028543c31a880f4d5d9154285584114f
Константин <aynkz@mail.ru>
2021-06-17 10:32:47 +0600

    dzoDataActual and dzoDataPrevYear delete

--- resources/js/components/economy_kenzhe/budgetExecution/budgetExecution.js
+++ resources/js/components/economy_kenzhe/budgetExecution/budgetExecution.js
@@ -3,9 +3,7 @@ import moment from "moment";
 
 export default {
   data: function () {
-    return {
-      dzoDataActual: dzoDataActual,
-      dzoDataPrevYear: dzoDataPrevYear,
+    return {   
       params:{differenceBetweenMonths: "12", company: "7", reload: true},
       numbersOfMonths: [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],  
       repttData: "0",
