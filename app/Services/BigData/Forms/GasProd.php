ed1dd872c6d01bb0643e5653d508b8cb77f95a8c
Ayana Adilova <aadilova19@gmail.com>
2021-06-21 09:30:44 +0600

    commit

--- app/Services/BigData/Forms/GasProd.php
+++ app/Services/BigData/Forms/GasProd.php
@@ -3,8 +3,95 @@
 declare(strict_types=1);
 
 namespace App\Services\BigData\Forms;
+use App\Models\BigData\Dictionaries\Tech;
+use App\Models\BigData\Well;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class GasProd extends TableForm
 {
     protected $configurationFileName = 'gas_prod';
+    public function getRows(array $params = [])
+    {
+        $tech = Tech::find($this->request->get('tech'));
+
+        $wellsQuery = Well::query()
+            ->with('techs', 'geo')
+            ->select('id', 'uwi')
+            ->orderBy('uwi')
+            ->active(Carbon::parse($this->request->get('date')))
+            ->whereHas(
+                'techs',
+                function ($query) use ($tech) {
+                    return $query
+                        ->where('dict.tech.id', $tech->id)
+                        ->whereDate('dict.tech.dbeg', '<=', $this->request->get('date'))
+                        ->whereDate('dict.tech.dend', '>=', $this->request->get('date'));
+                }
+            );
+
+        if (isset($params['filter']['row_id'])) {
+            $wellsQuery->where('id', $params['filter']['row_id']);
+        }
+
+        $wells = $wellsQuery->get();
+
+        $tables = $this->getFields()->pluck('table')->filter()->unique();
+        $rowData = $this->fetchRowData(
+            $tables,
+            $wells->pluck('id')->toArray(),
+            Carbon::parse($this->request->get('date'))
+        );
+
+        $wells->transform(
+            function ($item) use ($rowData) {
+                $result = [];
+
+                foreach ($this->getFields() as $field) {
+                    $fieldValue = $this->getFieldValue($field, $rowData, $item);
+                    if (!empty($fieldValue)) {
+                        $result[$field['code']] = $fieldValue;
+                    }
+                }
+                return $result;
+            }
+        );
+
+        $this->addLimits($wells);
+
+        return $wells->toArray();
+    }
+    protected function saveSingleFieldInDB(string $field, int $wellId, Carbon $date, $value): void
+    {
+        $column = $this->getFieldByCode($field);
+
+        $item = $this->getFieldRow($column, $wellId, $date);
+
+        if (empty($item)) {
+            $data = [
+                'well' => $wellId,
+                $column['column'] => $value,
+                'dbeg' => $date->toDateTimeString()
+            ];
+
+            if (!empty($column['additional_filter'])) {
+                foreach ($column['additional_filter'] as $key => $val) {
+                    $data[$key] = $val;
+                }
+            }
+
+            DB::connection('tbd')
+                ->table($column['table'])
+                ->insert($data);
+        } else {
+            DB::connection('tbd')
+                ->table($column['table'])
+                ->where('id', $item->id)
+                ->update(
+                    [
+                        $column['column'] => $value
+                    ]
+                );
+        }
+    }
 }
