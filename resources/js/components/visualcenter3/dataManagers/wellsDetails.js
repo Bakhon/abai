6772517b56092148c809ea9d59f6187f0d3bc07a
Serhii Suhal <vio3273@gmail.com>
2021-06-29 06:47:03 +0200

    extract fond list to json

--- resources/js/components/visualcenter3/dataManagers/wellsDetails.js
+++ resources/js/components/visualcenter3/dataManagers/wellsDetails.js
@@ -1,69 +1,10 @@
 import moment from "moment";
+import fondsMapping from '../founds_mapping.json';
 
 export default {
     data: function () {
         return {
-            fondList: {
-                production:  {
-                    'work': [
-                        'operating_production_fond',
-                        'active_production_fond',
-                        'inactive_production_fond',
-                        'developing_production_fond',
-                        'pending_liquidation_production_fond',
-                    ],
-                    'idle': [
-                        'prs_wait_downtime_production_wells_count',
-                        'prs_downtime_production_wells_count',
-                        'krs_wait_downtime_production_wells_count',
-                        'krs_downtime_production_wells_count',
-                        'well_survey_downtime_production_wells_count',
-                        'unprofitable_downtime_production_wells_count',
-                        'other_downtime_production_wells_count'
-                    ]
-                },
-                injection:  {
-                    'work': [
-                        'operating_injection_fond',
-                        'active_injection_fond',
-                        'inactive_injection_fond',
-                        'developing_injection_fond',
-                        'pending_liquidation_injection_fond',
-                    ],
-                    'idle': [
-                        'prs_wait_downtime_injection_wells_count',
-                        'prs_downtime_injection_wells_count',
-                        'krs_wait_downtime_injection_wells_count',
-                        'krs_downtime_injection_wells_count',
-                        'well_survey_downtime_injection_wells_count',
-                        'other_downtime_injection_wells_count'
-                    ]
-                },
-                other: [
-                    'glut_downtime_production_wells_count',
-                    'impulse_replacement_downtime_production_wells_count',
-                    'electrical_part_downtime_production_wells_count',
-                    'ground_repair_downtime_production_wells_count',
-                    'periodic_downtime_production_wells_count',
-                    'production_restriction_downtime_production_wells_count',
-                    'well_treatment_downtime_production_wells_count',
-                    'highly_watered_downtime_production_wells_count',
-                    'limited_download_downtime_production_wells_count',
-                    'profile_alignment_downtime_production_wells_count',
-                    'coiltubing_downtime_production_wells_count',
-                    'chrf_restriction_downtime_production_wells_count',
-                    'drilling_restriction_downtime_production_wells_count',
-                    'waiting_pump_downtime_production_wells_count',
-                    'waiting_swabbing_downtime_production_wells_count',
-                    'regulate_stopped_downtime_production_wells_count',
-                    'waiting_ppr_downtime_production_wells_count',
-                    'impact_prs_downtime_production_wells_count',
-                    'mkd_stop_downtime_production_wells_count',
-                    'technological_downtime_downtime_production_wells_count',
-                    'pns_production_wells_count',
-                    'vns_production_wells_count',
-                ]
-            },
+            fondList: fondsMapping,
             fondsFilter: {
                 'isProductionIdleActive': false,
                 'isInjectionIdleActive': false,
