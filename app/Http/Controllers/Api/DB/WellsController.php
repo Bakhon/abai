1dc04bd317f284c546c7dd87077bf33b32564bb1
IlyasDossanov <i.dossanov@kmg.kz>
2021-06-29 13:09:19 +0600

    delete unused statement

--- app/Http/Controllers/Api/DB/WellsController.php
+++ app/Http/Controllers/Api/DB/WellsController.php
@@ -11,8 +11,6 @@ use App\Models\BigData\Well;
 use App\Services\BigData\StructureService;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Exception;
-
 
 class WellsController extends Controller
 {
