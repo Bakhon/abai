db3ab7fbd57f7b3e3addda445f5e9da18c46feb2
Serhii Suhal <vio3273@gmail.com>
2021-07-02 09:51:51 +0200

    change table view to new design

--- resources/js/components/visualcenter3/dailyReport/index.vue
+++ resources/js/components/visualcenter3/dailyReport/index.vue
@@ -1,57 +1,92 @@
 <template>
     <div class="page-wrapper">
         <div class="page-container row">
-            <div class="col-12 header-title mt-2">
-                Добыча нефти и конденсата, тонн
+            <div class="col-12 mt-2 d-flex">
+                <div class="header-title col-9">
+                    <transition name="fade">
+                        <div v-if="buttonName === viewType.production">
+                            Оперативная суточная информация по добыче нефти и конденсата АО НК "КазМунайГаз", тонн
+                        </div>
+                    </transition>
+                    <transition name="fade">
+                        <div v-if="buttonName !== viewType.production">
+                            Оперативная суточная информация по сдачи нефти и конденсата АО НК "КазМунайГаз", тонн
+                        </div>
+                    </transition>
+                </div>
+                <div
+                        class="opec-filter-disabled col-1 mt-2"
+                        @click="switchView()"
+                >
+                    {{buttonName}}
+                </div>
+                <div
+                        :class="[isOpecActive ? 'opec-filter-active' : 'opec-filter-disabled','col-1 mt-2']"
+                        @click="isOpecActive = !isOpecActive"
+                >
+                    С учетом ОПЕК+
+                </div>
             </div>
-            <div class="col-10 mt-2 p-0">
-                <table>
+            <div class="col-12 mt-2">
+                <table class="col-12">
                     <tr>
                         <th rowspan="2">№ п/п</th>
                         <th rowspan="2">Предприятия</th>
                         <th rowspan="2">
-                            План<br>
-                            на {{currentYear}} г.
+                            План на {{currentYear}} г.
                         </th>
-                        <th rowspan="2">
-                            План<br>
-                            на {{currentMonthName}} месяц
+                        <th :class="!isOpecActive ? '' : 'hide-block'"rowspan="2">
+                            План на {{currentMonthName}} месяц
                         </th>
-                        <th rowspan="2">
+                        <th :class="isOpecActive ? '' : 'hide-block'" rowspan="2">
                             План на {{currentMonthName}} месяц <br>
                             с учетом ОПЕК+
                         </th>
-                        <th colspan="5" class="background-delimeters">СУТОЧНАЯ</th> <!-- >суточная <-->
-                        <th colspan="5" class="background-delimeters">С НАЧАЛА МЕСЯЦА</th> <!-- >с начала месяца <-->
+                        <th colspan="3" class="background-delimeters">СУТОЧНАЯ</th> <!-- >суточная <-->
+                        <th colspan="3" class="background-delimeters">С НАЧАЛА МЕСЯЦА</th> <!-- >с начала месяца <-->
+                        <th colspan="3" class="background-delimeters">С НАЧАЛА ГОДА</th> <!-- >с начала года <-->
                     </tr>
                     <tr>
 
                         <!-- >суточная <-->
-                        <th>План</th>
-                        <th>
+                        <th :class="!isOpecActive ? '' : 'hide-block'">План</th>
+                        <th :class="isOpecActive ? '' : 'hide-block'">
                             План<br>
                             с учетом ОПЕК+
                         </th>
                         <th>Факт</th>
-                        <th>(+,-)</th>
-                        <th>
+                        <th :class="!isOpecActive ? '' : 'hide-block'">(+,-)</th>
+                        <th :class="isOpecActive ? '' : 'hide-block'">
                             (+,-)<br>
                             с учетом ОПЕК+
                         </th>
                         <!-- >суточная <-->
                         <!-- >с начала месяца <-->
-                        <th>План</th>
-                        <th>
+                        <th :class="!isOpecActive ? '' : 'hide-block'">План</th>
+                        <th :class="isOpecActive ? '' : 'hide-block'">
                             План<br>
                             с учетом ОПЕК+
                         </th>
                         <th>Факт</th>
-                        <th>(+,-)</th>
-                        <th>
+                        <th :class="!isOpecActive ? '' : 'hide-block'">(+,-)</th>
+                        <th :class="isOpecActive ? '' : 'hide-block'">
                             (+,-)<br>
                             с учетом ОПЕК+
                         </th>
                         <!-- >с начала месяца <-->
+                        <!-- >с начала года <-->
+                        <th :class="!isOpecActive ? '' : 'hide-block'">План</th>
+                        <th :class="isOpecActive ? '' : 'hide-block'">
+                            План<br>
+                            с учетом ОПЕК+
+                        </th>
+                        <th>Факт</th>
+                        <th :class="!isOpecActive ? '' : 'hide-block'">(+,-)</th>
+                        <th :class="isOpecActive ? '' : 'hide-block'">
+                            (+,-)<br>
+                            с учетом ОПЕК+
+                        </th>
+                        <!-- >с начала года <-->
                     </tr>
                     <!-- >саммари <-->
                     <tr
@@ -63,6 +98,7 @@
                         <td
                                 v-for="(key, keyIndex) in Object.keys(item)"
                                 v-if="keyIndex > 1"
+                                :class="isColumnHidden(keyIndex) ? 'hide-block' : ''"
                         >
                             {{getFormattedNumber(item[key])}}
                         </td>
@@ -78,23 +114,13 @@
                         <td
                             v-for="(key, keyIndex) in Object.keys(item)"
                             v-if="keyIndex > 1"
+                            :class="isColumnHidden(keyIndex) ? 'hide-block' : ''"
                         >
                             {{getFormattedNumber(item[key])}}
                         </td>
                     </tr>
                     <!-- >дзо <-->
                </table>
-           </div>
-           <div class="col-2 mt-2">
-               <div class="visualization-header pt-1">
-                   Визуализация
-               </div>
-               <div class="d-flex visualization-percent">
-                   <span class="col-3">0%</span>
-                   <span class="col-3">30%</span>
-                   <span class="col-3">80%</span>
-                   <span class="col-3">100%</span>
-               </div>
            </div>
        </div>
 
@@ -125,6 +151,11 @@ export default {
                'factByMonth': 21000,
                'differenceByMonth': 1000,
                'differenceOpecByMonth': 3000,
+               'planByYear': 200000,
+               'planOpecByYear': 180000,
+               'factByYear': 210000,
+               'differenceByYear': 10000,
+               'differenceOpecByYear': 30000,
            },
            production: [],
            summary: [
@@ -144,6 +175,11 @@ export default {
                    'factByMonth': 21000,
                    'differenceByMonth': 1000,
                    'differenceOpecByMonth': 3000,
+                   'planByYear': 200000,
+                   'planOpecByYear': 180000,
+                   'factByYear': 210000,
+                   'differenceByYear': 10000,
+                   'differenceOpecByYear': 30000,
                },
                {
                    'number': '',
@@ -161,8 +197,20 @@ export default {
                    'factByMonth': 21000,
                    'differenceByMonth': 1000,
                    'differenceOpecByMonth': 3000,
+                   'planByYear': 200000,
+                   'planOpecByYear': 180000,
+                   'factByYear': 210000,
+                   'differenceByYear': 10000,
+                   'differenceOpecByYear': 30000,
                }
-           ]
+           ],
+           opecColumns: [4,6,9,11,14,16,19],
+           isOpecActive: false,
+           buttonName: 'Добыча',
+           viewType: {
+               'production': 'Добыча',
+               'delivery': 'Сдача'
+           }
        }
    },
    methods: {
@@ -176,11 +224,30 @@ export default {
        getFormattedNumber(num) {
            return (new Intl.NumberFormat("ru-RU").format(Math.round(num)))
        },
+       isColumnHidden(index) {
+           return this.opecColumns.includes(index);
+       },
+       switchOpecFilter() {
+            this.isOpecActive = !this.isOpecActive;
+       },
+       switchView() {
+            if (this.buttonName === this.viewType.production) {
+                this.buttonName = this.viewType.delivery;
+            } else {
+                this.buttonName = this.viewType.production;
+            }
+       },
    },
    mounted() {
-       for (let i=2; i<21; i++) {
+       for (let i=2; i<19; i++) {
            let emptyCompany = _.cloneDeep(this.template);
            emptyCompany.number = i;
+           if (i === 9) {
+               emptyCompany.differenceByDay = -1000;
+           }
+           if (i === 12) {
+               emptyCompany.differenceByDay = 3000;
+           }
            this.production.push(emptyCompany);
        }
    }
@@ -188,30 +255,43 @@ export default {
 </script>
 
 <style scoped lang="scss">
-   .visualization-header {
-       border: 2px solid #272953;
-       background: #3D4473;
-       min-height: 52px;
-       font-style: normal;
-       font-weight: bold;
-       font-size: 14px;
-   }
-   .visualization-percent {
-       border: 2px solid #272953;
-       min-height: 37px;
-       background: #4C537E;
-       span {
-           padding-top: 6px;
-       }
-   }
+    .header-title {
+        font-style: normal;
+        font-weight: bold;
+        font-size: 20px;
+        line-height: 24px;
+    }
+    .opec-filter-active {
+        line-height: 24px;
+        margin-left: auto;
+        border-radius: 10px;
+        background: #3366FF;
+    }
+    .opec-filter-disabled {
+        line-height: 24px;
+        margin-left: auto;
+        border-radius: 10px;
+        background: #4C537E;
+    }
+    div.opec-filter-disabled:hover {
+        -webkit-box-shadow: 2px 1px 33px 4px rgba(28,37,247,0.78);
+        box-shadow: 2px 1px 33px 4px rgba(28,37,247,0.78);
+    }
+    div.opec-filter-active:hover {
+        -webkit-box-shadow: 2px 1px 33px 4px rgba(28,37,247,0.78);
+        box-shadow: 2px 1px 33px 4px rgba(28,37,247,0.78);
+    }
+    .hide-block {
+        display:none;
+    }
    .background-light {
-       background: #3D4473;
+       background: #4C537E;
    }
    .background-dark {
        background: #272953;
    }
    .background-delimeters {
-       background: #4C537E;
+       background: #3D4473;
    }
    .page-container {
        flex-wrap: wrap;
@@ -225,40 +305,57 @@ export default {
        color: white;
        text-align: center;
    }
-   .header-title {
-       font-style: normal;
-       font-weight: bold;
-       font-size: 20px;
-       line-height: 24px;
+   .fade-enter-active, .fade-leave-active {
+       transition: opacity .5s;
+   }
+   .fade-enter, .fade-leave-to {
+       opacity: 0;
    }
    table {
-       min-width: 1530px;
+       table-layout: fixed;
+       border-collapse: collapse;
        tr {
+           &:nth-child(2) {
+               th  {
+                   height: 50px;
+               }
+           }
            &:nth-child(3), &:nth-child(4) {
-               background: #575975;
+               background: #3D4473;
                border-bottom: 2px solid #272953;
+               td {
+                   border-right: 2px solid #272953;
+               }
            }
        }
        th {
            font-style: normal;
            font-weight: bold;
-           font-size: 14px;
-           background: #3D4473;
+           font-size: 13px;
+           background: #353EA1;
            padding: 5px;
            border: 2px solid #272953;
+           width: 10%;
+           &:first-child {
+               width: 2%;
+           }
+           &:nth-child(3),&:nth-child(4),&:nth-child(5) {
+               width: 5%;
+           }
        }
        td {
-           border-right: 2px solid #3D4473;
-           font-size: 15px;
+           font-size: 14px;
            font-family: Bold;
-           min-width: 70px;
            padding: 7px;
+           width: 10%;
            &:first-child {
-               min-width: 50px;
+               width: 2%;
            }
            &:nth-child(2) {
                font-family: HarmoniaSansProCyr-Regular, Harmonia-sans;
-               max-width: 250px;
+           }
+           &:nth-child(3),&:nth-child(4),&:nth-child(5) {
+               width: 5%;
            }
        }
    }
