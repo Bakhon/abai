9d6f8f25fe159fa5dfcfc05ad956bd8f21a31949
Ruslan <russel1987@mail.ru>
2021-07-02 11:27:36 +0300

    created form

---
+++ app/Services/BigData/Forms/WellTechChangeTech.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\BigData\Forms;
+
+class WellTechChangeTech extends PlainForm
+{
+    protected $configurationFileName = 'well_tech_change_tech';
+}
