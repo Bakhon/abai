eb4a7fe11447bfbdd1b3a52aad37d154c68baa09
Ayana Adilova <aadilova19@gmail.com>
2021-07-03 15:35:35 +0600

    commit

--- app/Services/BigData/Forms/WellConstr.php
+++ app/Services/BigData/Forms/WellConstr.php
@@ -7,4 +7,26 @@ namespace App\Services\BigData\Forms;
 class WellConstr extends PlainForm
 {
     protected $configurationFileName = 'well_constr';
+
+    protected function isValidLandingDate($wellId, $landingDate): bool
+    {
+        $startDate =  DB::connection('tbd')
+            ->table('dict.well')
+            ->where('id', $wellId)
+            ->get('drill_start_date');
+
+        ($landingDate >= $startDate) ? true : false;     
+    }
+
+    protected function isValidDepth($depth):bool
+    {
+        $dailyDrill =  DB::connection('tbd')
+            ->table('prod.daily_drill')
+            ->where('id', $wellId)
+            ->get('daily_drill_progress');
+        $data = sum($dailyDrill);
+        ($depth <= $data) ? true : false;  
+    }
+
+    
 }
