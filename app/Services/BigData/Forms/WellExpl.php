3e95e91c8ee4d302038a04158f657b2a8002429d
Ayana Adilova <aadilova19@gmail.com>
2021-07-07 15:40:24 +0600

    commit

--- app/Services/BigData/Forms/WellExpl.php
+++ app/Services/BigData/Forms/WellExpl.php
@@ -7,4 +7,32 @@ namespace App\Services\BigData\Forms;
 class WellExpl extends PlainForm
 {
     protected $configurationFileName = 'well_expl';
+
+
+    private function isValidDate($wellId, $dbeg):bool
+    {
+            $dend = DB::connection('tbd')
+                        ->table('prod.well_expl')
+                        ->where('well', $wellId)
+                        ->orderBy('dend', 'desc')
+                        ->get('dend')
+                        ->first();
+                        
+
+            return $dbeg >= $dend;
+    }
+
+    protected function getCustomValidationErrors(): array
+    {
+        $errors = [];
+
+        if (!$this->isValidDate($this->request->get('well'),$this->request->get('dbeg'))){
+            $errors['dbeg'] = trans('bd.validation.dbeg');
+        }
+
+        return $errors;
+    }
+    
+   
+
 }
