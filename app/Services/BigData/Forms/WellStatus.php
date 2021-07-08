de776595929bf41f98e618aa0ddb9ee19a8d40c0
Ayana Adilova <aadilova19@gmail.com>
2021-07-08 17:48:05 +0600

    commit

--- app/Services/BigData/Forms/WellStatus.php
+++ app/Services/BigData/Forms/WellStatus.php
@@ -12,13 +12,25 @@ class WellStatus extends PlainForm
 
     private function isValidDate($wellId, $dbeg):bool
     {
+            
             $dend = DB::connection('tbd')
                         ->table('prod.well_status')
                         ->where('well', $wellId)
                         ->orderBy('dend', 'desc')
-                        ->get('dend')
-                        ->first();
-            return $dbeg < $dend;
+                        ->get('dend');
+            $collection = collect($dend);
+
+            $collection = $collection->skip(1);
+            // echo(json_encode($collection));
+            // $str = $collection->get('1') . "";
+            // echo date_format($dend[1], 'Y-m-d H:i:s');           
+            // $array = json_decode(str, true);
+            // echo(json_encode($array));
+            // $collection = $collection->get(1);             
+            echo(json_encode($collection->get(1)) . "\n");        
+            echo(json_encode($collection->get('dend')));
+            // echo substr($str, 0, 6), "\n";
+            return $dbeg >= $dend;
     }
 
     protected function getCustomValidationErrors(): array
