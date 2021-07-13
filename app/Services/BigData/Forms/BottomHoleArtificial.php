9b37656af46590b5b59166f67b6f8446681f871b
Ayana Adilova <aadilova19@gmail.com>
2021-07-13 17:19:34 +0600

    commit

--- app/Services/BigData/Forms/BottomHoleArtificial.php
+++ app/Services/BigData/Forms/BottomHoleArtificial.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace App\Services\BigData\Forms;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Traits\DepthValidationTrait;
 
 class BottomHoleArtificial extends PlainForm
 {
     protected $configurationFileName = 'bottom_hole_artificial';
+
+    use DepthValidationTrait;
 
     protected function isValidDate($wellId, $date): bool
     {
@@ -21,25 +24,7 @@ class BottomHoleArtificial extends PlainForm
             return true;
         }
         return $date >= $endDate->drill_end_date;    
-    }
-    function isCorrectSummofDailyDrill($dailyDrill, $depth) : bool {
-                
-        if($dailyDrill != 0){
-            return $depth < $dailyDrill;
-        }
-        return true;
-    }
-
-    protected function isValidDepth($wellId, $depth):bool
-    {
-        $dailyDrill =  DB::connection('tbd')
-            ->table('drill.well_daily_drill')
-            ->where('well', $wellId)
-            ->get('daily_drill_progress')
-            ->sum('daily_drill_progress');
-            
-        return ($this->isCorrectSummofDailyDrill($dailyDrill, $depth)) ? true : false ;
-    }
+    }   
 
         
     protected function getCustomValidationErrors(): array
