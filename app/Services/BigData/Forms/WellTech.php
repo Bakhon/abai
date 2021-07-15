4c3d68b2826ef5e4b607ece018de895aa356c1d6
Ayana Adilova <aadilova19@gmail.com>
2021-07-15 16:01:19 +0600

    commit

--- app/Services/BigData/Forms/WellTech.php
+++ app/Services/BigData/Forms/WellTech.php
@@ -3,8 +3,21 @@
 declare(strict_types=1);
 
 namespace App\Services\BigData\Forms;
+use App\Traits\BigData\Forms\DateMoreThanValidationTrait;
 
 class WellTech extends PlainForm
 {
     protected $configurationFileName = 'well_tech';
+
+    use DateMoreThanValidationTrait;
+    protected function getCustomValidationErrors(): array
+    {
+        $errors = [];
+
+        if (!$this->isValidDate($this->request->get('well'), $this->request->get('dbeg'), 'prod.well_tech' , 'dend')) {
+            $errors[$this->request->get('dbeg')][] = trans('bd.validation.dbeg');
+        }
+
+        return $errors;
+    }
 }
