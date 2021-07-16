bf6103690a0d475eeab485453848302b693d95dd
Nikolai Demin <demin.nikolai@Inbox.ru>
2021-07-16 12:41:14 +0800

    fix create pipe

---
+++ database/migrations/2021_07_16_103548_remove_pipe_coords_references.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemovePipeCoordsReferences extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pipe_coords', function (Blueprint $table) {
+            $table->dropForeign('pipe_coords_map_pipe_id_foreign');
+            $table->dropIndex('pipe_coords_map_pipe_id_foreign');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pipe_coords', function (Blueprint $table) {
+            $table->bigInteger('oil_pipe_id')->unsigned()->change();
+            $table->foreign('oil_pipe_id')->references('id')->on('oil_pipes')->onDelete('cascade');
+        });
+    }
+}
