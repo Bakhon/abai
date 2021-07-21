1a5be9621487226b970bed1b7eb74a8766dfcd74
Kuanysh <k.daniyarov@niikmg.kz>
2021-07-21 13:04:56 +0600

    development/add_maps_plotly

--- resources/js/components/DigitalRating/components/Sections.vue
+++ resources/js/components/DigitalRating/components/Sections.vue
@@ -135,6 +135,7 @@
 import { TransitionExpand } from 'vue-transition-expand';
 import L from 'leaflet';
 import { LMap, LTileLayer, LMarker, LWMSTileLayer, LControlLayers } from 'vue2-leaflet';
+import { Plotly } from "vue-plotly";
 import mapsData from '../dataMap.json';
 export default {
   name: "Sections",
@@ -160,8 +161,7 @@ export default {
   },
 
   async mounted() {
-    await this.initMap();
-    // console.log('mapsData', mapsData);
+    await this.initMap2();
     document.addEventListener('click', this.close);
   },
 
@@ -234,6 +234,41 @@ export default {
       }
 
       map.on('click', this.onMapClick);
+    },
+    initMap2() {
+      d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/2015_06_30_precipitation.csv', function(err, rows){
+        function unpack(rows, key) {
+          return rows.map(function(row) { return row[key]; });
+        }
+
+        var data = [{
+          type: 'scattermapbox', text: unpack(rows, 'Globvalue'),
+          lon: unpack(rows, 'Lon'), lat: unpack(rows, 'Lat'),
+          marker: {color: 'fuchsia', size: 4}
+        }];
+
+        var layout = {
+          dragmode: 'zoom',
+          mapbox: {
+            style: 'white-bg',
+            layers: [
+              {
+                "below": 'traces',
+                "sourcetype": "raster",
+                "source": [
+                  "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
+                ]
+              },
+              {
+                sourcetype: "raster",
+                source: ["https://geo.weather.gc.ca/geomet/?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX={bbox-epsg-3857}&CRS=EPSG:3857&WIDTH=1000&HEIGHT=1000&LAYERS=RADAR_1KM_RDBR&TILED=true&FORMAT=image/png"]}],
+            below: 'traces',
+            center: {lat: 38, lon: -90}, zoom: 4},
+          margin: {r: 0, t: 0, b: 0, l: 0},
+          showlegend: false};
+
+        Plotly.newPlot('map', data, layout);
+      });
     },
     onMapClick(e) {
       console.log('event', e);
