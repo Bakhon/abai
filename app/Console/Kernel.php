0de7bdbdbec17bf289e460945dd079b23ac5132b
Nikolai Demin <demin.nikolai@Inbox.ru>
2021-07-22 12:51:40 +0800

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -39,7 +39,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('monitoring-economic-calc:cron')->dailyAt('03:00')->timezone('Asia/Almaty');
         $schedule->command('create-emergency:cron')->dailyAt('08:50')->timezone('Asia/Almaty');
         $schedule->command('calculate_hydro_yesterday:cron')
-            ->dailyAt('08:00')
+            ->dailyAt('08:20')
             ->timezone('Asia/Almaty')
             ->appendOutputTo(storage_path('logs/calculate_hydro_yesterday.log'));
 
