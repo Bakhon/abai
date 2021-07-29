1b440c2932097d9bf46b820453cf3ff917370d03
Workstation Ryzen <fannyfan414@gmail.com>
2021-07-29 14:34:00 +0200

    design fix

--- resources/views/economic/optimization.blade.php
+++ resources/views/economic/optimization.blade.php
@@ -1,16 +1,10 @@
 @extends('layouts.app')
 @section('content')
-    <div class="col p-4" id="app">
+    <div id="app" class="col-sm-12">
         <economic-optimization></economic-optimization>
     </div>
 @endsection
 <style>
-    .p-4 {
-        background-color: #0F1430;
-        overflow-x: auto !important;
-        white-space: nowrap;
-    }
-
     .main {
         background-color: #0F1430;
         background-image: url({{ asset('img/level1/grid.svg') }});
