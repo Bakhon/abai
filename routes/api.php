1e3f296562a82325c28a29542222fa681a986b08
Nurzhik <nurzhananuarbek@mail.ru>
2021-08-03 14:38:20 +0600

    TestMapController

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-
+use App\Http\Controllers\TestMapController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,3 +16,4 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/jsons',[TestMapController::class,'index']);
