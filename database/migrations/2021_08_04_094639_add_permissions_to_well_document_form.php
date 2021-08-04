a7a5e0924c2951fca37c04b28375d7a6d1273281
Ayana Adilova <aadilova19@gmail.com>
2021-08-04 14:18:58 +0600

    commit

--- database/migrations/2021_08_04_094639_add_permissions_to_well_document_form.php
+++ database/migrations/2021_08_04_094639_add_permissions_to_well_document_form.php
@@ -7,7 +7,7 @@ class AddPermissionsToWellDocumentForm extends Migration
 {
     public function up()
     {
-        $permissions = json_decode('[{"name":"bd forms well_document list","guard_name":"web"},{"name":"bd forms well_document create","guard_name":"web"},{"name":"bd forms well_document edit","guard_name":"web"},{"name":"bd forms well_document history","guard_name":"web"},{"name":"bd forms well_document delete","guard_name":"web"}]', 1);
+        $permissions = json_decode('[{"name":"bigdata well_document list","guard_name":"web"},{"name":"bigdata well_document create","guard_name":"web"},{"name":"bigdata well_document edit","guard_name":"web"},{"name":"bigdata well_document history","guard_name":"web"},{"name":"bigdata well_document delete","guard_name":"web"}]', 1);
 
         foreach ($permissions as $permission) {
             DB::table('permissions')->insert($permission);
