d35524895b1764aa01fc32eb6e3e4e2db1333388
Nikolai Demin <demin.nikolai@Inbox.ru>
2021-08-05 16:09:10 +0800

    update

--- routes/modules/monitoring.php
+++ routes/modules/monitoring.php
@@ -75,6 +75,10 @@ Route::group(
                 Route::get('omgngdu/history/{omgngdu}', 'ComplicationMonitoring\OmgNGDUController@history')->name(
                     'omgngdu.history'
                 );
+                Route::post('omgngdu/get-omgngdu', 'ComplicationMonitoring\OmgNGDUController@getOmgNgdu')->name(
+                    'omgngdu.get-omg-ngdu'
+                );
+
                 Route::resource('omgngdu', 'ComplicationMonitoring\OmgNGDUController');
 
 
