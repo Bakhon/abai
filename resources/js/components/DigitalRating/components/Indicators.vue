cc1fd777abd55bc7d3cb35c701d72887bdb0de52
Kuanysh <k.daniyarov@niikmg.kz>
2021-08-10 14:12:54 +0600

    development/add_diagram_indicators

--- resources/js/components/DigitalRating/components/Indicators.vue
+++ resources/js/components/DigitalRating/components/Indicators.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="rating-indicators">
-    <div class="d-flex">
-      <table class="table text-center text-white rating-table w-50">
+    <div class="d-flex mb-20px">
+      <table class="table text-center text-white rating-table w-50 mb-0">
         <thead>
           <tr>
             <th class="align-middle" v-for="col in colsIndicator" :key="col.name">
@@ -17,9 +17,15 @@
           </tr>
         </tbody>
       </table>
+      <Plotly
+        :data="data"
+        :layout="layout"
+        :display-mode-bar="false"
+        :displaylogo="false"
+      />
     </div>
     <div class="d-flex">
-      <table class="table text-center text-white rating-table w-50">
+      <table class="table text-center text-white rating-table w-50 mb-0">
         <thead>
           <tr>
             <th class="align-middle" v-for="col in secondColsIndicators" :key="col.name">
@@ -35,20 +41,68 @@
           </tr>
         </tbody>
       </table>
+      <Plotly
+          :data="data"
+          :layout="layout"
+          :display-mode-bar="false"
+          :displaylogo="false"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import { indicators, secondIndicators } from '../json/data';
+import { Plotly } from 'vue-plotly';
 
 export default {
   name: "Indicators",
 
+  components: {
+    Plotly
+  },
+
   data() {
     return {
       indicators: indicators,
-      secondIndicators: secondIndicators
+      secondIndicators: secondIndicators,
+      data:[{
+        x: [2015, 2016, 2017, 2018, 2019, 2020, 2021],
+        y: [200,400,600,800,1000,1200,1400],
+        type:"scatter"
+      }],
+      layout: {
+        height: 320,
+        showlegend: true,
+        margin: {
+          pad: 10
+        },
+        xaxis: {
+          title: "",
+          zeroline: false,
+          gridcolor: "#3C4270",
+        },
+        yaxis: {
+          title: "Добыча жидкости,м3.Добыча нефти, т",
+          showlegend: true,
+          zeroline: false,
+          gridcolor: "#3C4270",
+          rangemode: 'tozero'
+        },
+
+        paper_bgcolor: "#2B2E5E",
+        plot_bgcolor: "#2B2E5E",
+        font: { color: "#fff" },
+
+        legend: {
+          orientation: "h",
+          y: -0.3,
+          font: {
+            size: 9.3,
+            color: "#fff",
+          },
+        },
+      },
     }
   },
 
