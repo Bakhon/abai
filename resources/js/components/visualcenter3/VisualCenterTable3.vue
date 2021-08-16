2b06c53f83aaa894aa6bb243304a15864434a3ef
Serhii Suhal <vio3273@gmail.com>
2021-08-16 13:24:07 +0300

    add temporary chart

--- resources/js/components/visualcenter3/VisualCenterTable3.vue
+++ resources/js/components/visualcenter3/VisualCenterTable3.vue
@@ -1806,13 +1806,16 @@
               </div>
               <br />
               <div class="row container-fluid">
-                <div class="vis-table px-4 col-sm-7">
+                <div
+                        :class="[isProductionFondPeriodSelected ? 'col-sm-7' : 'col-sm-12','vis-table px-4']"
+                >
                   <table v-if="productionFondData.length" class="table4 w-100 chemistry-table additional-tables">
                     <thead>
                     <tr>
                       <th v-if="fondDaysCountSelected.production < 2">{{ trans("visualcenter.prodWells") }} ({{ trans("visualcenter.Fact") }})</th>
                       <th v-else>{{ trans("visualcenter.prodWells") }} ({{ trans("visualcenter.fondMiddleInMonth") }})</th>
                       <th>{{ trans("visualcenter.otmMetricSystemWells") }}</th>
+                      <th v-if="!isProductionFondPeriodSelected">Количество добывающих скважин, скв.</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -1842,14 +1845,26 @@
                           {{ getFormattedNumber(item.fact) }}
                         </div>
                       </td>
+                      <td v-if="!isProductionFondPeriodSelected">
+                        <div class="progress progress-bar_header">
+                          <div
+                                  class="progress-bar progress-bar_body"
+                                  role="progressbar"
+                                  :style="{width: '25%',}"
+                                  :aria-valuenow="item.fact"
+                                  aria-valuemin="0"
+                                  :aria-valuemax="productionFondData[0].fact"
+                          ></div>
+                        </div>
+                      </td>
                     </tr>
                     </tbody>
                   </table>
                 </div>
-                <div class="col-sm-5">
+                <div :class="isProductionFondPeriodSelected ? 'col-sm-5' : 'col-0'">
                   <div  class="name-chart-left">{{ trans('visualcenter.wellsNumber') }}</div>
                   <visual-center3-wells
-                          v-if="productionFondDataForChart"
+                          v-if="productionFondDataForChart && isProductionFondPeriodSelected"
                           :chartData="productionFondDataForChart"
                   >
                   </visual-center3-wells>
@@ -2821,6 +2836,9 @@
       height: 80px;
       padding: 5px 5px 5px 10px;
       font-size: 16px !important;
+      &:nth-child(3) {
+        min-width: 612px;
+      }
     }
   }
 
@@ -3231,5 +3249,11 @@
     cursor: pointer;
     color: black;
     min-width: 99%;
+  }
+  .progress-bar_header {
+    height: 80px !important;
+  }
+  .progress-bar_body {
+    border-radius: 0;
   }
 </style>
