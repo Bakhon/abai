6c5989a519108656ca3880b55e66c121121cdd73
Nikolai Demin <demin.nikolai@Inbox.ru>
2021-08-18 15:35:52 +0800

    refactoring

--- app/Console/Commands/CalculateGuCorrosionCron.php
+++ app/Console/Commands/CalculateGuCorrosionCron.php
@@ -29,6 +29,7 @@ class CalculateGuCorrosionCron extends Command
      */
     protected $description = 'Calculate corrosion on point A for each GU for yesterday';
 
+    protected $date = null;
     /**
      * Create a new command instance.
      *
@@ -36,6 +37,7 @@ class CalculateGuCorrosionCron extends Command
      */
     public function __construct()
     {
+        $this->date = Carbon::yesterday()->format('Y-m-d');
         parent::__construct();
     }
 
@@ -52,90 +54,105 @@ class CalculateGuCorrosionCron extends Command
             ->whereNotNull('lon')
             ->get();
 
-        $date = Carbon::yesterday()->format('Y-m-d');
-        Log::channel('calculate_corrosion_yesterday:cron')->info('Yesterday date ' . $date);
+        Log::channel('calculate_corrosion_yesterday:cron')->info('Yesterday date ' . $this->date);
 
         foreach ($gus as $gu) {
-            $post = new POSTCaller(
-                WaterMeasurementController::class,
-                'getGuData',
-                Request::class,
-                [
-                    'gu_id' => $gu->id
-                ]
-            );
-
-            $guData = $post->call()->getData();
-
-            $post = new POSTCaller(
-                OmgNGDUController::class,
-                'getGuDataByDay',
-                Request::class,
-                [
-                    'dt' => $date,
-                    'gu_id' => $gu->id
-                ]
-            );
-
-            $guDataByDay = $post->call()->getData();
-
-            if ($guDataByDay->oilGas &&
-                $guData->pipe &&
-                $guDataByDay->ngdu &&
-                $guDataByDay->ngdu->surge_tank_pressure &&
-                $guDataByDay->ngdu->pump_discharge_pressure) {
-                $data = [
-                    "gu_id" => $gu->id,
-                    "WC" => $guDataByDay->ngdu->bsw,
-                    "GOR1" => $guData->constantsValues[0]->value,
-                    "sigma" => $guData->constantsValues[1]->value,
-                    "do" => $guData->pipe->outside_diameter,
-                    "roughness" => $guData->pipe->roughness,
-                    "l" => $guData->pipe->length,
-                    "thickness" => $guData->pipe->thickness,
-                    "P" => $guDataByDay->ngdu->pump_discharge_pressure,
-                    "t_heater" => $guDataByDay->ngdu->heater_output_temperature,
-                    "conH2S" => $guDataByDay->wmLastH2S->hydrogen_sulfide,
-                    "conCO2" => $guDataByDay->wmLastCO2->carbon_dioxide,
-                    "t_inlet_heater" => $guDataByDay->ngdu->heater_inlet_temperature,
-                    "q_l" => $guDataByDay->ngdu->daily_fluid_production,
-                    "H2O" => $guDataByDay->ngdu->bsw,
-                    "HCO3" => $guDataByDay->wmLastHCO3->hydrocarbonate_ion,
-                    "Cl" => $guDataByDay->wmLastCl->chlorum_ion,
-                    "SO4" => $guDataByDay->wmLastSO4->sulphate_ion,
-                    "q_g_sib" => $guDataByDay->ngdu->daily_gas_production_in_sib,
-                    "P_bufer" => $guDataByDay->ngdu->surge_tank_pressure,
-                    "rhol" => $guDataByDay->wmLastH2S->density,
-                    "rho_o" => $guDataByDay->oilGas->water_density_at_20,
-                    "rhog" => $guDataByDay->oilGas->gas_density_at_20,
-                    "mul" => $guDataByDay->oilGas->oil_viscosity_at_20,
-                    "mug" => $guDataByDay->oilGas->gas_viscosity_at_20,
-                    "q_o" => $guDataByDay->ngdu->daily_oil_production
-                ];
-
-                $post = new POSTCaller(
-                    DruidController::class,
-                    'corrosion',
-                    Request::class,
-                    $data
-                );
-                $corrosion = $post->call()->getData();
-
-                $message = 'GU ' . $gu->name . ' corrosion ' . $corrosion->corrosion_rate_mm_per_y_point_A;
-                Log::channel('calculate_corrosion_yesterday:cron')->info($message);
-
-                $calcCorrosion = CalculatedCorrosion::firstOrNew(
-                    [
-                        'date' => $date,
-                        'gu_id' => $gu->id
-                    ]
-                );
-
-                $calcCorrosion->corrosion = $corrosion->corrosion_rate_mm_per_y_point_A;
-                $calcCorrosion->save();
-            }
+            $this->calculateCorrosion($gu);
         }
 
         Log::channel('calculate_corrosion_yesterday:cron')->info('Сalculation completed');
+    }
+
+    public function calculateCorrosion (Gu $gu) {
+        $post = new POSTCaller(
+            WaterMeasurementController::class,
+            'getGuData',
+            Request::class,
+            [
+                'gu_id' => $gu->id
+            ]
+        );
+
+        $guData = $post->call()->getData();
+
+        $post = new POSTCaller(
+            OmgNGDUController::class,
+            'getGuDataByDay',
+            Request::class,
+            [
+                'dt' => $this->date,
+                'gu_id' => $gu->id
+            ]
+        );
+
+        $guDataByDay = $post->call()->getData();
+
+        if ($this->isValidParams($guDataByDay, $guData)) {
+            $data = $this->setParams($gu, $guDataByDay, $guData);
+
+            $post = new POSTCaller(
+                DruidController::class,
+                'corrosion',
+                Request::class,
+                $data
+            );
+            $corrosion = $post->call()->getData();
+
+            $message = 'GU ' . $gu->name . ' corrosion ' . $corrosion->corrosion_rate_mm_per_y_point_A;
+            Log::channel('calculate_corrosion_yesterday:cron')->info($message);
+
+            $calcCorrosion = CalculatedCorrosion::firstOrNew(
+                [
+                    'date' => $this->date,
+                    'gu_id' => $gu->id
+                ]
+            );
+
+            $calcCorrosion->corrosion = $corrosion->corrosion_rate_mm_per_y_point_A;
+            $calcCorrosion->save();
+        }
+    }
+
+    public function isValidParams ($guDataByDay, $guData) {
+        if ($guDataByDay->oilGas &&
+            $guData->pipe &&
+            $guDataByDay->ngdu &&
+            $guDataByDay->ngdu->surge_tank_pressure &&
+            $guDataByDay->ngdu->pump_discharge_pressure) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function setParams ($gu, $guDataByDay, $guData) {
+        return [
+            "gu_id" => $gu->id,
+            "WC" => $guDataByDay->ngdu->bsw,
+            "GOR1" => $guData->constantsValues[0]->value,
+            "sigma" => $guData->constantsValues[1]->value,
+            "do" => $guData->pipe->outside_diameter,
+            "roughness" => $guData->pipe->roughness,
+            "l" => $guData->pipe->length,
+            "thickness" => $guData->pipe->thickness,
+            "P" => $guDataByDay->ngdu->pump_discharge_pressure,
+            "t_heater" => $guDataByDay->ngdu->heater_output_temperature,
+            "conH2S" => $guDataByDay->wmLastH2S->hydrogen_sulfide,
+            "conCO2" => $guDataByDay->wmLastCO2->carbon_dioxide,
+            "t_inlet_heater" => $guDataByDay->ngdu->heater_inlet_temperature,
+            "q_l" => $guDataByDay->ngdu->daily_fluid_production,
+            "H2O" => $guDataByDay->ngdu->bsw,
+            "HCO3" => $guDataByDay->wmLastHCO3->hydrocarbonate_ion,
+            "Cl" => $guDataByDay->wmLastCl->chlorum_ion,
+            "SO4" => $guDataByDay->wmLastSO4->sulphate_ion,
+            "q_g_sib" => $guDataByDay->ngdu->daily_gas_production_in_sib,
+            "P_bufer" => $guDataByDay->ngdu->surge_tank_pressure,
+            "rhol" => $guDataByDay->wmLastH2S->density,
+            "rho_o" => $guDataByDay->oilGas->water_density_at_20,
+            "rhog" => $guDataByDay->oilGas->gas_density_at_20,
+            "mul" => $guDataByDay->oilGas->oil_viscosity_at_20,
+            "mug" => $guDataByDay->oilGas->gas_viscosity_at_20,
+            "q_o" => $guDataByDay->ngdu->daily_oil_production
+        ];
     }
 }
