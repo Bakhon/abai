4bedfef91820ca5011fb320dad969b099acf3216
Yerassyl <ye.kurmangali@niikmg.kz>
2021-08-18 16:00:36 +0600

    removed unused component

--- routes/modules/plast_fluids.php
+++ routes/modules/plast_fluids.php
@@ -7,7 +7,6 @@ Route::group(
             ['middleware' => 'auth'],
             function () {
                 Route::get('/pf', 'PlastFluids\PlastFluidsController@pf')->name('pf');
-                Route::get('/pf-template-pvt-plast-oil', 'PlastFluids\PlastFluidsController@pfTmplPvtPlastOil')->name('pf-template-pvt-plast-oil');
                 Route::get('/pf-upload-monitoring', 'PlastFluids\PlastFluidsController@pfUploadMonitoring')->name('pf-upload-monitoring');
                 Route::get('/pf-standart-separation-flash', 'PlastFluids\PlastFluidsController@pfStandartSeparationFlash')->name('pf-standart-separation-flash');
                 Route::get('/pf-data-analysis', function () {
