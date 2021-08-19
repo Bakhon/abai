b56554a32fcaea47bacd30f9b1ab11cc36c84f4d
Ruslan <russel1987@mail.ru>
2021-08-19 09:21:34 +0300

    refactor

--- app/Services/AttachmentService.php
+++ app/Services/AttachmentService.php
@@ -78,6 +78,6 @@ class AttachmentService
     {
         $size = array('B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
         $factor = floor((strlen($bytes) - 1) / 3);
-        return round($bytes / pow(1024, $factor)) . @$size[$factor];
+        return round($bytes / pow(1024, $factor)) . $size[$factor];
     }
 }
