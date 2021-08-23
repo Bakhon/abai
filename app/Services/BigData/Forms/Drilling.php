5b0b99dbc8dfeda7a4876650fef7b05acce5b30a
Ayana Adilova <aadilova19@gmail.com>
2021-08-23 10:15:19 +0600

    commit

--- app/Services/BigData/Forms/Drilling.php
+++ app/Services/BigData/Forms/Drilling.php
@@ -3,8 +3,77 @@
 declare(strict_types=1);
 
 namespace App\Services\BigData\Forms;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
-class Drilling extends PlainForm
+class Drilling extends TableForm
 {
     protected $configurationFileName = 'drilling';
+
+    public function getRows(array $params = []): array
+    {
+        $filter = json_decode($this->request->get('filter'));
+        $wells = $this->getWells((int)$this->request->get('id'), $this->request->get('type'), $filter, $params);
+
+        $tables = $this->getFields()->pluck('table')->filter()->unique();
+        $rowData = $this->fetchRowData(
+            $tables,
+            $wells->pluck('id')->toArray(),
+            Carbon::parse($this->request->get('date'))
+        );
+
+        $wells->transform(
+            function ($item) use ($rowData) {
+                $result = [];
+
+                foreach ($this->getFields() as $field) {
+                    $fieldValue = $this->getFieldValue($field, $rowData, $item);
+                    if (!empty($fieldValue)) {
+                        $result[$field['code']] = $fieldValue;
+                    }
+                }
+                return $result;
+            }
+        );
+
+        $this->addLimits($wells);
+
+        return [
+            'rows' => $wells->toArray()
+        ];
+    }
+
+    protected function saveSingleFieldInDB(array $params): void
+    {
+        $column = $this->getFieldByCode($params['field']);
+
+        $item = $this->getFieldRow($column, $params['wellId'], $params['date']);
+
+        if (empty($item)) {
+            $data = [
+                'well' => $params['wellId'],
+                $column['column'] => $params['value'],
+                'dbeg' => $params['date']->toDateTimeString()
+            ];
+
+            if (!empty($column['additional_filter'])) {
+                foreach ($column['additional_filter'] as $key => $val) {
+                    $data[$key] = $val;
+                }
+            }
+
+            DB::connection('tbd')
+                ->table($column['table'])
+                ->insert($data);
+        } else {
+            DB::connection('tbd')
+                ->table($column['table'])
+                ->where('id', $item->id)
+                ->update(
+                    [
+                        $column['column'] => $params['value']
+                    ]
+                );
+        }
+    }
 }
