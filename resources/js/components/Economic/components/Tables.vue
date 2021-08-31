f7a0a827fbd7436e691688c94fcd0c603b25e1ae
Workstation Ryzen <fannyfan414@gmail.com>
2021-08-31 09:53:23 +0200

    design fix

--- resources/js/components/Economic/components/Tables.vue
+++ resources/js/components/Economic/components/Tables.vue
@@ -2,18 +2,18 @@
   <div class="row p-3 bg-main1 position-relative">
     <div class="d-flex">
       <chart-button
-          v-for="(tab, index) in tabs"
+          v-for="(tab, index) in Object.keys(tabs)"
           :key="index"
-          :text="tab"
-          :active="activeIndex === index"
+          :text="tabs[tab]"
+          :active="activeTab === tab"
           :class="index ? 'ml-2' : ''"
           class="px-2 d-flex align-items-center"
-          @click.native="selectTab(index)"/>
+          @click.native="selectTab(tab)"/>
     </div>
 
     <div class="mt-3 w-100">
       <table-specific-indicators
-          v-if="activeTab === 'specific-indicators'"
+          v-if="activeTab === 'specific_indicators'"
           :org="res.org"
           :scenario="scenario"
           :oil-prices="scenarioVariations.oil_prices"
@@ -21,7 +21,7 @@
           class="text-white"/>
 
       <table-technical-economic-indicators
-          v-else-if="activeTab === 'technical-economic-indicators'"
+          v-else-if="activeTab === 'technical_economic_indicators'"
           :org="res.org"
           :scenarios="res.scenarios"
           :scenario="scenario"
@@ -29,14 +29,14 @@
           class="text-white"/>
 
       <table-oil-price-options
-          v-else-if="activeTab === 'oil-price-options'"
+          v-else-if="activeTab === 'oil_price_options'"
           :org="res.org"
           :scenarios="res.scenarios"
           :scenario="scenario"
           class="text-white"/>
 
       <table-well-changes
-          v-else-if="activeTab === 'well-changes'"
+          v-else-if="activeTab === 'well_changes'"
           :org="res.org"
           :scenarios="res.scenarios"
           :scenario="scenario"
@@ -46,7 +46,7 @@
           class="text-white"/>
 
       <table-economic-efficiency
-          v-else-if="activeTab === 'economic-efficiency'"
+          v-else-if="activeTab === 'economic_efficiency'"
           :scenarios="res.scenarios"
           :scenario="scenario"
           :oil-prices="scenarioVariations.oil_prices"
@@ -60,7 +60,7 @@
           class="text-white"/>
 
       <table-technological-indicators
-          v-else-if="activeTab === 'technological-indicators'"
+          v-else-if="activeTab === 'technological_indicators'"
           :scenarios="res.scenarios"
           :scenario="scenario"
           :oil-prices="scenarioVariations.oil_prices"
@@ -115,52 +115,28 @@ export default {
     }
   },
   data: () => ({
-    activeIndex: 0,
-    activeTab: 'specific-indicators',
+    activeTab: 'specific_indicators',
   }),
   computed: {
     tabs() {
-      return [
-        this.trans('economic_reference.specific_indicators'),
-        this.trans('economic_reference.technical_economic_indicators'),
-        this.trans('economic_reference.oil_price_options'),
-        this.trans('economic_reference.table_well_changes'),
-        this.trans('economic_reference.economic_efficiency'),
-        this.trans('economic_reference.table_porcupine'),
-        this.trans('economic_reference.technological_indicators'),
-        this.trans('economic_reference.palette'),
-      ]
+      return {
+        specific_indicators: this.trans('economic_reference.specific_indicators'),
+        technical_economic_indicators: this.trans('economic_reference.technical_economic_indicators'),
+        oil_price_options: this.trans('economic_reference.oil_price_options'),
+        well_changes: this.trans('economic_reference.table_well_changes'),
+        economic_efficiency: this.trans('economic_reference.economic_efficiency'),
+        porcupine: this.trans('economic_reference.table_porcupine'),
+        technological_indicators: this.trans('economic_reference.technological_indicators'),
+        palette: this.trans('economic_reference.palette'),
+      }
     }
   },
   methods: {
-    selectTab(index) {
-      this.activeIndex = index
+    selectTab(tab) {
+      this.activeTab = tab
 
-      this.activeTab = this.mapActiveTab(index)
-
-      this.$emit('updateTab', index)
+      this.$emit('updateTab', tab)
     },
-
-    mapActiveTab(index) {
-      switch (index) {
-        case 0:
-          return 'specific-indicators'
-        case 1:
-          return 'technical-economic-indicators'
-        case 2:
-          return 'oil-price-options'
-        case 3:
-          return 'well-changes'
-        case 4:
-          return 'economic-efficiency'
-        case 5:
-          return 'porcupine'
-        case 6:
-          return 'technological-indicators'
-        case 7:
-          return 'palette'
-      }
-    }
   }
 }
 </script>
