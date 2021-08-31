126b4394adcae8d790638b92248dacbbd4d3ed5a
Workstation Ryzen <fannyfan414@gmail.com>
2021-08-31 07:42:50 +0200

    design fix

--- resources/js/components/Economic/components/TablePalette.vue
+++ resources/js/components/Economic/components/TablePalette.vue
@@ -82,6 +82,22 @@
 <script>
 import Subtitle from "./Subtitle";
 
+const ROMANS = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1
+}
+
 export default {
   name: "TablePalette",
   components: {
@@ -103,22 +119,6 @@ export default {
   },
   methods: {
     convertToRoman(value) {
-      const ROMANS = {
-        M: 1000,
-        CM: 900,
-        D: 500,
-        CD: 400,
-        C: 100,
-        XC: 90,
-        L: 50,
-        XL: 40,
-        X: 10,
-        IX: 9,
-        V: 5,
-        IV: 4,
-        I: 1
-      };
-
       let str = '';
 
       for (let i of Object.keys(ROMANS)) {
