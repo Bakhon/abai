b0f4e4cc0826e71b7cdfca2fc9ec5a686c61b834
Nikolai Demin <demin.nikolai@Inbox.ru>
2021-09-01 17:57:30 +0800

    calculate manual pipes net

--- app/Jobs/ManualCalculateHydroDynamics.php
+++ app/Jobs/ManualCalculateHydroDynamics.php
@@ -64,61 +64,6 @@ class ManualCalculateHydroDynamics implements ShouldQueue
         'SGwater'
     ];
 
-    const ID = 0;
-    const POINTS_OR_SEGMENT = 0;
-
-    protected $hydroCalcLongSchema = [
-        'distance' => 1,
-        'dout' => 2,
-        'wt' => 3,
-        'liq_rate' => 4,
-        'gor' => 5,
-        'wc' => 6,
-        'pin' => 7,
-        'pout' => 8,
-        'tin' => 9,
-        'tout' => 10,
-        'flow_pattern' => 11,
-        'vliq' => 12,
-        'vgas' => 13,
-        'vm' => 14,
-        'pressure_gradient' => 15,
-        'ohtc' => 16,
-        'nre' => 17,
-        'holdup' => 18,
-        'lambda' => 19,
-        'h_soil' => 20,
-        'h_f' => 21,
-        'npr' => 22,
-        'nnu' => 23,
-        'f_f_ratio' => 24,
-        'rs' => 25,
-        'rsw' => 26,
-        'ev' => 27,
-        'comment' => 28
-    ];
-
-    protected $hydroCalcShortSchema = [
-        'length' => 3,
-        'qliq' => 4,
-        'bsw' => 5,
-        'gazf' => 6,
-        'press_start' => 7,
-        'press_end' => 8,
-        'temperature_start' => 9,
-        'temperature_end' => 10,
-        'start_point' => 11,
-        'end_point' => 12,
-        'mix_speed_avg' => 14,
-        'fluid_speed' => 15,
-        'gaz_speed' => 16,
-        'flow_type' => 17,
-        'press_change' => 18,
-        'break_qty' => 19,
-        'height_drop' => 20
-    ];
-
-
     /**
      * Create a new job instance.
      *
@@ -225,69 +170,11 @@ class ManualCalculateHydroDynamics implements ShouldQueue
             }
 
             $data = json_decode($request->getBody()->getContents());
-            $short = $data->short->data;
-            $long = $data->long;
-
-            if ($short) {
-                $this->storeShortResult($short);
-            }
-
-            if ($long) {
-                array_unshift($long->data, $long->columns);
-                $this->storeLongResult($long->data);
-            }
-        }
-    }
-
-    protected function storeShortResult(array $data): void
-    {
-        foreach ($data as $row) {
-            $trunkline_point = TrunklinePoint::find($row[self::ID]);
-
-            $hydroCalcResult = HydroCalcResult::firstOrCreate(
+            $this->setOutput(
                 [
-                    'date' => Carbon::parse($this->input['date'])->format('Y-m-d'),
-                    'oil_pipe_id' => $trunkline_point->oil_pipe_id,
-                    'trunkline_point_id' => $trunkline_point->id,
+                    'data' => $data
                 ]
             );
-
-            foreach ($this->hydroCalcShortSchema as $param => $index) {
-                $hydroCalcResult->$param = $row[$index];
-            }
-
-            $hydroCalcResult->save();
-        }
-    }
-
-    protected function storeLongResult(array $data): void
-    {
-        foreach ($data as $row) {
-            if (!ctype_digit($row[self::POINTS_OR_SEGMENT])) {
-                $pointsNames = explode(' - ', $row[self::POINTS_OR_SEGMENT]);
-
-                $trunkline_point = TrunklinePoint::where('name', $pointsNames[0])
-                    ->whereHas('trunkline_end_point', function ($query) use ($pointsNames) {
-                        return $query->where('name', $pointsNames[1]);
-                    })
-                    ->first();
-
-                continue;
-            }
-
-            $hydroCalcLong = HydroCalcLong::firstOrCreate(
-                [
-                    'date' => Carbon::parse($this->input['date'])->format('Y-m-d'),
-                    'oil_pipe_id' => $trunkline_point->oil_pipe_id,
-                    'segment' => $row[self::POINTS_OR_SEGMENT]
-                ]
-            );
-
-            foreach ($this->hydroCalcLongSchema as $param => $index) {
-                $hydroCalcLong->$param = $row[$index];
-            }
-
-            $hydroCalcLong->save();
         }
     }
 
