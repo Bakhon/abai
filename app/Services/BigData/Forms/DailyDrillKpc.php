57ce2bd9521e677b25428e39e5f8cae5ddf491a9
Ayana Adilova <aadilova19@gmail.com>
2021-09-02 15:54:07 +0600

    commit

--- app/Services/BigData/Forms/DailyDrillKpc.php
+++ app/Services/BigData/Forms/DailyDrillKpc.php
@@ -30,7 +30,7 @@ class DailyDrillKpc extends TableForm
 
     protected function saveSingleFieldInDB(array $params): void
     {
-        list($company, $machine_type, $well , $geo , $repair_work_type , $work_done) = explode('_', $params['field']);
+        list($machine_type, $work_done) = explode('_', $params['field']);
         $result = [
                     'id' => $this->request->get('id')
                 ];
@@ -48,7 +48,7 @@ class DailyDrillKpc extends TableForm
                 ->leftJoin('prod.report_org_daily_repair as pp', 'pw.id', 'pp.workover')
                 ->leftJoin('dict.company as dc', 'pw.contractor', 'dc.id')           
                 ->first();
-                $result['contractor'] = ['value' => $company->contractor];
+               
             $well = DB::connection('tbd')
             ->table('prod.well_workover as pw')
             ->select('dw.uwi')
@@ -89,38 +89,42 @@ class DailyDrillKpc extends TableForm
     
     public function getRows(array $params = []): array
     {
+        $id = $this->request->get('id');
+        
         $result = [
-            'id' => $this->request->get('id')
+            'id' => $id
         ];
-        $filter = json_decode($this->request->get('filter'));
-        if ($this->request->get('id')) {
-            $org = Org::find($this->request->get('id'));
+       
+        if ($id) {
+            $org = Org::find($id);
             if (!$org) {
                 return ['rows' => []];
-            }
-            $result['org'] = ['value' => $org->name_ru];
+            }            
+            $result['org'] = ['value' => $org->name_ru];           
         }
-        $filter->optionId = $filter->optionId ?? 0;
+        // $filter->optionId = $filter->optionId ?? 0;
         $company = DB::connection('tbd')
             ->table('prod.well_workover as pw')
             ->select('pw.contractor')
             ->leftJoin('prod.report_org_daily_repair as pp', 'pw.id', 'pp.workover')
-            ->leftJoin('dict.company as dc', 'pw.contractor', 'dc.id')           
+            ->leftJoin('dict.company as dc', 'pw.contractor', 'dc.id')     
             ->first();
             $result['contractor'] = ['value' => $company->contractor];
+          
         $well = DB::connection('tbd')
         ->table('prod.well_workover as pw')
         ->select('dw.uwi')
+        ->leftJoin('prod.report_org_daily_repair as pr', 'pw.id' , 'pr.workover')     
         ->leftJoin('dict.well as dw', 'pw.well', 'dw.id')           
-        ->first();
-        $result['well'] = ['value' => $well->uwi]; 
+        ->get();
+        $result['well'] = ['value' => $well->keyBy('uwi')]; 
         
         $geo = DB::connection('tbd')
         ->table('prod.well_workover as pw')
         ->select('g.name_ru')
         ->leftJoin('prod.well_geo as pg', 'pw.well', 'pg.well')   
         ->leftJoin('dict.geo as g', 'pg.geo', 'g.id')                   
-        ->first();
+        ->get();
         $result['geo'] = ['value' => $geo->name_ru]; 
 
         $repair_work_type = DB::connection('tbd')
@@ -129,6 +133,19 @@ class DailyDrillKpc extends TableForm
         ->leftJoin('dict.repair_work_type as dw', 'pw.repair_work_type', 'dw.id')           
         ->first();
         $result['repair_work_type'] = ['value' => $repair_work_type->repair_work_type]; 
+        $machine_type = DB::connection('tbd')
+        ->table('prod.well_workover as pw')
+        ->select('pr.machine_type')
+        ->leftJoin('prod.report_org_daily_repair as pr', 'pw.id' , 'pr.workover')
+        ->leftJoin('dict.machine_type as dw', 'pr.machine_type', 'dw.id')           
+        ->first();
+        $result['machine_type'] = ['value' => $machine_type->machine_type];
+        $work_done = DB::connection('tbd')
+        ->table('prod.well_workover as pw')
+        ->select('pr.work_done')
+        ->leftJoin('prod.report_org_daily_repair as pr', 'pw.id' , 'pr.workover')          
+        ->first();
+        $result['work_done'] = ['value' => $work_done->work_done];
         return ['rows' => [$result]];
     }   
 }
