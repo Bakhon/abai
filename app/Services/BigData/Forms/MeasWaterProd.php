82d67c976491d733ddf0812de536fe027afbf022
Ruslan <russel1987@mail.ru>
2021-09-09 07:39:54 +0300

    refactored planned events

--- app/Services/BigData/Forms/MeasWaterProd.php
+++ app/Services/BigData/Forms/MeasWaterProd.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace App\Services\BigData\Forms;
 
+use App\Traits\BigData\Forms\HasPlannedEvents;
+
 class MeasWaterProd extends PlainForm
 {
+    use HasPlannedEvents;
+
     protected $configurationFileName = 'meas_water_prod';
 }
