ffb881e9187316fab6f7bef03f4c582ae86b7a8b
Ruslan <russel1987@mail.ru>
2021-09-10 13:56:22 +0300

    updated form

--- app/Services/BigData/Forms/Drilling.php
+++ app/Services/BigData/Forms/Drilling.php
@@ -3,71 +3,46 @@
 declare(strict_types=1);
 
 namespace App\Services\BigData\Forms;
+use App\Models\BigData\Dictionaries\Org;
 use Illuminate\Support\Facades\DB;
 
 class Drilling extends TableForm
 {
+    protected $configurationFileName = 'drilling';
+
     protected function saveSingleFieldInDB(array $params): void
     {
-        $reportId = $params['wellId'];
-
-        DB::connection('tbd')
-            ->table('prod.report_org_daily_drill')
-            ->where('id', $reportId)
-            ->update(
-                [
-                    $params['field'] => $params['value']
-                ]
-            );
     }
-
 
     public function getRows(array $params = []): array
     {
         $filter = json_decode($this->request->get('filter'));
         if (empty($filter->date)) {
-            return ['rows' => []];
+            throw new \Exception(trans('bd.select_date'));
         }
 
         if ($this->request->get('type') !== 'org') {
             throw new \Exception(trans('bd.select_dzo_ngdu'));
         }
 
+        $orgIds = $this->getOrgIds();
+
         $rows = DB::connection('tbd')
-            ->table('prod.report_org_daily_drill as rodr')
+            ->table('prod.report_org_daily_drill as rodd')
             ->select(
-                'rodr.id',
-                'org.name_ru as org',
-                'ww.well',
-                'ww.drill',
-                'ww.daily_drill_progress',
+                'wdd.id',
+                'w.id as well_id',
+                'w.uwi',
+                'wdd.daily_drill_progress',
                 'bh.depth',
-                'dw.name_ru',
-                'ww.liquid_density',
-                'ww.liquid_crust',
-                'ww.liquid_viscosity',
-                'ww.liquid_ph',
-                'ww.luquid_water_yield',
-                'ww.drill_rate',
-                'ww.drill_load',
-                'ww.revs_per_minute',
-                'ww.drill_pump_p',
-                'ww.rotating_moment',
-                'ww.drill_piston_d',
-                'ww.kern_roofing',
-                'ww.kern_sole',
-                'ww.drill_chisel',
-                'ww.diameter',
-                'ww.value',
+                'wdd.well_status_type',
+                'wdd.work_name'
             )
-            ->join('drill.well_daily_drill as ww', 'rodr.drill', 'ww.id')
-            ->join('dict.org as org', 'rodr.org', 'org.id')
-            ->join('prod.bottom_hole as bh', 'ww.well' , 'bh.well')
-            ->join('dict.well_status_type as dw', 'ww.well_status_type' , 'dw.id')
-            ->join('dict.drill_pump_type as dp', 'ww.drill_pump_type' , 'dp.id')
-            ->join('dict.drill_chisel as dc' , 'ww.drill_chisel' , 'dc.id')
-            ->where('rodr.org', $this->request->get('id'))
-            ->where('rodr.report_date', $filter->date)
+            ->join('drill.well_daily_drill as wdd', 'rodd.drill', 'wdd.id')
+            ->join('prod.bottom_hole as bh', 'wdd.well', 'bh.well')
+            ->join('dict.well as w', 'wdd.well', 'w.id')
+            ->whereIn('rodd.org', $orgIds)
+            ->where('rodd.report_date', $filter->date)
             ->get()
             ->map(function ($item) {
                 $result = [];
@@ -76,16 +51,17 @@ class Drilling extends TableForm
                         $result[$key] = $value;
                         continue;
                     }
-                    $result[$key] = [
-                        'value' => $value
-                    ];
+                    $result[$key] = ['value' => $value];
                 }
-                $result['geo'] = [
-                    'value' => 12
-                ];
                 return $result;
             });
 
         return ['rows' => $rows];
+    }
+
+    private function getOrgIds()
+    {
+        $org = Org::find($this->request->get('id'));
+        return array_merge([$org->id], $org->children->pluck('id')->toArray());
     }
 }
