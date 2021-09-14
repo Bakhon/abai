e759607ee38748b8d79c820b99bc52c835934990
Workstation Ryzen <fannyfan414@gmail.com>
2021-09-14 13:39:25 +0200

    update source

--- app/Http/Controllers/Economic/EconomicOptimizationController.php
+++ app/Http/Controllers/Economic/EconomicOptimizationController.php
@@ -20,7 +20,7 @@ class EconomicOptimizationController extends Controller
     protected $structureService;
 
     const DATA_SOURCE = 'economic_scenario_KBM_Scenario_Steam_Test_short_v5_gtm_optimize_v2';
-    const DATA_SOURCE_WELL_CHANGES = 'economic_well_changes_scenario_KBM_Scenario_Steam_Test_short_v7';
+    const DATA_SOURCE_WELL_CHANGES = 'economic_well_changes_scenario_KBM_Scenario_Steam_Test_short_v8';
     const DATA_SOURCE_DATE = '2021/01/01';
 
     const SCENARIO_COLUMNS = [
