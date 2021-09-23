1474acae2ed89b89cb1316e60de0d9731b75c299
Rustam Ibragimov <winder84@mail.ru>
2021-09-23 16:57:41 +0300

    небольшой рефакторинг и первая версия загрузки карты и контуров

--- routes/web.php
+++ routes/web.php
@@ -72,13 +72,11 @@ Route::group(
                     'gtm'
                 );
 
-                Route::post('attachments', 'AttachmentController@upload');
-                Route::get('attachments/{attachment}', 'AttachmentController@get');
-
-                Route::post('map_constructor/import', 'MapConstructor@importFile');
                 Route::post('attachments', 'AttachmentController@upload')->name('attachment.upload');
                 Route::get('attachments/file-info/{attachment}', 'AttachmentController@getFileInfo')->name('attachment.get-file-info');
                 Route::get('attachments/{attachment}', 'AttachmentController@download')->name('attachment.download');
+
+                Route::post('map_constructor/import', 'MapConstructor@importFile');
             }
         );
         Auth::routes(
