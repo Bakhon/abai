a3c243fc5dd919ee1cf8eecc2f2f3d049cf20e23
aidarzhussupov <aidarzhussupov@gmail.com>
2021-09-23 11:27:07 +0600

    Добавил парсинг сущнойстей из общей базы ИС Абай для вставки их id

--- app/Imports/PaegtmCarriedOutGtmsImport.php
+++ app/Imports/PaegtmCarriedOutGtmsImport.php
@@ -2,6 +2,10 @@
 
 namespace App\Imports;
 
+use App\Models\BigData\Dictionaries\Geo;
+use App\Models\BigData\Dictionaries\GtmType;
+use App\Models\BigData\Dictionaries\Org;
+use App\Models\BigData\Dictionaries\Well;
 use App\Models\Paegtm\CarriedOutGtm;
 use Carbon\Carbon;
 use ErrorException;
@@ -26,6 +30,21 @@ class PaegtmCarriedOutGtmsImport implements ToModel, WithStartRow, WithBatchInse
         'planned_increase' => 7,
     ];
 
+    private $_orgs;
+    private $_geos;
+    private $_gtm_kinds;
+    private $_wells;
+
+    public function __construct()
+    {
+        $this->_orgs = Org::all();
+        $this->_geos = Geo::all();
+        $this->_gtm_kinds = GtmType::all();
+        $this->_wells = Well::select('id','uwi')->get()->mapWithKeys(function ($item) {
+            return [$item['uwi'] => $item['id']];
+        })->toArray();
+    }
+
     /**
      * @param array $row
      *
@@ -33,13 +52,27 @@ class PaegtmCarriedOutGtmsImport implements ToModel, WithStartRow, WithBatchInse
      */
     public function model(array $row)
     {
+        $dzoNameShort = $row[self::COLUMNS['org_name_short']];
+        $gtmKindName = $row[self::COLUMNS['gtm']];
+        $geoCode = $row[self::COLUMNS['oilfield']];
+        $uwi = $row[self::COLUMNS['uwi']];
+
+        $org = $this->_orgs->where('name_short_ru', $dzoNameShort)->first();
+        $gtmKind = $this->_gtm_kinds->where('name_short_ru', $gtmKindName)->first();
+        $geo = $this->_geos->where('field_code', $geoCode)->first();
+        $well = $this->_wells[$uwi] ?? null;
+
         return new CarriedOutGtm([
+            'org_id' => $org ? $org->id : null,
             'org_name_short' => $row[self::COLUMNS['org_name_short']],
             'oilfield' => $row[self::COLUMNS['oilfield']],
+            'geo_id' => $geo ? $geo->id : null,
             'uwi' => $row[self::COLUMNS['uwi']],
+            'well' => $well,
             'date_stop_before_gtm' => Date::excelToDateTimeObject($row[self::COLUMNS['date_stop_before_gtm']]),
             'date_start_after_gtm' => Date::excelToDateTimeObject($row[self::COLUMNS['date_start_after_gtm']]),
             'gtm' => $row[self::COLUMNS['gtm']],
+            'gtm_type_id' => $gtmKind ? $gtmKind->id : null,
             'planned_increase' => $row[self::COLUMNS['planned_increase']],
         ]);
     }
