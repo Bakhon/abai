390fa426b3ab3edf8baf2a96e31cfef5dedd875e
Ruslan <russel1987@mail.ru>
2021-09-28 07:26:51 +0300

    refactored

--- app/Services/BigData/Forms/DailyReportsFluidProd.php
+++ app/Services/BigData/Forms/DailyReportsFluidProd.php
@@ -4,39 +4,5 @@ namespace App\Services\BigData\Forms;
 
 class DailyReportsFluidProd extends DailyReports
 {
-
     protected $metricCode = 'FLR';
-
-    protected function getData($filter): array {
-        $data = parent::getReports($filter);
-        $result = [];
-        $plan = $data->sum('plan');
-        $fact = $data->sum('fact');
-        switch ($filter->period) {
-            case self::DAY:
-                $result['plan'] = ['value' => $plan];
-                $result['fact'] = $result['daily_fact_cits'] = ['value' => $fact];
-                break;
-            case self::MONTH:
-                $result['month_plan'] = ['value' => $plan];
-                $result['month_fact'] = $result['month_fact_cits'] = ['value' => $fact];
-                break;
-            case self::YEAR:
-                $result['year_plan'] = ['value' => $plan];
-                $result['year_fact'] = $result['year_fact_cits'] = ['value' => $fact];
-                break;
-        }
-
-        if ($filter->type === self::GS) {
-            $result['fact'] = ['value' => 0];
-            $result['month_fact'] = ['value' => 0];
-            $result['year_fact'] = ['value' => 0];
-        }
-        $result['daily_fact_gs'] = ['value' => 0];
-        $result['month_fact_gs'] = ['value' => 0];
-        $result['year_fact_gs'] = ['value' => 0];
-
-        return $result;
-    }
-
 }
