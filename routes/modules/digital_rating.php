22a8fe384cd3ddf05340580e853108de40a8e1c5
Nurzhik <nurzhananuarbek@mail.ru>
2021-09-30 01:59:22 +0600

    develppment/IZ1-86 Fixed search on the horizon. Uploading data on injection wells. Added a route for injection wells

--- routes/modules/digital_rating.php
+++ routes/modules/digital_rating.php
@@ -12,6 +12,7 @@ Route::group(
             Route::get('/reports', function () {
                 return view('digital_rating.reports');
             })->name('digital-rating-report');
-            Route::get('/search_wells', 'DigitalRating\DigitalRatingContoller@search_wells')->name('search_wells');   
+            Route::get('/get_environment_data', 'DigitalRating\DigitalRatingContoller@get_environment_data')->name('search_wells');   
+            Route::get('/get_injection_wells', 'DigitalRating\DigitalRatingContoller@get_injection_wells')->name('get_injection_wells');   
         });
     });
