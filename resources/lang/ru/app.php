87d4e6c1a49a8471c9f89ad5fab78a4b0655af5f
Nurmukhamed <nurmukhamed.ibrahim@gmail.com>
2021-09-30 19:41:23 +0600

    fixed sidebar menu

--- resources/lang/ru/app.php
+++ resources/lang/ru/app.php
@@ -44,7 +44,7 @@ return array (
   'restore' => 'Восстановить',
   'restore_title' => 'Восстановление',
   'rollback' => 'Откатить',
-  'months' => 
+  'months' =>
   array (
     1 => 'Январь',
     2 => 'Февраль',
@@ -62,4 +62,5 @@ return array (
   'module_title' => 'Модуль',
   'download' => 'Скачать',
   'thereIsNoData' => 'Нет данных',
+  'modules' => 'Модули',
 );
