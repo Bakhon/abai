0fa775b94ce32e04a0a1d426935d2d8ab9945eab
Ruslan <russel1987@mail.ru>
2021-10-01 09:30:42 +0300

    fixed daily report forms

--- app/Services/BigData/Forms/DailyReportsGasProduction.php
+++ app/Services/BigData/Forms/DailyReportsGasProduction.php
@@ -18,15 +18,7 @@ class DailyReportsGasProduction extends DailyReports
     protected function getMeasuredFieldValues(Org $org, CarbonImmutable $date): Collection
     {
         $wells = $this->getOrgWells($org, $date);
-        dd(
-            DB::connection('tbd')
-                ->table('prod.meas_gas_prod')
-                ->select('dbeg', 'gas_prod_val', 'well')
-                ->where('dbeg', '>=', $date->startOfYear())
-                ->where('dbeg', '<=', $date->endOfDay())
-                ->whereIn('well', $wells)
-                ->get()
-        );
+
         return DB::connection('tbd')
             ->table('prod.meas_gas_prod')
             ->select('dbeg', 'gas_prod_val', 'well')
