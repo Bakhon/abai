38c0c1a79c04a44edfc1398d129cfbfffbe0bf02
Aip Alisher <alish2621@gmail.com>
2021-10-01 11:33:48 +0600

    Updated

--- app/Services/BigData/Forms/WellTechConnect.php
+++ app/Services/BigData/Forms/WellTechConnect.php
@@ -7,11 +7,4 @@ namespace App\Services\BigData\Forms;
 class WellTechConnect extends PlainForm
 {
     protected $configurationFileName = 'well_tech_connect';
-
-    protected function checkFormPermission(string $action)
-    {
-        // if (auth()->user()->cannot("bigdata {$action} {$this->configurationFileName}")) {
-        //     throw new \Exception("You don't have permissions");
-        // }
-    }
 }
