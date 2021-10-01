9ba08093d5c49d94ec871a1c089543f2dbb13391
Ruslan <russel1987@mail.ru>
2021-10-01 06:55:46 +0300

    fixed date in measurement forms, refactored

--- app/Services/BigData/Forms/MeasurementLogForm.php
+++ app/Services/BigData/Forms/MeasurementLogForm.php
@@ -22,6 +22,43 @@ abstract class MeasurementLogForm extends TableForm
         self::WELL_STATUS_ACCUMULATION
     ];
 
+    public function getResults(): array
+    {
+        $filter = json_decode($this->request->get('filter'));
+        $params['filter']['well_category'] = $this->wellCategories;
+
+        $wells = $this->getWells((int)$this->request->get('id'), $this->request->get('type'), $filter, $params);
+
+        $tables = $this->getFields()->pluck('table')->filter()->unique();
+        $rowData = $this->fetchRowData(
+            $tables,
+            $wells->pluck('id')->toArray(),
+            Carbon::parse($filter->date)
+        );
+
+        $wells->transform(
+            function ($item) use ($rowData) {
+                $result = [];
+
+                foreach ($this->getFields() as $field) {
+                    $fieldValue = $this->getFieldValue($field, $rowData, $item);
+                    if (!empty($fieldValue)) {
+                        $result[$field['code']] = $fieldValue;
+                    }
+                }
+
+                $result['id'] = $result['uwi']['id'];
+                return $result;
+            }
+        );
+
+        $this->addLimits($wells);
+
+        return [
+            'rows' => $wells->toArray()
+        ];
+    }
+
     protected function getCustomFieldValue(array $field, array $rowData, Model $item): ?array
     {
         switch ($field['code']) {
--- app/Services/BigData/Forms/WaterProduction.php
+++ app/Services/BigData/Forms/WaterProduction.php
@@ -4,45 +4,8 @@ declare(strict_types=1);
 
 namespace App\Services\BigData\Forms;
 
-use Carbon\Carbon;
-
 class WaterProduction extends MeasurementLogForm
 {
     protected $configurationFileName = 'water_production';
-
-    public function getResults(): array
-    {
-        $filter = json_decode($this->request->get('filter'));
-        $params['filter']['well_category'] = ['WTR'];
-        $wells = $this->getWells((int)$this->request->get('id'), $this->request->get('type'), $filter, $params);
-
-        $tables = $this->getFields()->pluck('table')->filter()->unique();
-        $rowData = $this->fetchRowData(
-            $tables,
-            $wells->pluck('id')->toArray(),
-            Carbon::parse($this->request->get('date'))
-        );
-
-        $wells->transform(
-            function ($item) use ($rowData) {
-                $result = [];
-
-                foreach ($this->getFields() as $field) {
-                    $fieldValue = $this->getFieldValue($field, $rowData, $item);
-                    if (!empty($fieldValue)) {
-                        $result[$field['code']] = $fieldValue;
-                    }
-                }
-
-                $result['id'] = $result['uwi']['id'];
-                return $result;
-            }
-        );
-
-        $this->addLimits($wells);
-
-        return [
-            'rows' => $wells->toArray()
-        ];
-    }
+    protected $wellCategories = ['WTR'];
 }
