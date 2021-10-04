527ea8770a4d8d37c86e6daf39f95d62449355ff
Кирикбаев Абат Саматович <A.Kirikbayev@niikmg.kz>
2021-10-04 16:25:50 +0600

    bags_сss_front

--- resources/js/components/tkrs/tkrsMain.vue
+++ resources/js/components/tkrs/tkrsMain.vue
@@ -29,8 +29,8 @@ export default {
   },
   data(){
     return {
-      fields: ['№', 'Активность', 'Свзяь', 'Бригада', 'Месторождение', 'Скважина', 'Вид ремонта', 'Оповощение', 'GPS'],
-      items: [['1', 'Есет', 'Ерасыл'],['Абат', 'Есет', 'Ерасыл']]
+      fields: ['№', 'Активность', 'Свзяь', 'Бригада', 'Месторождение', 'Скважина', 'Тип ремонта', 'Вид ремонта', 'Оповощение', 'GPS', 'Видео', 'Датчик силы'],
+      items: [['1', '/img/tkrs/active.svg', '/img/tkrs/connection.svg', 'Бригада №11 ТОО "Жанрос Дриллинг"', "Акшабулак Восточный", 'Акш 40', 'КРС/ПРС', 'Ремонтно-изоляционные работы (РИР)', '/img/tkrs/notification_good.svg', '/img/tkrs/gps.svg', '/img/tkrs/video.svg', '/img/tkrs/force_sensor.svg',],['2', '/img/tkrs/active.svg', '/img/tkrs/no_connection.svg', 'Бригада №11 ТОО "Жанрос Дриллинг"', "Акшабулак Восточный", 'Акш 40', 'КРС/ПРС', 'Ремонтно-изоляционные работы (РИР)', '/img/tkrs/notification_error.svg', '/img/tkrs/gps.svg', '/img/tkrs/video_no.svg', '/img/tkrs/force_sensor.svg',]]
     }
   }
 };
