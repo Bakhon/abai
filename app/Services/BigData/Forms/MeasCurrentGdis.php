49023295ac2326c8f57bd86403cedd2bfeb0eea2
Ayana Adilova <aadilova19@gmail.com>
2021-10-06 00:29:50 +0600

    commit

---
+++ app/Services/BigData/Forms/MeasCurrentGdis.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\BigData\Forms;
+
+class MeasCurrentGdis extends TableForm
+{
+    protected $configurationFileName = 'meas_current_gdis';
+}
