daff24ff375d8aa7aea8c0f36982268a3fb7f439
Ayana Adilova <aadilova19@gmail.com>
2021-10-06 21:08:02 +0600

    commit

--- app/Services/BigData/Forms/GisType.php
+++ app/Services/BigData/Forms/GisType.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 namespace App\Services\BigData\Forms;
 
 use App\Traits\BigData\Forms\WithDocumentsUpload;
-
+use App\Traits\BigData\Forms\DepthValidationTrait;
 class GisType extends PlainForm
 {
     protected $configurationFileName = 'gis_type';
-
+    use DepthValidationTrait;
     use WithDocumentsUpload;
 
     protected function submitForm(): array
@@ -20,5 +20,28 @@ class GisType extends PlainForm
         $gis->methods()->sync($this->request->get('gis_method'));
 
         return $result;
+    }
+    
+    protected function getCustomValidationErrors(string $field = null): array
+    {
+        $errors = [];
+
+        if (!$this->isValidDepth($this->request->get('well'), $this->request->get('income_devices'))) {
+            $errors['income_devices'][] = trans('bd.validation.depth');
+        }
+        if (!$this->isValidDepth($this->request->get('well'), $this->request->get('zp_top'))) {
+            $errors['zp_top'][] = trans('bd.validation.depth');
+        }
+        if (!$this->isValidDepth($this->request->get('well'), $this->request->get('in_top'))) {
+            $errors['in_top'][] = trans('bd.validation.depth');
+        }
+        if (!$this->isValidDepth($this->request->get('well'), $this->request->get('pm_top'))) {
+            $errors['pm_top'][] = trans('bd.validation.depth');
+        }
+        if (!$this->isValidDepth($this->request->get('well'), $this->request->get('ip_top'))) {
+            $errors['ip_top'][] = trans('bd.validation.depth');
+        }       
+
+        return $errors;
     }
 }
