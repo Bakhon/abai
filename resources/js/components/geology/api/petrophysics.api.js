9987f155b77920aaf7ac01c59e450c0b4ca5244c
adikweb <adilhan97kz@gmail.com>
2021-10-11 03:35:47 +0600

    remove comment;

--- resources/js/components/geology/api/petrophysics.api.js
+++ resources/js/components/geology/api/petrophysics.api.js
@@ -3,5 +3,4 @@ import service from "../js/service";
 export const Fetch_DZOS = async () => await service('/dzos/');
 export const Fetch_Fields = async (id) => await service(`/fields/?organization_id=${id}`);
 export const Fetch_Wells = async (id, limit = 500) => await service(`/fields/${id}/wells?limit=${limit}`);
-// export const Fetch_WellsMnemonics = async (ids) => await service(`/wells/mnemonics?well_id=${ids.join('&&well_id=')}`);
 export const Fetch_WellsMnemonics = async (ids) => await service(`/wells/mnemonics?well_id=${[1037, 1038, 1337].join('&&well_id=')}`); // TODO Переделать под api.
