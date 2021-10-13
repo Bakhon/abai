932c790652d7aebe2b8ead7a37b21ab0765b3a72
Workstation Ryzen <fannyfan414@gmail.com>
2021-10-13 14:31:18 +0200

    design fix

--- resources/js/components/Economic/components/nrs/TableMatrix.vue
+++ resources/js/components/Economic/components/nrs/TableMatrix.vue
@@ -1221,14 +1221,18 @@ export default {
           <html xmlns:o="urn:schemas-microsoft-com:office:office"
                   xmlns:x="urn:schemas-microsoft-com:office:excel"
                   xmlns="http://www.w3.org/TR/REC-html40">
-              <head><!--[if gte mso 9]><xml><x:ExcelWorkbook>
-                    <x:ExcelWorksheets><x:ExcelWorksheet>
-                    <x:Name>${worksheetName}
-                    </x:Name>
-                    <x:WorksheetOptions><x:DisplayGridlines/>
-                    </x:WorksheetOptions></x:ExcelWorksheet>
-                    </x:ExcelWorksheets>
-                    </x:ExcelWorkbook></xml><![endif]-->
+              <head><!--[if gte mso 9]>
+                    <xml>
+                      <x:ExcelWorkbook>
+                        <x:ExcelWorksheets>
+                          <x:ExcelWorksheet>
+                            <x:Name>${worksheetName}</x:Name>
+                            <x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions>
+                          </x:ExcelWorksheet>
+                        </x:ExcelWorksheets>
+                      </x:ExcelWorkbook>
+                    </xml>
+                    <![endif]-->
                 <meta http-equiv="content-type" content="text/plain; charset=UTF-8"/>
               </head>
               <body><table>${htmlTable}</table></body>
