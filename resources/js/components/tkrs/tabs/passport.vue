3313de2511b2e38a95dfbca8f20ed5cbe086d551
Кирикбаев Абат Саматович <A.Kirikbayev@niikmg.kz>
2021-10-17 12:09:46 +0600

    tkrs start

---
+++ resources/js/components/tkrs/tabs/passport.vue
@@ -0,0 +1,139 @@
+<template>
+  <div>
+     <div class="table-div">
+            <div>
+              <table>
+                <thead>
+                  <!-- <tr>
+                    <th>№</th>
+                    <th>Активность</th>
+                    <th>Связь</th>
+                  </tr> -->
+                </thead>
+                <tbody>
+                  
+                    <tr>
+                      <td style="width: 5px; background: #333975;">1</td>
+                      <td style="font-weight: bold; font-size: 16px;">Скважина</td>
+                      <td>АКН_0453</td>
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">2</td>
+                      <td style="font-weight: bold; font-size: 16px;">Вид скважины</td>
+                      <td>Горизонтальная</td>
+        
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">3</td>
+                      <td style="font-weight: bold; font-size: 16px;">Месторождение</td>
+                      <td>Акшабулак Центральный</td>
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">4</td>
+                      <td style="font-weight: bold; font-size: 16px;">Блок</td>
+                      <td>1 объект</td>
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">5</td>
+                      <td style="font-weight: bold; font-size: 16px;">Горизонт</td>
+                      <td>Горизонтальная</td>
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">6</td>
+                      <td style="font-weight: bold; font-size: 16px;">Тех.структура</td>
+                      <td>М-1</td>
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">7</td>
+                      <td style="font-weight: bold; font-size: 16px;">Орг.структура</td>
+                      <td>ЦППН</td>
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">8</td>
+                      <td style="font-weight: bold; font-size: 16px;">Зона скважины</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">9</td>
+                      <td style="font-weight: bold; font-size: 16px;">Альтитуда устья скважины</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">10</td>
+                      <td style="font-weight: bold; font-size: 16px;">Координаты Х (устья)</td>
+                      <td>125</td>
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">11</td>
+                      <td style="font-weight: bold; font-size: 16px;">Координаты Y (устья)</td>
+                      <td>65</td>
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">12</td>
+                      <td style="font-weight: bold; font-size: 16px;">Координаты забоя X</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">13</td>
+                      <td style="font-weight: bold; font-size: 16px;">Координаты забоя Y</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">14</td>
+                      <td style="font-weight: bold; font-size: 16px;">Назначение скважины по проекту</td>
+                      <td>Контрольная</td>
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">15</td>
+                      <td style="font-weight: bold; font-size: 16px;">Категория</td>
+                      <td>Контрольная</td>
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">16</td>
+                      <td style="font-weight: bold; font-size: 16px;">Дата ввода в эксплуатацию</td>
+                      <td>09.02.2018</td>
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">17</td>
+                      <td style="font-weight: bold; font-size: 16px;">Буровой подрядчик</td>
+                      <td>ТОО “Сибу Кызылорда”</td>
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">18</td>
+                      <td style="font-weight: bold; font-size: 16px;">Проектная глубина</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">19</td>
+                      <td style="font-weight: bold; font-size: 16px;">Радиус дренирования</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">20</td>
+                      <td style="font-weight: bold; font-size: 16px;">Состояние</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td style="width: 5px; background: #333975;">21</td>
+                      <td style="font-weight: bold; font-size: 16px;">Способ эксплуатации</td>
+                      <td></td>
+                    </tr>
+
+                  
+                  
+                </tbody>
+              </table>
+            </div>
+      </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style lang="scss" scoped src="../BaseTableStyles.scss"></style>
