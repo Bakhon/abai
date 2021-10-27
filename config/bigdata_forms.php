dac5e7d32c94d97562e77efc04d0e13848135f72
Ayana Adilova <aadilova19@gmail.com>
2021-10-27 10:21:57 +0600

    commit

--- config/bigdata_forms.php
+++ config/bigdata_forms.php
@@ -77,4 +77,5 @@
   'tech_mode_prod' => '\\App\\Services\\BigData\\Forms\\TechModeProd',
   'tech_mode_inj' => '\\App\\Services\\BigData\\Forms\\TechModeInj',
   'water_production_month' => '\\App\\Services\\BigData\\Forms\\WaterProductionMonth',
+  'production_plan' => '\\App\\Services\\BigData\\Forms\\ProductionPlan',
 );
