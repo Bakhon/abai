e92e5c0cfe4e7aec0e1e4437e0852307f906343b
aidarzhussupov <aidarzhussupov@gmail.com>
2021-11-01 17:07:41 +0600

    Правки по импорту фактически проведенных ГТМ

--- app/Imports/PaegtmCarriedOutGtmsImport.php
+++ app/Imports/PaegtmCarriedOutGtmsImport.php
@@ -3,7 +3,7 @@
 namespace App\Imports;
 
 use App\Models\BigData\Dictionaries\Geo;
-use App\Models\BigData\Dictionaries\GtmType;
+use App\Models\BigData\Dictionaries\GtmKind;
 use App\Models\BigData\Dictionaries\Org;
 use App\Models\BigData\Dictionaries\Well;
 use App\Models\Paegtm\CarriedOutGtm;
@@ -23,11 +23,11 @@ class PaegtmCarriedOutGtmsImport implements ToModel, WithStartRow, WithBatchInse
     const COLUMNS = [
         'org_name_short' => 0,
         'oilfield' => 1,
-        'uwi' => 3,
-        'date_stop_before_gtm' => 4,
-        'date_start_after_gtm' => 5,
-        'gtm' => 6,
-        'planned_increase' => 7,
+        'uwi' => 2,
+        'date_stop_before_gtm' => 3,
+        'date_start_after_gtm' => 4,
+        'gtm' => 5,
+        'planned_increase' => 6,
     ];
 
     private $_orgs;
@@ -39,7 +39,7 @@ class PaegtmCarriedOutGtmsImport implements ToModel, WithStartRow, WithBatchInse
     {
         $this->_orgs = Org::all();
         $this->_geos = Geo::all();
-        $this->_gtm_kinds = GtmType::all();
+        $this->_gtm_kinds = GtmKind::all();
         $this->_wells = Well::select('id','uwi')->get()->mapWithKeys(function ($item) {
             return [$item['uwi'] => $item['id']];
         })->toArray();
@@ -69,10 +69,10 @@ class PaegtmCarriedOutGtmsImport implements ToModel, WithStartRow, WithBatchInse
             'geo_id' => $geo ? $geo->id : null,
             'uwi' => $row[self::COLUMNS['uwi']],
             'well' => $well,
-            'date_stop_before_gtm' => Date::excelToDateTimeObject($row[self::COLUMNS['date_stop_before_gtm']]),
-            'date_start_after_gtm' => Date::excelToDateTimeObject($row[self::COLUMNS['date_start_after_gtm']]),
+            'date_stop_before_gtm' => $row[self::COLUMNS['date_stop_before_gtm']] ? Date::excelToDateTimeObject($row[self::COLUMNS['date_stop_before_gtm']]) : null,
+            'date_start_after_gtm' => $row[self::COLUMNS['date_start_after_gtm']] ? Date::excelToDateTimeObject($row[self::COLUMNS['date_start_after_gtm']]) : null,
             'gtm' => $row[self::COLUMNS['gtm']],
-            'gtm_type_id' => $gtmKind ? $gtmKind->id : null,
+            'gtm_kind_id' => $gtmKind ? $gtmKind->id : null,
             'planned_increase' => $row[self::COLUMNS['planned_increase']],
         ]);
     }
---
+++ app/Models/BigData/Dictionaries/GtmKind.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\BigData\Dictionaries;
+
+use App\Models\TBDModel;
+
+class GtmKind extends TBDModel
+{
+    protected $table = 'dict.gtm_kind';
+}
