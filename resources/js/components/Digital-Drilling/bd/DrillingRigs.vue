a31b8cb47095b50bd6faf227c8243d224a0ac4b9
Bekarysalashybayev <bekarysalashybaev@gmail.com>
2021-11-02 09:44:31 +0600

    added operationg costs

--- resources/js/components/Digital-Drilling/bd/DrillingRigs.vue
+++ resources/js/components/Digital-Drilling/bd/DrillingRigs.vue
@@ -130,6 +130,29 @@
                 <p class="num"><span>14251</span>метров</p>
                 <p class="name"><img src="/img/digital-drilling/drilling-all.svg" alt=""><span>{{ trans('digital_drilling.default.total_drilled') }}</span></p>
             </div>
+            <div class="operatingCosts">
+                <div class="operatingCosts-title">
+                    {{ trans('digital_drilling.default.EMG_development_drilling') }}
+                </div>
+                <div class="operatingCosts-statistics">
+                    <div class="operatingCosts-single" v-for="costs in operatingCosts">
+                        <div class="operatingCosts-single-title">
+                            {{costs.year}}
+                        </div>
+                        <div class="operatingCosts-single-content">
+                            <progress max="20000000" :value="costs.item">
+                                {{costs.item}}
+                            </progress>
+                            <div class="value">
+                                {{costs.item}},00
+                            </div>
+                        </div>
+                        <div class="operatingCosts-single-tg">
+                            {{ trans('digital_drilling.default.thousands_tenge') }}
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="analyticsBlock">
                 <div class="techNumsBlock">
                     <p class="name">{{ trans('digital_drilling.default.technical_and_economic') }}</p>
@@ -276,7 +299,33 @@
                 characteristicGraph: false,
                 characteristicScheme: false,
                 rigs: [],
-                technicalDescription: []
+                technicalDescription: [],
+                operatingCosts: [
+                    {
+                        year: '2016 г.',
+                        item: 8228564
+                    },
+                    {
+                        year: '2017 г.',
+                        item: 8785866
+                    },
+                    {
+                        year: '2018 г.',
+                        item: 7947803
+                    },
+                    {
+                        year: '2019 г.',
+                        item: 14397692
+                    },
+                    {
+                        year: '2020 г.',
+                        item: 15093826
+                    },
+                    {
+                        year: '2021 г.',
+                        item: 10325881
+                    },
+                ]
             }
         },
         mounted(){
@@ -405,7 +454,7 @@
     .rigs__content{
         background-color: #272953;
         width: 100%;
-        height: 675px;
+        height: 916px;
         padding: 0 4px 0;
         position: relative;
         overflow-x: hidden;
