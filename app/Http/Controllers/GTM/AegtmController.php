8cf3cc93553f3373d6cb5a0c1537e32eae044f88
aidarzhussupov <aidarzhussupov@gmail.com>
2021-11-04 16:55:49 +0600

    Рефакторинг кода получения данных по комбинированному расчету

--- app/Http/Controllers/GTM/AegtmController.php
+++ app/Http/Controllers/GTM/AegtmController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\GTM;
 use App\Http\Controllers\Controller;
 use App\Models\Paegtm\DzoAegtm;
 use App\Models\Paegtm\TechEfficiency;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -21,58 +22,16 @@ class AegtmController extends Controller
     {
         $result = [];
 
-        //TODO: вынести в отдельный метод
-        $techEfficiencyQuery = TechEfficiency::query();
+        $techEfficiencyData = $this->getTechEfficiencyData($request);
+        $dzoOtmData = $this->getDzoOtmData($request);
 
-        $techEfficiencyQuery->select(
-            'gtm',
-            'uwi',
-            DB::raw('sum(actual_production_month) as sum_actual_production_month'),
-            DB::raw('avg(cast(NULLIF(actual_increase_month, 0) AS DOUBLE PRECISION)) AS avg_actual_increase_month')
-        );
-
-        $techEfficiencyQuery->when($request->filled('dzoName'), function ($q) use ($request) {
-            return $q->where('org_name_short', $request->dzoName);
-        });
-
-
-        $techEfficiencyQuery->when($request->filled('dateStart') && $request->filled('dateEnd'), function ($q) use ($request) {
-            return $q->whereBetween('month', [
-                $request->input('dateStart'),
-                $request->input('dateEnd'),
-            ])
-            ->whereBetween('date_start_after_gtm', [
-                $request->input('dateStart'),
-                $request->input('dateEnd'),
-            ]);
-        });
-
-        $techEfficiencyResult = $techEfficiencyQuery->groupBy('gtm', 'uwi')->get();
-
-        $techEfficiencyResultGrouped = $techEfficiencyResult->groupBy('gtm')->toArray();
-
-        $techEfficiencyResult = $techEfficiencyResult->groupBy('gtm')->map->count();
-
-        //TODO: вынести в отдельный метод
-        $dzoAegtmQuery = DzoAegtm::query();
-
-        $dzoAegtmQuery->when($request->filled('dzoName'), function ($q) use ($request) {
-            return $q->where('org_name_short', $request->dzoName);
-        });
-
-        $dzoAegtmQuery->when($request->filled('dateStart') && $request->filled('dateEnd'), function ($q) use ($request) {
-            return $q->whereBetween('date', [
-                $request->input('dateStart'),
-                $request->input('dateEnd'),
-            ]);
-        });
-
-        $dzoAegtmResult = $dzoAegtmQuery->orderBy('date')->get();
+        $techEfficiencyResultGrouped = $techEfficiencyData->groupBy('gtm')->toArray();
+        $techEfficiencyResult = $techEfficiencyData->groupBy('gtm')->map->count();
 
         $pvrPlan = $grpPlan = $vpsPlan = $pvlgPlan = $vnsPlan = $vnsGrpPlan = $vbdPlan = $zbgsPlan = $zbsPlan = $gsPlan = $gsGrpPlan = 0;
         $pvrProdPlan = $grpProdPlan = $vpsProdPlan = $pvlgProdPlan = $vnsProdPlan = $vnsGrpProdPlan = $vbdProdPlan = $zbgsProdPlan = $zbsProdPlan = $gsProdPlan = $gsGrpProdPlan = 0;
 
-        foreach ($dzoAegtmResult as $item) {
+        foreach ($dzoOtmData as $item) {
             $pvrPlan+= $item->pvr_plan;
             $grpPlan+= $item->grp_plan;
             $vpsPlan+= $item->vps_plan;
@@ -165,5 +124,61 @@ class AegtmController extends Controller
         }
 
         return response()->json($result);
+    }
+
+    /**
+     * @param Request $request
+     * @return Collection
+     */
+    public function getTechEfficiencyData(Request $request): Collection
+    {
+        $techEfficiencyQuery = TechEfficiency::query();
+
+        $techEfficiencyQuery->select(
+            'gtm',
+            'uwi',
+            DB::raw('sum(actual_production_month) as sum_actual_production_month'),
+            DB::raw('avg(cast(NULLIF(actual_increase_month, 0) AS DOUBLE PRECISION)) AS avg_actual_increase_month')
+        );
+
+        $techEfficiencyQuery->when($request->filled('dzoName'), function ($q) use ($request) {
+            return $q->where('org_name_short', $request->dzoName);
+        });
+
+
+        $techEfficiencyQuery->when($request->filled('dateStart') && $request->filled('dateEnd'), function ($q) use ($request) {
+            return $q->whereBetween('month', [
+                $request->input('dateStart'),
+                $request->input('dateEnd'),
+            ])
+                ->whereBetween('date_start_after_gtm', [
+                    $request->input('dateStart'),
+                    $request->input('dateEnd'),
+                ]);
+        });
+
+        return $techEfficiencyQuery->groupBy('gtm', 'uwi')->get();
+    }
+
+    /**
+     * @param Request $request
+     * @return Collection
+     */
+    public function getDzoOtmData(Request $request): Collection
+    {
+        $dzoAegtmQuery = DzoAegtm::query();
+
+        $dzoAegtmQuery->when($request->filled('dzoName'), function ($q) use ($request) {
+            return $q->where('org_name_short', $request->dzoName);
+        });
+
+        $dzoAegtmQuery->when($request->filled('dateStart') && $request->filled('dateEnd'), function ($q) use ($request) {
+            return $q->whereBetween('date', [
+                $request->input('dateStart'),
+                $request->input('dateEnd'),
+            ]);
+        });
+
+        return $dzoAegtmQuery->orderBy('date')->get();
     }
 }
