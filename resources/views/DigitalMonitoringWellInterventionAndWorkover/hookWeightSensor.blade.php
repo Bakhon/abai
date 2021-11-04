00dadee38f853f5aba195f326c82001f6dec4a58
Кирикбаев Абат Саматович <A.Kirikbayev@niikmg.kz>
2021-11-04 16:46:33 +0600

    graph_css

--- resources/views/DigitalMonitoringWellInterventionAndWorkover/hookWeightSensor.blade.php
+++ resources/views/DigitalMonitoringWellInterventionAndWorkover/hookWeightSensor.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.tkrs')
 @section('content')
-    <div class="col px-4 tkrs_page"  >
+    <div class="pip-4 tkrs_page"  >
         <a href="{{url('/')}}/ru/export" class="float-right">
         </a>
-        <div class="level1-content row">
-            <div class="main col-md-12 col-lg-12 row">
+        <div class="level1-content row-tkrs">
+            <div class="main md-12 lg-12 row-tkrs">
             <hook-weight-sensor></hook-weight-sensor>
 
             </div>
@@ -12,13 +12,23 @@
     </div>
 @endsection
 <style>
-    .p-4{
+    .pip-4{
         background-color: #0F1430;
+        width: 100%;
+        height: 100%;
     }
     .title, .subtitle {
         color:white;
     }
     .top{
         display: none;
+    }
+    .row-tkrs {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    }
+    .level1-content {
+    width: 100%;
     }
 </style>
