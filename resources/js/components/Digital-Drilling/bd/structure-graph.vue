6a468a55e23dd7346ae6f4e593df16d0e1e79727
Bekarysalashybayev <bekarysalashybaev@gmail.com>
2021-11-11 16:59:08 +0600

    checnged style of strucuture

--- resources/js/components/Digital-Drilling/bd/structure-graph.vue
+++ resources/js/components/Digital-Drilling/bd/structure-graph.vue
@@ -14,11 +14,12 @@
 
 <style scoped>
 .structure-graph{
-    background: #272953;
     width: 100%;
     height: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
+    margin-top: 100px;
+
 }
 </style>
