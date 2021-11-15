7ac1d097d3e4e1e41a1f93b1acb1b4d124ffb79b
Aip Alisher <alish2621@gmail.com>
2021-11-15 10:54:09 +0600

    Updated

--- resources/js/components/reportConstructor/ReportHeaderNode.vue
+++ resources/js/components/reportConstructor/ReportHeaderNode.vue
@@ -32,6 +32,8 @@
             @input="updateChildNode"
             :group="group"
             :rowKey="rowKey"
+            :updateThisComponent="updateThisComponent"
+            :renderComponent="renderComponent"
         >
           <span>{{ translateAttribute(item.label) }}</span>
         </ReportHeaderNode>
@@ -71,6 +73,8 @@ export default {
       default: "label",
     },
     translateAttribute: Function,
+    renderComponent: Number,
+    updateThisComponent: Function,
   },
   data() {
     return {
@@ -123,7 +127,39 @@ export default {
       } else {
         this.headerNode.isChecked = !this.headerNode.isChecked
       }
+
+      this.updateChildren(this.headerNode);
+      this.updateParent(this.headerNode.isChecked);
+      this.updateThisComponent();
     },
+    updateChildren(headerNode) {
+      if(!headerNode?.children) return;
+      for(let child of headerNode.children) {
+        child.isChecked = headerNode.isChecked;
+        this.updateChildren(child);
+      }
+    },
+    updateParent(val) {
+      let content = this.$parent;
+      while(!!content) {
+        if(!content.headerNode) {
+          content = content.$parent;
+          continue;
+        }
+        if(!val && this.hasSelectedChildren(content.headerNode)) {
+          break;
+        }
+        content.headerNode.isChecked = val;
+        content = content.$parent;
+      }
+    },
+    hasSelectedChildren(headerNode) {
+      if(!headerNode?.children) return false;
+      for(let child of headerNode.children) {
+        if(child.isChecked || this.hasSelectedChildren(child)) return true;
+      }
+      return false;
+    }
   },
 };
 </script>
