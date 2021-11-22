d73d599de33501757075dac6f6a23671984f43cc
Workstation Ryzen <fannyfan414@gmail.com>
2021-11-22 14:31:52 +0200

    add economic calculations

--- app/Http/Controllers/Economic/EconomicOptimizationController.php
+++ app/Http/Controllers/Economic/EconomicOptimizationController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\Economic;
 use App\Http\Controllers\Controller;
 use App\Models\BigData\Well;
 use App\Models\EcoRefsCost;
+use App\Models\EcoRefsDiscontCoefBar;
+use App\Models\EcoRefsEmpPer;
+use App\Models\EcoRefsMacro;
+use App\Models\EcoRefsNdoRates;
+use App\Models\EcoRefsTarifyTn;
 use App\Models\Refs\EcoRefsGtm;
 use App\Models\Refs\Org;
 use App\Services\BigData\StructureService;
@@ -351,5 +356,169 @@ class EconomicOptimizationController extends Controller
             $column . self::SUFFIX_PROFITLESS_CAT_1 => $column . self::SUFFIX_PROFITLESS_CAT_1 . self::SUFFIX_OPTIMIZE,
             $column . self::SUFFIX_PROFITLESS_CAT_2 => $column . self::SUFFIX_PROFITLESS_CAT_2 . self::SUFFIX_OPTIMIZE,
         ];
+    }
+
+    public function getEconomicData()
+    {
+        $scFa = 51;
+
+        $defaultScFa = 1;
+
+        $saleShares = EcoRefsEmpPer::query()
+            ->selectRaw(DB::raw("
+                company_id,
+                date,
+                {$this->sqlSumByRoute('emp_per', 'sale_share_')}
+            "))
+            ->whereRaw(DB::raw("sc_fa = $scFa"))
+            ->groupByRaw(DB::raw("company_id, date"))
+            ->toSql();
+
+        $discounts = EcoRefsDiscontCoefBar::query()
+            ->selectRaw(DB::raw("
+                company_id,
+                date,
+                {$this->sqlSumByRoute('discont', 'discount_')},
+                {$this->sqlSumByRoute('macro', 'price_')},
+                {$this->sqlSumByRoute('barr_coef', 'Barrel_ratio_')}
+            "))
+            ->whereRaw(DB::raw("sc_fa = $scFa"))
+            ->groupByRaw(DB::raw("company_id, date"))
+            ->toSql();
+
+        $transExpenditures = EcoRefsTarifyTn::query()
+            ->selectRaw(DB::raw("
+                company_id,
+                date,
+                {$this->sqlSumByRoute('tn_rate', 'trans_exp_')}
+            "))
+            ->whereRaw(DB::raw("sc_fa = $scFa"))
+            ->groupByRaw(DB::raw("company_id, date"))
+            ->toSql();
+
+        $tableNdoRates = (new EcoRefsNdoRates())->getTable();
+
+        $tableMacros = (new EcoRefsMacro())->getTable();
+
+        $tableCosts = DB::table((new EcoRefsCost())->getTable())
+            ->selectRaw(DB::raw("
+                company_id,
+                date,
+                variable,
+                variable_processing,
+                fix_noWRpayroll,
+                fix_payroll,
+                fix_nopayroll,
+                pes_id,
+                fix,
+                gaoverheads,
+                wr_nopayroll,
+                wr_payroll,
+                wo,
+                amort
+            "))
+            ->whereRaw(DB::raw("sc_fa = $scFa"))
+            ->toSql();
+
+        $costs = DB::table(DB::raw("($tableCosts) as costs"))
+            ->selectRaw(DB::raw("
+                costs.company_id,
+                costs.date,
+                costs.variable AS cost_variable,
+                costs.variable_processing as cost_variable_processing,
+                costs.fix_noWRpayroll AS cost_fix_noWRpayroll,
+                costs.fix_payroll AS cost_fix_payroll,
+                costs.fix_nopayroll AS cost_fix_nopayroll,
+                costs.pes_id,
+                costs.fix,
+                costs.gaoverheads AS cost_Gaoverheads,
+                costs.wr_nopayroll AS cost_WR_nopayroll,
+                costs.wr_payroll AS cost_WR_payroll,
+                costs.wr_nopayroll + wr_payroll as cost_WR,
+                costs.wo AS cost_WO,
+                costs.amort AS cost_amort,
+                ndo_rates.ndo_rates,
+                macros.barrel_world_price AS price_export_world,
+                macros.ex_rate_dol AS currency_ratio,
+                sale_shares.*,
+                discounts.*,
+                trans_expenditures.*
+            "))
+            ->leftJoin("$tableNdoRates as ndo_rates", function ($join) use ($scFa) {
+                $join
+                    ->on(DB::raw("costs.company_id"), '=', DB::raw("ndo_rates.company_id"))
+                    ->whereRaw(DB::raw("ndo_rates.sc_fa = $scFa"));
+            })
+            ->leftJoin("$tableMacros as macros", function ($join) use ($scFa) {
+                $join
+                    ->on(DB::raw("costs.date"), '=', DB::raw("macros.date"))
+                    ->whereRaw(DB::raw("macros.sc_fa = $scFa"));
+            })
+            ->leftJoin(DB::raw("($saleShares) as sale_shares"), function ($join) {
+                $join
+                    ->on(DB::raw("costs.company_id"), '=', DB::raw("sale_shares.company_id"))
+                    ->on(DB::raw("costs.date"), '=', DB::raw("sale_shares.date"));
+            })
+            ->leftJoin(DB::raw("($discounts) as discounts"), function ($join) {
+                $join
+                    ->on(DB::raw("costs.company_id"), '=', DB::raw("discounts.company_id"))
+                    ->on(DB::raw("costs.date"), '=', DB::raw("discounts.date"));
+            })
+            ->leftJoin(DB::raw("($transExpenditures) as trans_expenditures"), function ($join) use ($scFa) {
+                $join
+                    ->on(DB::raw("costs.company_id"), '=', DB::raw("trans_expenditures.company_id"))
+                    ->on(DB::raw("costs.date"), '=', DB::raw("trans_expenditures.date"));
+            })
+            ->get();
+
+        return $costs;
+    }
+
+    private function sqlMapCompanyOrg(string $alias): string
+    {
+        $companyMap = [
+            5 => 2, // OZEN
+            6 => 3, // EMBA
+            7 => 2000000000004, // MANGISTAU
+            8 => 5000001017, // KBM
+            9 => 5000002020, // KAZGER
+        ];
+
+        $sqlQuery = "";
+
+        foreach ($companyMap as $companyId => $tbdOrgId) {
+            $sqlQuery .= "WHEN $alias.company_id = $companyId THEN $alias.$tbdOrgId ";
+        }
+
+        return "CASE $sqlQuery END";
+    }
+
+    private function sqlSumByRoute(string $sumKey, string $prefix): string
+    {
+        $routeMap = [
+            1 => 'export_AA',
+            2 => 'export_KTK',
+            3 => 'export_Samara',
+            4 => 'local_ANPZ',
+            5 => 'local_PNHZ',
+            6 => 'local_PKOP',
+            7 => 'local_KBITUM',
+            9 => 'export_Aktau',
+            10 => 'export_other',
+            11 => 'local_other',
+        ];
+
+        $sqlQuery = "";
+
+        foreach ($routeMap as $routeId => $routeName) {
+            $sqlQuery .= "
+                SUM(CASE 
+                    WHEN route_id = $routeId 
+                    THEN $sumKey
+                    ELSE 0 
+                END) as '$prefix$routeName',";
+        }
+
+        return substr($sqlQuery, 0, -1);
     }
 }
