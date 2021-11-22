9040f4c67fec23fa78343fe808fb8af84e32a3ee
Ruslan <russel1987@mail.ru>
2021-11-22 16:44:06 +0300

    updated well perf actual intervals logic

--- app/Services/BigData/DictionaryService.php
+++ app/Services/BigData/DictionaryService.php
@@ -387,7 +387,7 @@ class DictionaryService
         $cacheKey = 'bd_dict_' . $dict;
 
         if ($this->cache->has($cacheKey)) {
-            return $this->cache->get($cacheKey);
+//            return $this->cache->get($cacheKey);
         }
 
         if (key_exists($dict, self::DICTIONARIES)) {
@@ -621,16 +621,13 @@ class DictionaryService
 
     public function getUserGeoPermissionIds() {
         $orgIds = $this->getUserOrgPermissionIds();
-        $result = [];
-        foreach($orgIds as $id) {
-            $itemElements = DB::connection('tbd')
-                ->table('prod.org_geo as og')
-                ->select('og.geo as geo')
-                ->where('og.org', $id)
-                ->get()
-                ->toArray();
-            $result = array_merge($result, $itemElements);
-        }
+        $result = DB::connection('tbd')
+            ->table('prod.org_geo as og')
+            ->select('og.geo as geo')
+            ->whereIn('og.org', $orgIds)
+            ->get()
+            ->toArray();
+
         $result = array_unique(array_map(function ($item) {
             return (int)$item->geo;
         }, $result));
@@ -674,33 +671,30 @@ class DictionaryService
         return $result;
     }
 
-    public function getUserOrgPermissionIds() {
-        $orgIds = array_filter(auth()->user()->org_structure, function($item) {
+    public function getUserOrgPermissionIds()
+    {
+        $orgIds = array_filter(auth()->user()->org_structure, function ($item) {
             return substr($item, 0, strpos($item, ":")) == 'org';
         });
         $orgIds = array_map(function ($item) {
             return (int)substr($item, strpos($item, ":") + 1);
         }, $orgIds);
-        $organizations = [];
-        foreach($orgIds as $id) {
-            $organization = Org::find($id);
-            if(isset($organization)) {
-                $organizations[] = $organization;
-            }
-        }
 
-        $orgIds = $this->getOrgWithChildrens($organizations);
+        $parentOrganizations = Org::whereIn('id', $orgIds)->get();
+        $allOrganizations = Org::select('id', 'parent')->get();
+
+        $orgIds = $this->getOrgWithChildren($parentOrganizations, $allOrganizations);
         return $orgIds;
     }
 
-    private function getOrgWithChildrens($organizations) {
+    private function getOrgWithChildren($parentOrganizations, $allOrganizations)
+    {
         $result = [];
-        foreach($organizations as $organization) {
-            if(!isset($organization->id)) continue;
+        foreach ($parentOrganizations as $organization) {
             $result[] = $organization->id;
-            $children = $organization->children()->get();
-            
-            $result = array_merge($result, $this->getOrgWithChildrens($children));
+            $children = $allOrganizations->where('parent', $organization->id);
+
+            $result = array_merge($result, $this->getOrgWithChildren($children, $allOrganizations));
         }
 
         return $result;
