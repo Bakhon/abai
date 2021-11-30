3cebfcde431ff76af5ebffab8bdfc20d3619a48b
Ruslan <russel1987@mail.ru>
2021-11-30 15:38:09 +0300

    fixed dbeg, dend dates in prod.tech_mode_prod_oil tables

--- app/Services/BigData/Forms/TechModeForm.php
+++ app/Services/BigData/Forms/TechModeForm.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Services\BigData\Forms;
 
-use Illuminate\Support\Facades\DB;
-
 abstract class TechModeForm extends TableForm
 {
     public function submitForm(array $fields, array $filter = []): array
@@ -14,33 +12,14 @@ abstract class TechModeForm extends TableForm
             foreach ($wellFields as $columnCode => $field) {
                 $column = $this->getFieldByCode($columnCode);
 
-                $row = DB::connection('tbd')
-                    ->table($column['table'])
-                    ->where('well', $wellId)
-                    ->where('dbeg', $filter['date'])
-                    ->where('dend', $filter['date'])
-                    ->first();
-                if (!empty($row)) {
-                    DB::connection('tbd')
-                        ->table($column['table'])
-                        ->where('id', $row->id)
-                        ->update(
-                            [
-                                $column['column'] => $field['value']
-                            ]
-                        );
-                } else {
-                    $data = [
-                        'well' => $wellId,
-                        'dbeg' => $filter['date'],
-                        'dend' => $filter['date'],
-                        $column['column'] => $field['value']
-                    ];
-
-                    DB::connection('tbd')
-                        ->table($column['table'])
-                        ->insert($data);
-                }
+                $this->insertValueInCell(
+                    $column['table'],
+                    $column['column'],
+                    null,
+                    $wellId,
+                    $filter['date'],
+                    $field['value']
+                );
             }
         }
         return [];
