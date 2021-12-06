84c90563056e4db843708b6497666fa796752f41
N.Zhumadil <Nurbest1>
2021-12-06 09:25:00 +0600

    addEditDate

--- resources/js/components/tr/tr.vue
+++ resources/js/components/tr/tr.vue
@@ -558,7 +558,7 @@
               </a>
 
               <a
-                v-if="!isEdit && isPermission && isMaxDate"
+                v-if="!isEdit && isPermission && isMaxDate && isEditable"
                 v-bind:title="trans('tr.edit')"
                 style="cursor: pointer;"
                 data-toggle="tooltip"
