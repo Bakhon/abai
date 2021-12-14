c51602b77e573d0a37587f96fc0669aea7267cd8
Nurzhik <nurzhananuarbek@mail.ru>
2021-12-15 01:50:15 +0600

    AK-1246 update design

--- resources/js/components/reportConstructor/ReportHeaderNode.vue
+++ resources/js/components/reportConstructor/ReportHeaderNode.vue
@@ -186,13 +186,13 @@ export default {
     padding-left: 0;
 }
 .cmp-tree-container {
-    padding-left: 20px;
+    padding-left: 25px;
 }
 .cmp-node label {
       margin-bottom: 0;
       display: block;
       height: auto;
-      margin-right: 4px;
+      margin-right: 30px;
 }
 .cmp-drag-node {
   background-color: #768487;
