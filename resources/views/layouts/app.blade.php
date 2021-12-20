993ca72ad725f8ccb54754580d9d970ba273dfd7
Бахыт <bahyt@MacBook-Air-Bahyt.local>
2021-12-20 09:12:22 +0600

    add change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,7 @@
 
     <script src="{{ asset('js/jquery.js') }}"></script>
     <script src="{{ route('assets.lang') }}"></script>
+    <script src="https://unpkg.com/xlsx/dist/xlsx.full.min.js"></script>
 </head>
 
 <body class="@yield('body_class')">
