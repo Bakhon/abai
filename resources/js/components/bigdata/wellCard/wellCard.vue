0bb2b10de7ea7518156df3249b86a9c4ade53c9d
IgorSanych <djcold15@gmail.com>
2021-12-29 09:34:15 +0600

    AK-1451 Edit Excel File

--- resources/js/components/bigdata/wellCard/wellCard.vue
+++ resources/js/components/bigdata/wellCard/wellCard.vue
@@ -806,15 +806,7 @@ export default {
       }
     },
 
-    // ExportToExcel(type, fn, dl) {
-
     ExportToExcel(tbl_exporttable_to_xls, filename = "") {
-      //   var elt = document.getElementById("tbl_exporttable_to_xls");
-      //   var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
-      //   return dl
-      //     ? XLSX.write(wb, { bookType: type, bookSST: true, type: "base64" })
-      //     : XLSX.writeFile(wb, fn || "WellCard." + (type || "xlsx"));
-
       var downloadLink;
       var dataType = "application/vnd.ms-excel";
       var tableSelect = document.getElementById(tbl_exporttable_to_xls);
